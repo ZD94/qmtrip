8426ffbbd92a3b9dc8cad6b96121eb6779061db9
Ke Peng <ke.peng@tulingdao.com>
2015-12-08 12:25:45 +0800

    init server code.

---
+++ config/index.js
@@ -0,0 +1,2 @@
+"use strict";
+module.exports = require('common/config')(__dirname);
---
+++ server.js
@@ -0,0 +1,34 @@
+/**
+ * Created by YCXJ on 2014/5/6.
+ */
+'use strict';
+var path = require('path');
+var Q = require('q');
+var fs = require("fs");
+
+//可以直接require服务器根目录下的模块
+require('app-module-path').addPath(__dirname);
+
+var config = require("./config");
+
+var Logger = require('common/logger');
+Logger.init(config.logger);
+var logger = new Logger('main');
+
+var Server = require('common/server');
+var server = new Server(config.appName, config.pid_file);
+
+server.enable_cluster = config.cluster;
+
+server.http_handler = require('./app');
+server.http_port = config.port;
+if(config.socket_file){
+    server.http_port = config.socket_file;
+}
+
+server.api_path = path.join(__dirname, 'api');
+server.api_port = config.apiPort;
+server.api_config = config.api;
+
+server.start();
+
