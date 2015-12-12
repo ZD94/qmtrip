1b240331323c1ed519705199bd76a4800827cf50
wanglihui <lihui.wang@tulingdao.com>
2015-12-12 16:20:16 +0800

    add power module

---
+++ api/power/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by wlh on 15/12/12.
+ *
+ *  权限模块
+ */
+
+var services = {};
+
+var STAFF_POWER = [];   //员工权限
+var MANAGER_POWER = []; //管理员权限
+var OWNER_POWER = [];   //拥有者权限
+
+
+services.getPower = function(data, callback) {
+
+}
+module.exports = services;
