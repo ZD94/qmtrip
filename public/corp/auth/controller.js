3c267010fb31df148067c8f8cc2282ffbe72bae6
qp <qiuping.cao@tulingdao.com>
2015-12-14 16:32:56 +0800

    all-login.html

---
+++ public/corp/auth/controller.js
@@ -0,0 +1,15 @@
+
+'use strict';
+var corp=(function(){
+    API.require('auth');
+
+    var  corp = {};
+
+    corp.loginController = function ($scope) {
+        
+    }
+    corp.registerController = function($scope) {
+
+    }
+})();
+module.exports = corp;
