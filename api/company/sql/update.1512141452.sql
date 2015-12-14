63fd98bfb0edf8985668325036214d725347e8f5
yumiao <miao.yu@tulingdao.com>
2015-12-14 18:43:27 +0800

    alter table company add column domain_name

--- api/company/sql/update.1512141452.sql
+++ api/company/sql/update.1512141452.sql
@@ -4,6 +4,7 @@ alter table company.company add column agency_id uuid;
 alter table company.company rename column logo to domain_name;
 COMMENT ON COLUMN company.company.domain_name IS '企业域名';
 
+alter table company.company drop column if exists company_create_at;
 alter table company.company drop column if exists website;
 
 alter table company.funds_accounts add column status integer default 0;
