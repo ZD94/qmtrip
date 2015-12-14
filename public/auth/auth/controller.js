fbcd457aae0ce1dd4a6b53b20ffb818a2045712d
qp <qiuping.cao@tulingdao.com>
2015-12-14 17:37:18 +0800

    add auth module

---
+++ public/auth/auth/controller.js
@@ -0,0 +1,20 @@
+
+'use strict';
+var auth=(function(){
+    //API.require('auth');
+    //
+    //API.onload(function() {
+    //    console.info(API);
+    //})
+    var  auth = {};
+
+    auth.loginController = function ($scope) {
+
+    }
+    auth.registerController = function($scope) {
+
+    }
+    return auth;
+})();
+
+module.exports = auth;
