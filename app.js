9dae9ab23511529cfff71375a4b9085cda5e4532
Ke Peng <ke.peng@tulingdao.com>
2015-12-17 18:44:08 +0800

    添加actions/.holder文件

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ var fs = require('fs');
 
 function load_action_dir(app, dir){
     var action_files = fs.readdirSync(dir);
-    action_files.sort();
-    for(var f of action_files){
-        var filepath = path.join(dir, f);
-        var actions = require(filepath);
-        if (typeof actions == 'function') {
-            actions = [actions];
-        }
-        for(var action of actions){
-            action(app);
-        }
-    }
+    action_files = action_files
+        .filter(function(name){ return !name.match(/^\./); })
+        .sort()
+        .forEach(function(filename){
+            var filepath = path.join(dir, filename);
+            var actions = require(filepath);
+            if (typeof actions == 'function') {
+                actions = [actions];
+            }
+            for(var action of actions){
+                action(app);
+            }
+        });
 }
 
 function initHttpApp(app){
