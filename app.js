4e1ef16bf9ae6907a76bcd071d061518a4ae9a5c
Ke Peng <ke.peng@tulingdao.com>
2015-12-17 17:00:43 +0800

    http favicon和static root的逻辑移入common/server

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 "use strict";
 
-var path = require('path');
-var express = require('express');
-
 function initHttpApp(app){
-    var favicon = require('serve-favicon');
-    app.use(favicon(path.join(__dirname, '/public/favicon.ico'), {maxAge: 1}));
-
-    app.use(express.static(path.join(__dirname, 'public')));
-
     app.use('/', require('./routes/index'));
 }
 
