6dffc56e642577f4a92f012fc5997fff8895b9da
yumiao <miao.yu@tulingdao.com>
2015-12-17 15:55:58 +0800

    新增agency API说明

--- api/client/company/index.js
+++ api/client/company/index.js
@@ -2,10 +2,9 @@
  * Created by yumiao on 15-12-9.
  */
 
-var API = require('../../common/api');
-var Logger = require('../../common/logger');
-var auth = require('./auth');
-var needPowersMiddleware = auth.needPowersMiddleware;
+var API = require('common/api');
+var Logger = require('common/logger');
+var needPowersMiddleware = require('../auth').needPowersMiddleware;
 var logger = new Logger();
 
 var company = {}
@@ -18,7 +17,6 @@ var company = {}
  * @returns {*}
  */
 company.createCompany = function(params, callback){
-    logger.info("createCompany=>\n", params);
     params.createUser = this.accountId;
     return API.company.createCompany(params, callback);
 };
