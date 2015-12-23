aa54d1a9b378c3a5bbc8db8082c838bab44acc6a
wangyali <yali.wang@tulingdao.com>
2015-12-23 15:18:52 +0800

    getCurrentAgency

--- api/client/agency/index.js
+++ api/client/agency/index.js
@@ -87,13 +87,7 @@ agency.deleteAgencyUser = API.agency.deleteAgencyUser;
 agency.updateAgencyUser = API.agency.updateAgencyUser;
 agency.getAgencyUser = API.agency.getAgencyUser;
 agency.getCurrentAgency = function(callback){
-    return API.agency.getAgencyUser(this.accountId)
-        .then(function(user){
-            var agencyId = user.agencyId;
-            return API.agency.getAgency({accountId: this.accountId, agencyId: agencyId});
-        })
-        .nodeify(callback);
-
+    return API.agency.getAgencyUser(this.accountId, callback);
 }
 agency.listAndPaginateAgencyUser = API.agency.listAndPaginateAgencyUser;
 
