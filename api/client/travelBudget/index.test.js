5a535fefa5e144d7da21a0d543c756787b58c1ec
wanglihui <lihui.wang@tulingdao.com>
2015-12-24 16:50:37 +0800

    修改 获取预算,获取商圈API

--- api/client/travelBudget/index.test.js
+++ api/client/travelBudget/index.test.js
@@ -2,9 +2,26 @@
  * Created by wlh on 15/12/24.
  */
 
+var travelBudget = require("./index");
+var assert = require("assert");
+
 describe("API.travelBudget", function() {
 
     it("#getTravelPolicyBudget should be ok", function(done) {
-        done();
+        travelBudget.getTravelPolicyBudget("北京", function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            if (typeof result == 'string') {
+                result = JSON.parse(result);
+            }
+            var traffic = result.traffic ? true: false;
+            var hotel = result.hotel ? true : false;
+
+            assert.equal(traffic, true);
+            assert.equal(hotel, true);
+            done();
+        })
     })
-})
+});
