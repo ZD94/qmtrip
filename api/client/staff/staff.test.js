b4573125d92745b8e7c711e35e0c1f28b0da022c
wangyali <yali.wang@tulingdao.com>
2015-12-29 12:28:36 +0800

    taff unit testing

--- api/client/staff/staff.test.js
+++ api/client/staff/staff.test.js
@@ -7,116 +7,120 @@ var assert = require("assert");
 
 describe("api/client/staff.js", function() {
 
+    var id = "0387c260-addb-11e5-abe2-3fab33762bb5";
     var obj = {
-        email: "yali188.wang@tulingdao.com",
-        name: "wyl163",
-        mobile: "18301208613",
+        email: "xiaoyu.wang@tulingdao.com",
+        name: "wyll",
+        mobile: "18345433986",
         companyId: '9f20e3c0-9f24-11e5-beab-31a51ecd9fc2'
     }
 
+    var updateobj = {
+        id:"bb8b8710-a39b-11e5-ad58-6b881dd4ecc0",
+        email: "yali.wang@tulingdao.com",
+        name: "wyll",
+        mobile: "18345433986",
+        companyId: '9f20e3c0-9f24-11e5-beab-31a51ecd9fc2'
+    }
+
+
     //创建员工
-    /*describe("API.staff.createStaff", function() {
+    describe("API.staff.createStaff", function() {
         it("API.staff.createStaff", function(done) {
-            API.client.staff.createStaff(obj, function(err, result) {
+            API.staff.createStaff(obj, function(err, result) {
                 assert.equal(err, null);
-                assert.equal(result.code, 0);
+                console.log(err);
                 console.log(result);
+                id = result.id;
                 done();
             });
         })
-    })*/
+    })
     //查询员工集合
-    /*describe("API.staff.listAndPaginateStaff", function() {
+    describe("API.staff.listAndPaginateStaff", function() {
         it("API.staff.listAndPaginateStaff", function(done) {
-            API.client.staff.listAndPaginateStaff({}, function(err, result) {
+            API.staff.listAndPaginateStaff({}, function(err, result) {
                 assert.equal(err, null);
+                console.log(err);
                 console.log(result);
 //                console.log(result.items);//item dataValues里存放的才是记录信息
-                *//*{ page: 1,
-                    perPage: 6,
-                    total: 1,
-                    items:
-                    [ { dataValues: [Object],
-                        _previousDataValues: [Object],
-                        _changed: {},
-                        '$modelOptions': [Object],
-                        '$options': [Object],
-                        hasPrimaryKeys: true,
-                        __eagerlyLoadedAssociations: [],
-                        isNewRecord: false } ],
-                        currentPageTotal: 1,
-                    pages: 1 }*//*
                 done();
             });
         })
-    })*/
+    })
+
     //更新员工信息
-    /*describe("API.staff.updateStaff", function() {
+    describe("API.staff.updateStaff", function() {
         it("API.staff.updateStaff", function(done) {
-            API.client.staff.updateStaff("cae7bfa0-9f25-11e5-ada1-dda0b01c44bc", obj, function(err, result) {
+            API.staff.updateStaff(updateobj, function(err, result) {
                 assert.equal(err, null);
+                console.log(err);
                 console.log(result);
                 done();
             });
         })
-    })*/
-    //删除员工信息
-    /*describe("API.staff.deleteStaff", function() {
-        it("API.staff.deleteStaff", function(done) {
-            API.client.staff.deleteStaff({id: "9f20e3c0-9f24-11e5-beab-31a51ecd9fc2"}, function(err, result) {
-                assert.equal(err, null);
-                console.log(result);
-                done();
-            });
-        })
-    })*/
+    })
     //通过id得到员工
-    /*describe("API.staff.getStaff", function() {
+    describe("API.staff.getStaff", function() {
         it("API.staff.getStaff", function(done) {
-            API.client.staff.getStaff("ee3eb6a0-9f22-11e5-8540-8b3d4cdf6eb6", function(err, result) {
+            API.staff.getStaff({id:id}, function(err, result) {
                 assert.equal(err, null);
+                console.log(err);
                 console.log(result);
                 done();
             });
         })
-    })*/
+    })
     //加积分
-    /*describe("API.staff.increaseStaffPoint", function() {
+    describe("API.staff.increaseStaffPoint", function() {
         it("API.staff.increaseStaffPoint", function(done) {
-            API.client.staff.increaseStaffPoint({id: "ee3eb6a0-9f22-11e5-8540-8b3d4cdf6eb6", increasePoint: 1000, remark: "差旅省钱加积分"}, function(err, result) {
+            API.staff.increaseStaffPoint({id: id, increasePoint: 1000, remark: "差旅省钱加积分"}, function(err, result) {
                 assert.equal(err, null);
+                console.log(err);
                 console.log(result);
                 done();
             });
         })
-    })*/
+    })
     //减积分
-    /*describe("API.staff.decreaseStaffPoint", function() {
+    describe("API.staff.decreaseStaffPoint", function() {
         it("API.staff.decreaseStaffPoint", function(done) {
-            API.client.staff.decreaseStaffPoint({id: "ee3eb6a0-9f22-11e5-8540-8b3d4cdf6eb6", decreasePoint: 1000, remark: "兑换礼品减积分"}, function(err, result) {
+            API.staff.decreaseStaffPoint({id: id, decreasePoint: 1000, remark: "兑换礼品减积分"}, function(err, result) {
                 assert.equal(err, null);
                 console.log(err);
                 console.log(result);
                 done();
             });
         })
-    })*/
+    })
     //积分记录查询
-    /*describe("API.staff.listAndPaginatePointChange", function() {
+    describe("API.staff.listAndPaginatePointChange", function() {
         it("API.staff.listAndPaginatePointChange", function(done) {
-            API.client.staff.listAndPaginatePointChange({staffId: "ee3eb6a0-9f22-11e5-8540-8b3d4cdf6eb6"}, function(err, result) {//查询条件此处用staffId或者staff_id均可
+            API.staff.listAndPaginatePointChange({staffId: id}, function(err, result) {//查询条件此处用staffId或者staff_id均可
                 assert.equal(err, null);
                 console.log(err);
                 console.log(result);
                 done();
             });
         })
-    })*/
+    })
+
+    //删除员工信息
+    describe("API.staff.deleteStaff", function() {
+        it("API.staff.deleteStaff", function(done) {
+            API.staff.deleteStaff({id: id}, function(err, result) {
+                assert.equal(err, null);
+                console.log(err);
+                console.log(result);
+                done();
+            });
+        })
+    })
 
     //导入员工
     //describe("API.staff.importExcel", function() {
     //    it("API.staff.importExcel", function(done) {
-    //        API.client.staff.importExcel({}, function(err, result) {//查询条件此处用staffId或者staff_id均可
+    //        API.staff.importExcel({}, function(err, result) {//查询条件此处用staffId或者staff_id均可
     //            assert.equal(err, null);
     //            console.log(err);
     //            console.log(result);
