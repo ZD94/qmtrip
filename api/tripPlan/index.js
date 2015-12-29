75d629dfcf9099c3e2a6a4773b8b84ac3d9d2b71
yumiao <miao.yu@tulingdao.com>
2015-12-29 16:53:44 +0800

    只选择住宿 不限制出发地点和时间

--- api/tripPlan/index.js
+++ api/tripPlan/index.js
@@ -28,7 +28,7 @@ var tripPlan = {}
  * @returns {*}
  */
 tripPlan.savePlanOrder = function(params, callback){
-    var checkArr = ['accountId', 'companyId', 'type', 'startPlace', 'destination', 'startAt', 'budget'];
+    var checkArr = ['accountId', 'companyId', 'type', 'destination', 'budget'];
     return Q.all([
         API.seeds.getSeedNo('tripPlanOrderNo'),
         checkParams(checkArr, params)
