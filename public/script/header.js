b258ea1a3c207de65abf6a1d3247a39c452e582f
chenhao <hao.chen@tulingdao.com>
2015-12-29 10:38:15 +0800

    header.js console

--- public/script/header.js
+++ public/script/header.js
@@ -1,5 +1,5 @@
 $(function(){
-	$(window).load(function(){ minHeight();console.info("9999999999999")});
+	$(window).load(function(){ minHeight(););
 	$(window).scroll(function(){ minHeight();});
 	$(window).resize(function(){ minHeight();});
 	$("#account").hover(function(){
