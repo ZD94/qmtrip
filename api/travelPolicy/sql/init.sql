8f3e86afb7b0c178cd274a9d41cdfa7664641886
wangyali <yali.wang@tulingdao.com>
2016-01-06 12:17:07 +0800

    console delete

--- api/travelPolicy/sql/init.sql
+++ api/travelPolicy/sql/init.sql
@@ -22,3 +22,9 @@ COMMENT ON COLUMN travelpolicy.travel_policy.hotel_level IS '酒店标准';
 COMMENT ON COLUMN travelpolicy.travel_policy.hotel_price IS '酒店价格';
 COMMENT ON COLUMN travelpolicy.travel_policy.is_change_level IS '是否允许调换机票等级';
 COMMENT ON COLUMN travelpolicy.travel_policy.company_id IS '所属企业id';
+
+INSERT INTO travelpolicy.travel_policy(
+            id, name, plane_level, plane_discount, train_level, hotel_level,
+            hotel_price, is_change_level, company_id, create_at)
+    VALUES ('dc6f4e50-a9f2-11e5-a9a3-9ff0188d1c1a', '系统默认标准', '经济舱', 8.0, '二等座/硬卧', '四星级/高档型',
+            400, true, null, now());
