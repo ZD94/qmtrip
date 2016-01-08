0aeba179ebbf983a519abb5feb27dfa653955c58
chenhao <hao.chen@tulingdao.com>
2016-01-08 18:45:52 +0800

    首页图表

--- public/corp/UsersFirst/controller.js
+++ public/corp/UsersFirst/controller.js
@@ -31,7 +31,16 @@ var UsersFirst = (function(){
 								$scope.funds = resutlt;
 								$scope.num = num;
 								$scope.travelLevel = travel_level;
-								console.info(date);
+								if(date.planMoney>date.expenditure) {
+									$scope.different =  "节省 "+Math.round((date.planMoney-date.expenditure)*100/date.planMoney)+"%";
+								}
+								else if(date.planMoney<date.expenditure){
+									$scope.different =  "超出 "+Math.round((date.expenditure-date.planMoney)*100/date.expenditure)+"%";
+								}
+								else{
+									$scope.different = "持平 0%"
+								}
+								$scope.initCharts(date.qmBudget,date.planMoney,date.expenditure);
 								$scope.$apply();
 							})
 							.catch(function(err){
@@ -45,6 +54,109 @@ var UsersFirst = (function(){
 			})
 		}
 		$scope.initCorpMain();
+		$scope.initCharts = function(first,second,third){
+			var myChart = window.echarts.init(document.getElementById('charts')); 
+		    var option = {
+				    tooltip: {
+				        trigger: 'item'
+				    },
+				    toolbox: {
+				        show: false,							//工具箱
+				        feature: {
+				            dataView: {show: true, readOnly: false},
+				            restore: {show: true},
+				            saveAsImage: {show: true}
+				        }
+				    },
+				    calculable: false,							//禁止拖拽
+				    grid: {
+				        borderWidth: 0,
+				        borderColor:'#fff',
+				        x:0,
+				        y: 80,
+				        y2: 30
+				    },
+				    xAxis: [
+				        {
+				            type: 'category',
+				            show: true,							//x轴是否显示
+				            axisLine : {						//轴线
+				            	lineStyle:{
+				            		color:'#ededed',
+				            		type:'solid',
+				            		width:4
+				            	}
+				            },
+				            axisTick:{							//轴标记
+				            	show:false
+				            },
+				            axisLabel:{	
+				            	textStyle:{
+				            		fontSize:14
+				            	}
+				            },
+				            splitLine: {show:false},			//分隔线
+				            boundaryGap: true,
+				            data: ['动态预算', '计划支出', '实际支出']
+				        }
+				    ],
+				    yAxis: [
+				        {
+				            type: 'value',
+				            show: false
+				        }
+				    ],
+				    series: [
+				        {
+				            type: 'bar',
+				            itemStyle: {
+				                normal: {
+				                    color: function(params) {
+				                        // build a color map as your need.
+				                        var colorList = [
+				                          '#00EACF','#FD6961','#8250FE'
+				                        ];
+				                        return colorList[params.dataIndex]
+				                    },
+				                    label: {
+				                        show: true,
+				                        position: 'top',
+				                        formatter: '￥{c}'
+				                    }
+				                }
+				            },
+				            markLine:{
+				            	data: [
+				            		// [
+				            		// 	{name:"first",xAxis:'计划支出',yAxis: 20,type:"max"},
+				            		// 	{name:"second",xAxis:'实际支出',yAxis: 20,type:"min"}
+				            		// ]##01BDFF
+				            		[
+				            			{name:"节省",value:second-third,xAxis:1,yAxis: second,
+				            				itemStyle:{
+				            					normal:{
+				            							color:'#01BDFF',
+					            						label:{
+						            						show:false
+						            					}
+				            						}
+				            					}
+				            				},
+				            			{xAxis:2,yAxis: third}
+				            		]
+				            		// [
+				            		// 	{type:'max',name:'first'},
+				            		// 	{type:'min',name:"two"}
+				            		// ]
+				            	]
+				            },
+				            data: [first,second,third]
+				        }
+				    ]
+				};
+		    // 为echarts对象加载数据 
+		    myChart.setOption(option); 
+		}
 	}
 
 	return UsersFirst;
