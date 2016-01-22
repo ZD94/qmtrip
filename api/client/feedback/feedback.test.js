ed7257b1e5b21ba22334c557bd9a9df7f0bf3ff6
wangyali <yali.wang@tulingdao.com>
2016-01-22 15:54:13 +0800

    意见反馈

---
+++ api/client/feedback/feedback.test.js
@@ -0,0 +1,89 @@
+/**
+ * Created by wyl on 16-01-22.
+ */
+var API = require('common/api');
+
+var assert = require("assert");
+var Q = require("q");
+
+describe("api/client/feedback.js", function() {
+
+    var parentId_f = "";
+    var agencyUserId = "";
+    var self = {};
+    var companyId = "";
+    var accountId = "";
+    var obj = {
+        name: "销售部"
+    }
+    var company = {
+        name: 'feedbackTest的企业',
+        userName: '张三',
+        domain: 'tulingdao.com',
+        description: '企业API测试用',
+        email: 'feedback.company.test@tulingdao.com',
+        mobile: '15269866999'
+    }
+
+    var agency = {
+        email: "feedback.agency.test@tulingdao.com",
+        userName: "feedbackTest代理商",
+        name: 'feedbackTest的代理商',
+        mobile: "15269866777",
+        description: '企业API测试用'
+    };
+
+    //创建部门
+    before(function(done) {
+        Q.all([
+                API.agency.deleteAgencyByTest({email: agency.email, mobile: agency.mobile}),
+                API.company.deleteCompanyByTest({email: company.email, mobile: company.mobile}),
+                API.staff.deleteAllStaffByTest({email: company.email, mobile: company.mobile})
+            ])
+            .spread(function(ret1, ret2, ret3){
+                return API.agency.registerAgency(agency);
+            })
+            .then(function(ret){
+                agencyId = ret.agency.id;
+                agencyUserId = ret.agencyUser.id;
+                return API.client.company.createCompany.call({accountId: agencyUserId}, company);
+            })
+            .then(function(company){
+                assert.equal(company.status, 0);
+                companyId = company.id;
+                accountId = company.createUser;
+                self = {accountId: accountId};
+                done();
+            })
+            .catch(function(err){
+                console.info(err);
+                throw err;
+            })
+            .done();
+    });
+
+    after(function(done) {
+        Q.all([
+                API.agency.deleteAgencyByTest({email: agency.email, mobile: agency.mobile}),
+                API.company.deleteCompanyByTest({email: company.email, mobile: company.mobile}),
+                API.staff.deleteAllStaffByTest({email: company.email, mobile: company.mobile})
+            ])
+            .spread(function(ret1, ret2, ret3){
+                done();
+            })
+            .catch(function(err){
+                console.info(err);
+                throw err;
+            })
+            .done();
+    });
+    //创建默认部门
+    it("#sendFeedback should be ok", function(done) {
+        obj.companyId = companyId;
+        API.client.feedback.sendFeedback.call(self, {content: "意见反馈test", isAnonymity: true}, function(err, result) {
+            assert.equal(err, null);
+            //console.log(result);
+            done();
+        });
+    })
+})
---
+++ api/feedback/sql/init.sql
@@ -0,0 +1,19 @@
+--schema=feedback
+create table feedback.feedback (
+    id uuid primary key,
+    content text,
+    user_name varchar(50),
+    company_name varchar(50),
+    user_id uuid,
+    is_anonymity boolean default false,
+    create_at timestamp without time zone DEFAULT now()
+);
+
+COMMENT ON TABLE feedback.feedback
+  IS '意见反馈表';
+COMMENT ON COLUMN feedback.feedback.content IS '反馈内容';
+COMMENT ON COLUMN feedback.feedback.user_name IS '用户名';
+COMMENT ON COLUMN feedback.feedback.company_name IS '企业名';
+COMMENT ON COLUMN feedback.feedback.user_id IS '反馈人id';
+COMMENT ON COLUMN feedback.feedback.is_anonymity IS '是否匿名';
+COMMENT ON COLUMN feedback.feedback.create_at IS '反馈时间';
