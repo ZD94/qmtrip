7670c77848b3f1a6d018a13c61f810da6e0b3d59
wanglihui <lihui.wang@tulingdao.com>
2016-01-25 11:10:34 +0800

    0. 修改mediaId2key 错误 1. 修改mediaId2key 测试用例

--- api/client/wechat/wechat.test.js
+++ api/client/wechat/wechat.test.js
@@ -9,6 +9,7 @@ var path = require("path");
 describe("api/client/wechat/index.js", function() {
     this.timeout(60 * 1000);
 
+    var key;
     var mediaId;
     before(function(done) {
         var filepath = path.join(__dirname, "../../../public/www/img/5050.jpg");
@@ -19,13 +20,14 @@ describe("api/client/wechat/index.js", function() {
             mediaId = result.media_id;
             done();
         });
-    })
+    });
 
     it("#mediaId2key should be ok", function(done) {
         var self = {accountId: "12341234-1234-1234-1234-123400001234"};
         API.client.wechat.mediaId2key.call(self, {mediaId: mediaId}, function(err, result) {
             assert.equal(err, null);
             assert.equal(result.length, 32);
+            key = result;
             done();
         })
     })
