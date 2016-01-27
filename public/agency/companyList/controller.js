a5295282f30a76d5cd299b89058bb86424ab4513
jian.zhang@tulingdao.com <jian.zhang@tulingdao.com>
2016-01-27 15:30:46 +0800

    '代理商协助企业管理差旅标准'

--- public/agency/companyList/controller.js
+++ public/agency/companyList/controller.js
@@ -738,7 +738,7 @@ module.exports = (function(){
 
 
 	//组织架构页面
-	companyList.DepartmentController = function($scope,  $routeParams) {
+	companyList.DepartmentController = function($scope, $routeParams) {
 		$("title").html("组织架构");
 		loading(false);
 		//初始化
@@ -859,5 +859,326 @@ module.exports = (function(){
 			$(".confirmFixed").hide();
 		}
 	}
+
+
+
+
+
+
+
+
+	/*
+	 差旅标准列表
+	 * @param $scope
+	 * @constructor
+	 */
+	companyList.PolicyListController = function($scope, $routeParams) {
+		$("title").html("差旅标准");
+		Myselect();
+		$scope.companyId = $routeParams.companyId;
+
+		//获取差旅标准列表
+		$scope.initPolicyList = function () {
+			loading(false);
+			API.onload(function(){
+				var params = {};
+				var options = {order: [["create_at", "asc"]]};
+				options.perPage = 100;
+				params.options = options;
+				params.companyId = $scope.companyId;
+				API.travelPolicy.agencyListAndPaginateTravelPolicy(params)
+					.then(function(result){
+						console.info (result);
+						$scope.PolicyTotal = result.total;
+						$scope.PolicyList = result.items;
+						if ($scope.PolicyTotal==0) {
+							$(".create_policy").show();
+						}
+						$(window).scroll(function() {
+							if ($(window).scrollTop()<=64) {
+								$(".policy_title").removeClass('policy_titlefixed');
+
+							}
+							else {
+								$(".policy_title").addClass('policy_titlefixed');
+							}
+						});
+						loading(true);
+						$scope.$apply();
+					})
+					.catch(function(err){
+						console.info (err);
+					});
+			})
+		}
+		$scope.initPolicyList();
+
+
+		//增加标准
+		$scope.createPolicyShow = function () {
+			$scope.resetting();
+			$(".create_policy").show();
+			$(".update_policy").hide();
+			$(".policy_page li").css('opacity','1');
+		}
+		//增加标准取消
+		$scope.createClose = function () {
+			$(".create_policy").hide();
+			$(".policy_page li").css('opacity','1');
+		}
+		$scope.createPolicy = function () {
+			if ($(".create_policy .Cname").val()=="") {
+				Myalert("温馨提示","请填写等级名称");
+				return false;
+			}
+			if ($(".create_policy .CplaneLevel").html()=="请选择仓位") {
+				Myalert("温馨提示","请选择飞机仓位");
+				return false;
+			}
+			if ($(".create_policy .CplaneDiscount").html()=="请选择折扣") {
+				Myalert("温馨提示","请选择飞机折扣");
+				return false;
+			}
+			if ($(".create_policy .CtrainLevel").html()=="请选择座次") {
+				Myalert("温馨提示","请选择火车座次");
+				return false;
+			}
+			if ($(".create_policy .ChotelTevel").html()=="星级标准") {
+				Myalert("温馨提示","请选择住宿标准");
+				return false;
+			}
+
+
+
+			API.onload(function(){
+				API.travelPolicy.agencyCreateTravelPolicy({
+					name:$(".create_policy .Cname").val(),
+					planeLevel:$(".create_policy .CplaneLevel").html(),
+					planeDiscount:$(".create_policy .CplaneDiscount").attr('selectValue'),
+					trainLevel:$(".create_policy .CtrainLevel").html().replace('/',','),
+					isChangeLevel:$(".create_policy .Ccheckbox").is(':checked'),
+					hotelLevel:$(".create_policy .ChotelTevel").html().replace('/',','),
+					hotelPrice:$(".create_policy .ChotelPrice").val(),
+					companyId:$scope.companyId
+				})
+					.then(function(result){
+						Myalert("温馨提示","增加成功");
+						$scope.initPolicyList();
+						$(".create_policy").hide();
+						console.info (result);
+					})
+					.catch(function(err){
+						Myalert("温馨提示", err.msg);
+						console.info (err);
+					});
+			})
+		}
+
+
+		//删除标准
+		$scope.deletePolicyShow = function (id,name) {
+			$scope.deleteId = id;
+			$scope.deleteName = name;
+			$(".messageText").html("您确定删除&quot;"+$scope.deleteName+"&quot;差旅标准吗？");
+			$(".confirmFixed").show();
+		}
+		$scope.deletePolicy = function () {
+			API.onload(function(){
+				API.travelPolicy.agencyDeleteTravelPolicy({companyId:$scope.companyId,id:$scope.deleteId})
+					.then(function(result){
+						$(".confirmFixed").hide();
+						$scope.initPolicyList();
+						console.info (result);
+					})
+					.catch(function(err){
+						$(".deleteFail").show();
+						$(".deleteFail_text").html(err.msg);
+						console.info (err);
+					});
+			})
+		}
+
+		//关闭弹窗
+		$scope.confirmClose = function () {
+			$(".confirmFixed,.deleteFail").hide();
+		}
+
+
+
+
+
+
+
+
+		//修改标准
+		$scope.updatePolicyShow = function (id,index) {
+			$scope.updateId = id;
+			if (index == 0) {
+				$(".update_policy").css({'top':'10px','left':'0px'});
+			}
+			else if (index == 1) {
+				$(".update_policy").css({'top':'10px','left':'505px'});
+			}
+			else if (index == 2) {
+				$(".update_policy").css({'top':'230px','left':'0px'});
+			}
+			else if (index == 3) {
+				$(".update_policy").css({'top':'230px','left':'505px'});
+			}
+			else if (index == 4) {
+				$(".update_policy").css({'top':'450px','left':'0px'});
+			}
+			else if (index == 5) {
+				$(".update_policy").css({'top':'450px','left':'505px'});
+			}
+			else if (index == 6) {
+				$(".update_policy").css({'top':'670px','left':'0px'});
+			}
+			else if (index == 7) {
+				$(".update_policy").css({'top':'670px','left':'505px'});
+			}
+			else if (index == 8) {
+				$(".update_policy").css({'top':'890px','left':'0px'});
+			}
+			else if (index == 9) {
+				$(".update_policy").css({'top':'890px','left':'505px'});
+			}
+			else if (index == 10) {
+				$(".update_policy").css({'top':'1110px','left':'0px'});
+			}
+			else if (index == 11) {
+				$(".update_policy").css({'top':'1110px','left':'505px'});
+			}
+			else if (index == 12) {
+				$(".update_policy").css({'top':'1330px','left':'0px'});
+			}
+			else if (index == 13) {
+				$(".update_policy").css({'top':'1330px','left':'505px'});
+			}
+			else if (index == 14) {
+				$(".update_policy").css({'top':'1550px','left':'0px'});
+			}
+			else if (index == 15) {
+				$(".update_policy").css({'top':'1550px','left':'505px'});
+			}
+			var obj = {0:"全价",8:"最高8折",7:"最高7折",6:"最高6折",5:"最高5折",4:"最高4折"};
+			var discountTxt = $scope.PolicyList[index].planeDiscount;
+			$(".update_policy .Cname").val($scope.PolicyList[index].name);
+			$(".update_policy .CplaneLevel").html($scope.PolicyList[index].planeLevel);
+			$(".update_policy .CplaneDiscount").html(obj[discountTxt]).attr("selectValue",discountTxt);
+			$(".update_policy .CtrainLevel").html($scope.PolicyList[index].trainLevel);
+			$(".update_policy .Ccheckbox").attr('checked',$scope.PolicyList[index].isChangeLevel);
+			$(".update_policy .ChotelTevel").html($scope.PolicyList[index].hotelLevel);
+			$(".update_policy .ChotelPrice").val($scope.PolicyList[index].hotelPrice);
+			if ($scope.PolicyList[index].isChangeLevel==true) {
+				$(".Ccheckboxlabel").removeClass('lablefalse');
+				$(" .Ccheckboxlabel").html('&#xe9ec;');
+			}
+			else {
+				$(".Ccheckboxlabel").addClass('lablefalse');
+				$(".Ccheckboxlabel").html('');
+			}
+			$(".update_policy").show();
+			$(".create_policy").hide();
+			$(".policy_page li").css('opacity','0.2');
+		}
+		//修改标准取消
+		$scope.updateClose = function () {
+			$(".update_policy").hide();
+			$(".policy_page li").css('opacity','1');
+		}
+		$scope.updatePolicy = function () {
+			if ($(".update_policy .Cname").val()=="") {
+				Myalert("温馨提示","请填写等级名称");
+				return false;
+			}
+			if ($(".update_policy .CplaneLevel").html()=="请选择仓位") {
+				Myalert("温馨提示","请选择飞机仓位");
+				return false;
+			}
+			if ($(".update_policy .CplaneDiscount").html()=="请选择折扣") {
+				Myalert("温馨提示","请选择飞机折扣");
+				return false;
+			}
+			if ($(".update_policy .CtrainLevel").html()=="请选择座次") {
+				Myalert("温馨提示","请选择火车座次");
+				return false;
+			}
+			if ($(".update_policy .ChotelTevel").html()=="星级标准") {
+				Myalert("温馨提示","请选择住宿标准");
+				return false;
+			}
+
+
+			API.onload(function(){
+				API.travelPolicy.agencyUpdateTravelPolicy({
+					id:$scope.updateId,
+					name:$(".update_policy .Cname").val(),
+					planeLevel:$(".update_policy .CplaneLevel").html(),
+					planeDiscount:$(".update_policy .CplaneDiscount").attr('selectValue'),
+					trainLevel:$(".update_policy .CtrainLevel").html(),
+					isChangeLevel:$(".update_policy .Ccheckbox").is(':checked'),
+					hotelLevel:$(".update_policy .ChotelTevel").html(),
+					hotelPrice:$(".update_policy .ChotelPrice").val(),
+					companyId:$scope.companyId
+				})
+					.then(function(result){
+						Myalert("温馨提示","修改成功");
+						$scope.initPolicyList();
+						$(".update_policy").hide();
+						$(".policy_page li").css('opacity','1');
+						console.info (result);
+					})
+					.catch(function(err){
+						Myalert("温馨提示","内容不完整，请重新填写");
+						console.info (err);
+					});
+			})
+		}
+
+
+		//重置
+		$scope.resetting = function () {
+			$(".Cname").val("");
+			$(".CplaneLevel").html("不限");
+			$(".CplaneDiscount").html("不限").attr("selectValue","0");
+			$(".CtrainLevel").html("不限");
+			$(".Ccheckbox").attr('checked',true);
+			$(".Ccheckboxlabel").removeClass('lablefalse');
+			$(".Ccheckboxlabel").html('&#xe9ec;');
+			$(".ChotelTevel").html("不限");
+			$(".ChotelPrice").val("");
+		}
+
+		//修改自定义复选框
+		$scope.updateChangecheck = function () {
+			if ($(".update_policy .Ccheckbox").is(':checked')==false) {
+				$(".update_policy .Ccheckboxlabel").removeClass('lablefalse');
+				$(".update_policy .Ccheckboxlabel").html('&#xe9ec;');
+			}
+			else {
+				$(".update_policy .Ccheckboxlabel").addClass('lablefalse');
+				$(".update_policy .Ccheckboxlabel").html('');
+			}
+		}
+		//创建自定义复选框
+		$scope.createChangecheck = function () {
+			if ($(".create_policy .Ccheckbox").is(':checked')==false) {
+				$(".create_policy .Ccheckboxlabel").removeClass('lablefalse');
+				$(".create_policy .Ccheckboxlabel").html('&#xe9ec;');
+			}
+			else {
+				$(".create_policy .Ccheckboxlabel").addClass('lablefalse');
+				$(".create_policy .Ccheckboxlabel").html('');
+			}
+		}
+
+	}
+
+
+
+
+
+
 	return companyList;
 })();
