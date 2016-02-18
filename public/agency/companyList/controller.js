1e4785f97b82e68dd773892bea5043e30e1df4cf
jian.zhang@tulingdao.com <jian.zhang@tulingdao.com>
2016-02-18 17:19:15 +0800

    '差旅标准bug'

--- public/agency/companyList/controller.js
+++ public/agency/companyList/controller.js
@@ -956,10 +956,10 @@ module.exports = (function(){
 					planeLevel:$(".create_policy .CplaneLevel").html(),
 					planeDiscount:$(".create_policy .CplaneDiscount").attr('selectValue'),
 					trainLevel:$(".create_policy .CtrainLevel").html().replace('/',','),
-					isChangeLevel:$(".create_policy .Ccheckbox").is(':checked'),
+					isChangeLevel:$(".create_policy .Ccheckbox").attr('checkedd'),
 					hotelLevel:$(".create_policy .ChotelTevel").html().replace('/',','),
 					hotelPrice:$(".create_policy .ChotelPrice").val(),
-					companyId:$scope.companyId
+					companyId:$scope.company_id
 				})
 					.then(function(result){
 						Myalert("温馨提示","增加成功");
@@ -1117,10 +1117,10 @@ module.exports = (function(){
 					planeLevel:$(".update_policy .CplaneLevel").html(),
 					planeDiscount:$(".update_policy .CplaneDiscount").attr('selectValue'),
 					trainLevel:$(".update_policy .CtrainLevel").html(),
-					isChangeLevel:$(".update_policy .Ccheckbox").is(':checked'),
+					isChangeLevel:$(".update_policy .Ccheckbox").attr('checkedd'),
 					hotelLevel:$(".update_policy .ChotelTevel").html(),
 					hotelPrice:$(".update_policy .ChotelPrice").val(),
-					companyId:$scope.companyId
+					companyTd:$scope.company_id
 				})
 					.then(function(result){
 						Myalert("温馨提示","修改成功");
@@ -1143,7 +1143,7 @@ module.exports = (function(){
 			$(".CplaneLevel").html("不限");
 			$(".CplaneDiscount").html("不限").attr("selectValue","0");
 			$(".CtrainLevel").html("不限");
-			$(".Ccheckbox").attr('checked',true);
+			$(".Ccheckbox").attr('checkedd',1);
 			$(".Ccheckboxlabel").removeClass('lablefalse');
 			$(".Ccheckboxlabel").html('&#xe9ec;');
 			$(".ChotelTevel").html("不限");
@@ -1152,24 +1152,28 @@ module.exports = (function(){
 
 		//修改自定义复选框
 		$scope.updateChangecheck = function () {
-			if ($(".update_policy .Ccheckbox").is(':checked')==false) {
+			if ($(".update_policy .Ccheckbox").attr('checkedd')!=1) {
 				$(".update_policy .Ccheckboxlabel").removeClass('lablefalse');
 				$(".update_policy .Ccheckboxlabel").html('&#xe9ec;');
+				$(".update_policy .Ccheckbox").attr('checkedd',1);
 			}
 			else {
 				$(".update_policy .Ccheckboxlabel").addClass('lablefalse');
 				$(".update_policy .Ccheckboxlabel").html('');
+				$(".update_policy .Ccheckbox").attr('checkedd',0);
 			}
 		}
 		//创建自定义复选框
 		$scope.createChangecheck = function () {
-			if ($(".create_policy .Ccheckbox").is(':checked')==false) {
+			if ($(".create_policy .Ccheckbox").attr('checkedd')!=1) {
 				$(".create_policy .Ccheckboxlabel").removeClass('lablefalse');
 				$(".create_policy .Ccheckboxlabel").html('&#xe9ec;');
+				$(".create_policy .Ccheckbox").attr('checkedd',1);
 			}
 			else {
 				$(".create_policy .Ccheckboxlabel").addClass('lablefalse');
 				$(".create_policy .Ccheckboxlabel").html('');
+				$(".create_policy .Ccheckbox").attr('checkedd',0);
 			}
 		}
 
