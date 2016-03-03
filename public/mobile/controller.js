8f8a7cd733bbfd71587816c3bd62d344bef06ada
qp <qiuping.cao@tulingdao.com>
2016-03-03 16:26:50 +0800

    个人中心。

---
+++ public/mobile/controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by qp on 2016/3/3.
+ */
+'use strict';
+module.exports = (function() {
+    var user = {};
+
+    user.IndexController = function($scope) {
+        loading(true);
+        alert(111);
+    }
+    return user;
+})();
