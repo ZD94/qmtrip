77c2da92ef3a45b94aa69e530b7d802c909a2469
chenhao <hao.chen@tulingdao.com>
2016-03-09 16:39:36 +0800

    add invoicedetail

--- public/mobile/travelplan/controller.js
+++ public/mobile/travelplan/controller.js
@@ -34,7 +34,52 @@ var travelplan=(function(){
 
 
 
+    /*
+     行程单详细
+     * @param $scope
+     * @constructor
+     */
+    travelplan.InvoicedetailController = function($scope, $routeParams) {
+        loading(true);
+        $("title").html("票据详情");
+        var planId = $routeParams.planId;
+        $scope.planId = planId;
+        $scope.status = $routeParams.status;
+        $scope.invoiceId = $routeParams.invoiceId;
+        API.require("attachment");
+        API.onload(function() {
+            API.tripPlan.getTripPlanOrderById(planId)
+            .then(function(result){
+                var InvoiceDetail;
+                $scope.planDetail = result;
 
+                if ($scope.status=='outTraffic') {
+                    InvoiceDetail = result.outTraffic[0];
+                }
+                if ($scope.status=='backTraffic') {
+                    InvoiceDetail = result.backTraffic[0];
+                }
+                if ($scope.status=='hotel') {
+                    InvoiceDetail = result.hotel[0];
+                }
+                return InvoiceDetail;
+            })
+            .then(function(invoiceDetail) {
+                $scope.InvoiceDetail = invoiceDetail;
+                return  API.attachment.previewSelfImg({fileId: invoiceDetail.newInvoice})
+                .then(function(invoiceImg) {
+                    $scope.invoiceImg = invoiceImg;
+                    $scope.$apply();
+                })
+            })
+            .catch(function(err){
+                TLDAlert(err.msg || err);
+            })
+        })
+        $scope.goDetail = function () {
+            window.location.href = "#/travelPlan/PlanDetail?planId="+planId;
+        }
+    }
 
 
 
