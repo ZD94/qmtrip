043daf5bcc4064e4b8c5a8b37b9815200d815e9b
Ke Peng <ke.peng@tulingdao.com>
2016-03-28 17:06:17 +0800

    icon和symbol标签

--- public/script/nglibs/icon.js
+++ public/script/nglibs/icon.js
@@ -1,74 +1,111 @@
 "use strict";
 
+
 module.exports = function ($module){
+    var fonts = {
+        fa: 'fa fa-',
+        customs: {
+            symfont: 'web-icon-font3',
+            symcode: {
+                success: '\ue921',
+                consult: '\ue920',
+                cross: '\ue91F',
+                like: '\ue91E',
 
-    $module.directive("icon",function(){
+                hotel: '\ue914',
+                "arrow-down": '\ue913',
+                points: '\ue912',
+                train: '\ue911',
 
-        return {
-            template: "",
-            link: function( scope,elemmmm,attrs ){
-                console.log( scope,elemmmm,attrs );
-                console.log( attrs.name );
-                
-                var icons = {
-                    success: "&#xe921;",
-                    consult: "&#xe920;",
-                    cross: "&#xe91F;",
-                    like: "&#xe91E;",
+                help: '\ue903',
+                arrows: '\ue902',
+                info: '\ue901',
+                calendar: '\ue900',
 
-                    hotel: "&#xe914;",
-                    "arrow-down": "&#xe913;",
-                    points: "&#xe912;",
-                    train: "&#xe911;",
+                close: '\ue910',
+                suitcase: '\ue90F',
+                plane: '\ue90e',
+                gift: '\ue90c',
 
-                    help: "&#xe903;",
-                    arrows: "&#xe902;",
-                    info: "&#xe901;",
-                    calendar: "&#xe900;",
+                bulb: '\ue919',
+                exclaimation: '\ue918',
+                monitor: '\ue917',
+                home: '\ue916',
 
-                    close: "&#xe910;",
-                    suitcase: "&#xe90F;",
-                    plane: "&#xe90e;",
-                    gift: "&#xe90c;",
+                foot: '\ue907',
+                "arrow-up": '\ue906',
+                gear: '\ue905',
+                star: '\ue904',
 
-                    bulb: "&#xe919;",
-                    exclaimation: "&#xe918;",
-                    monitor: "&#xe917;",
-                    home: "&#xe916;",
+                checkbox: '\ue90d',
+                upload: '\ue915',
 
-                    foot: "&#xe907;",
-                    "arrow-up": "&#xe906;",
-                    gear: "&#xe905;",
-                    star: "&#xe904;",
+                employee: '\ue91d',
+                yuan: '\ue91c',
+                users: '\ue91b',
+                "new-points": '\ue91a',
 
-                    checkbox: "&#xe90d;",
-                    upload: "&#xe915;",
+                pin: '\ue90b',
+                export: '\ue90a',
+                error: '\ue909',
+                charts: '\ue908',
 
-                    employee: "&#xe91d;",
-                    yuan: "&#xe91c;",
-                    users: "&#xe91b;",
-                    "new-points": "&#xe91a;",
+                invoice: '\ue92c',
+                "small-yuan": '\ue934',
+                "small-exlaimation": '\ue914',
 
-                    pin: "&#xe90b;",
-                    export: "&#xe90a;",
-                    error: "&#xe909;",
-                    charts: "&#xe908;",
-
-                    invoice: "&#xe92c;",
-                    "small-yuan": "&#xe934;",
-                    "small-exlaimation": "&#xe914;",
-
-                    query: "&#xe92f;",
-                    "train-n-plane": "&#xe90c;"
-
-                }
-
-                var icon = icons[ attrs.name ];
-                
-                elemmmm.html( "<i class='icon web-icon-font3' style='font-size:inherit'>" + icon + "</i>" );
+                query: '\ue92f',
+                "train-n-plane": '\ue90c'
             }
         }
-        
+    };
+
+    var iconController = function($scope){
+        var symfont = 'customs';
+        var symname = $scope.name;
+        var m = $scope.name.match(/^([\w-]+)\.([\w-]+)$/);
+        if(m){
+            symfont = m[1];
+            symname = m[2];
+        }
+        var symconf = fonts[symfont];
+        if(typeof symconf == 'string'){
+            $scope.symfont = symconf;
+            $scope.symname = symname;
+            $scope.symcode = '';
+        } else {
+            $scope.symfont = symconf.symfont;
+            $scope.symname = '';
+            $scope.symcode = symconf.symcode[symname];
+        }
+    }
+
+    $module.directive("symbol",function(){
+        return {
+            template: '<i class="{{symfont}}{{symname}}">{{symcode}}</i>',
+            replace: true,
+            scope: {
+                name: '@'
+            },
+            controller: iconController
+        }
     });
+
+    $module.directive("icon",function(){
+        return {
+            template: '<div style="display: inline-block;text-align: center;"><i class="{{symfont}}{{symname}}">{{symcode}}</i></div>',
+            replace: true,
+            scope: {
+                name: '@'
+            },
+            link: function(scope, element, attrs){
+                element.css('font-size', element.width()/1.28594);
+                element.css('line-height', element.width()+'px');
+            },
+            controller: iconController
+        }
+
+    });
+
 
 }
