8f3b47868fac26eff36d819e9595e29aac3c1fc4
jian.zhang@tulingdao.com <jian.zhang@tulingdao.com>
2016-03-31 17:08:34 +0800

    '机票列表静态页'

---
+++ public/mobile/airticket/controller.js
@@ -0,0 +1,27 @@
+/**
+ * Created by ZLW on 2016/03/24.
+ */
+'use strict';
+var airTicket = (function () {
+
+    API.require('tripPlan');
+    API.require('auth');
+    API.require('attachment');
+    API.require('staff');
+    API.require('travelBudget');
+
+    var airTicket = {};
+
+    /*
+     机票订单详情页
+     * @param $scope
+     * @constructor
+     */
+    airTicket.FlightlistController = function ($scope) {
+        loading(true);
+
+    }
+    return airTicket;
+})();
+
+module.exports = airTicket;
