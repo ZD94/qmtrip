37933158a1d63c6254fc0ae0d054713d51edc85d
赵璐炜 <luwei.zhao@tulingdao.com>
2016-03-31 18:05:45 +0800

    update

--- public/mobile/travelplan/controller.js
+++ public/mobile/travelplan/controller.js
@@ -207,7 +207,7 @@ var travelplan = (function () {
                              TLDAlert(err.msg || err)
                              })
                              */
-                            $scope.$apply();
+                            //$scope.$apply();
                             loading(true);
                         }
                     )
@@ -317,7 +317,7 @@ var travelplan = (function () {
                                 ;
                             })();
 
-                            $scope.$apply();
+                            //$scope.$apply();
                             loading(true);
                         }
                     )
@@ -378,7 +378,7 @@ var travelplan = (function () {
                             })
                             .then(function (outTrafficBookListUrl) {
                                 $scope.URL.outTrafficBookListUrl = outTrafficBookListUrl;
-                                $scope.$apply();
+                                //$scope.$apply();
                                 window.location.href = $scope.URL.outTrafficBookListUrl;
                             })
                             .catch(function (err) {
@@ -400,7 +400,7 @@ var travelplan = (function () {
                         })
                         .then(function (backTrafficBookListUrl) {
                             $scope.URL.backTrafficBookListUrl = backTrafficBookListUrl;
-                            $scope.$apply();
+                            //$scope.$apply();
                             window.location.href = $scope.URL.backTrafficBookListUrl;
                         })
                         .catch(function (err) {
@@ -419,7 +419,7 @@ var travelplan = (function () {
                         })
                         .then(function (r) {
                             $scope.URL.hotelBookListUrl = r;
-                            $scope.$apply();
+                            //$scope.$apply();
                             console.log($scope.URL.hotelBookListUrl);
                             window.location.href = $scope.URL.hotelBookListUrl;
                         })
@@ -452,7 +452,7 @@ var travelplan = (function () {
                             API.tripPlan.commitTripPlanOrder($scope.ITEM.id)
                                 .then(function (result) {
                                     $scope.getData($routeParams.orderId);
-                                    $scope.$apply();
+                                    //$scope.$apply();
                                     msgbox.log("提交审核成功");
                                 })
                                 .catch(function (err) {
@@ -512,7 +512,7 @@ var travelplan = (function () {
                     return API.attachment.previewSelfImg({fileId: invoiceDetail.newInvoice})
                         .then(function (invoiceImg) {
                             $scope.invoiceImg = invoiceImg;
-                            $scope.$apply();
+                            //$scope.$apply();
                             loading(true);
                         })
                 })
