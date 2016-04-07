31ad833136f639126bac340486fb3cded538c59f
chenhao <hao.chen@tulingdao.com>
2016-04-07 18:30:31 +0800

    delete note

--- public/script/nglibs/filters/currency_point.ts
+++ public/script/nglibs/filters/currency_point.ts
@@ -32,35 +32,6 @@ export = function($module){
 					}
 				}
 			}
-			// if(typeof value == 'number'){
-	  //           var floatnum = parseFloat(value);
-	  //           var intval = parseInt(value);
-			// 	var intvalue = Math.floor((floatnum - intval) * 100);
-			// 	var stringn = intvalue.toString();
-			// 	var length = stringn.length;
-	  //           var i =0;
-			// 	while (stringn.length < 3) {
-			// 		stringn = '0' + stringn;
-			// 		i++;
-			// 	}
-			// 	var point;
-	  //           if(i == 1 && stringn.substr(-1) == "0"){
-			// 		point = stringn.substr(length - i, 1);
-			// 		value = '￥' + intval + '<i>.' + point + '</i>';
-	  //           }else{
-	  //               point = stringn.substr(length-i);
-			// 		if (point == 0) {
-			// 			value = '￥' + intval;
-			// 			return value;
-			// 		} else {
-			// 			value = '￥' + intval + '<i>.' + point + '</i>';
-			// 			return value;
-			// 		}
-	  //           }
-			// }else{
-			// 	value = typeof value;
-				
-			// }
 		}
 		
 	})
