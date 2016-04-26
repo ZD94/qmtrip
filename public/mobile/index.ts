357e7c27f9931499dae0b80f562fc8c17310472a
Ke Peng <ke.peng@tulingdao.com>
2016-04-26 16:41:32 +0800

    mobile中增加ionic测试页

--- public/mobile/index.ts
+++ public/mobile/index.ts
@@ -34,7 +34,33 @@ ngapp.depend('qm.common');
 ngapp.depend('qm.model');
 ngapp.root('mobile', '/usercenter/index');
 ngapp.initializer(require('nglibs'));
+ngapp.useRoutePolicy(ngapp.RoutePolicy.None);
+ngapp.routePushEmbed('tab');
+ngapp.routeNamedEmbed('tab-dash');
+ngapp.routeNamedEmbed('tab-chats');
+ngapp.routePopEmbed();
+ngapp.routeAddSingle('');
 var app = ngapp.create('qm.mobile');
+app.config(function($ionicConfigProvider){
+    $ionicConfigProvider.views.maxCache(0);
+});
+app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins['Keyboard']) {
+            let Keyboard = cordova.plugins['Keyboard'];
+            Keyboard.hideKeyboardAccessoryBar(true);
+            Keyboard.disableScroll(true);
+
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+});
+
 
 var dyload = require('dyload');
 
