fa7cd658773706f0f74b109997deb38a9832013d
陈浩 <seven@chenhaoMacBook-Pro.local>
2016-05-09 19:10:11 +0800

    补充添加controller

--- public/ionic/company/controller.ts
+++ public/ionic/company/controller.ts
@@ -11,6 +11,14 @@ export function BudgetController($scope){
 
 }
 
+export function RecordController($scope){
+
+}
+
+export function DistributionController($scope){
+
+}
+
 export function DepartmentController($scope){
 
 }
