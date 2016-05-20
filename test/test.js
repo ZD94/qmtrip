9f92dfcbb111815cecaf54e6025132cd984d436a
Ke Peng <ke.peng@tulingdao.com>
2016-05-20 17:15:11 +0800

    mocha测试中处理zone

--- test/test.js
+++ test/test.js
@@ -6,12 +6,18 @@ var path = require('path');
 process.env.NODE_PATH = '.:'+process.env.NODE_PATH;
 
 require('app-module-path').addPath(path.normalize(path.join(__dirname, '..')));
-require('common/zone');
+var zone = require('common/zone');
 require('common/typescript');
 
 global.Promise = require('bluebird');
-Promise.config({ longStackTraces: true });
+Promise.config({ longStackTraces: false });
 Promise.promisifyAll(require('fs'));
+
+process.on('unhandledRejection', (reason, p) => {
+    throw reason;
+});
+
+require('./mocha-zone')(global);
 
 var config = require("../config");
 
@@ -31,12 +37,13 @@ var API = require('common/api');
 var model = require('common/model');
 model.init(config.postgres.url);
 
-
-API.init(path.join(__dirname, '../api'), config.api)
-    .then(API.loadTests.bind(API))
-    .then(run)
-    .catch(function(e){
-        logger.error(e.stack?e.stack:e);
-        console.error(e.stack?e.stack:e);
-        process.exit();
-    });
+zone.run(function(){
+    return API.init(path.join(__dirname, '../api'), config.api)
+        .then(API.loadTests.bind(API))
+        .then(run)
+        .catch(function(e){
+            logger.error(e.stack?e.stack:e);
+            console.error(e.stack?e.stack:e);
+            process.exit();
+        });
+})
