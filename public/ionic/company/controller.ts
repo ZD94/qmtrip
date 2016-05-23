fab95753626e6152bdd6dcf7ac66822093e1d832
chenhao <hao.chen@jingli.tech>
2016-05-23 21:30:10 +0800

    list detail

--- public/ionic/company/controller.ts
+++ public/ionic/company/controller.ts
@@ -6,7 +6,7 @@ import {EStaffRole, Staff} from "api/_types/staff";
 import {TravelPolicy} from "api/_types/travelPolicy";
 import {Department} from "api/_types/department";
 
-export async function ManagementController($scope,Models){
+export async function ManagementController($scope, Models) {
     var staff = await Staff.getCurrent();
     var company = staff.company;
     var [staffs, policies,departments] = await Promise.all([
@@ -19,53 +19,50 @@ export async function ManagementController($scope,Models){
     $scope.policiesnum = policies.length;
 }
 
-export async function BudgetController($scope){
+export async function BudgetController($scope) {
 
 }
 
-export async function RecordController($scope){
+export async function RecordController($scope) {
 
 }
 
-export async function DistributionController($scope){
+export async function DistributionController($scope) {
 
 }
 
-export async function DepartmentController($scope, Models, $ionicPopup){
+export async function DepartmentController($scope, Models, $ionicPopup ,$route) {
     var staff = await Staff.getCurrent();
-    var initdepartment = async function(){
-        var departments = await staff.company.getDepartments();
-        $scope.departments = departments.map(function(department){
-            var depart = {department:department,staffnum:0};
-            return depart;
-        });
-        await Promise.all($scope.departments.map(async function(depart){
-            console.info(depart);
-            var result = await depart.department.getStaffs();
-            depart.staffnum = result.length;
-            return depart;
-        }));
-    };
-    initdepartment();
+    var departments = await staff.company.getDepartments();
+    $scope.departments = departments.map(function (department) {
+        var depart = {department: department, staffnum: 0};
+        return depart;
+    });
+    await Promise.all($scope.departments.map(async function (depart) {
+        console.info(depart);
+        var result = await depart.department.getStaffs();
+        depart.staffnum = result.length;
+        return depart;
+    }));
     var newdepartment = $scope.newdepartment = Department.create();
-    $scope.newdepart = function(){
+    $scope.newdepart = function () {
         var nshow = $ionicPopup.show({
             template: '<input type="text" ng-model="newdepartment.name">',
-            title:'创建部门',
-            scope:$scope,
-            buttons:[
+            title: '创建部门',
+            scope: $scope,
+            buttons: [
                 {
-                    text:'取消'
+                    text: '取消'
                 },
                 {
                     text: '保存',
-                    type:'button-positive',
-                    onTap:function(e){
-                        if(!$scope.newdepartment.name){
+                    type: 'button-positive',
+                    onTap: function (e) {
+                        if (!$scope.newdepartment.name) {
                             e.preventDefault();
-                        }else{
+                        } else {
                             $scope.newdepartment.save();
-                            initdepartment();
+                            $route.reload();
                         }
                     }
                 }
@@ -74,27 +71,27 @@ export async function DepartmentController($scope, Models, $ionicPopup){
     }
 }
 
-export async function StaffsController($scope, Models){
+export async function StaffsController($scope, Models) {
     var staff = await Staff.getCurrent();
     var staffs = await staff.company.getStaffs();
-    $scope.staffs = staffs.map(function(staff){
-        var obj = {staff:staff,role:""};
-        if(obj.staff.roleId == EStaffRole.OWNER ){
+    $scope.staffs = staffs.map(function (staff) {
+        var obj = {staff: staff, role: ""};
+        if (obj.staff.roleId == EStaffRole.OWNER) {
             obj.role = '创建者';
         }
         return obj;
     });
     console.info(staffs);
-    await Promise.all($scope.staffs.map(async function(obj){
+    await Promise.all($scope.staffs.map(async function (obj) {
         obj.travelPolicy = await obj.staff.getTravelPolicy();
         return obj;
     }));
-    $scope.search = function(){
-        
+    $scope.search = function () {
+
     }
 }
 
-export async function StaffdetailController($scope, $stateParams, Models, $ionicHistory){
+export async function StaffdetailController($scope, $stateParams, Models, $ionicHistory) {
     //使下一个面变为根目录可呼出menu
     $ionicHistory.nextViewOptions({
         historyRoot: true,
@@ -105,72 +102,73 @@ export async function StaffdetailController($scope, $stateParams, Models, $ionic
     let staff;
     var currentstaff = await Staff.getCurrent();
     var company = currentstaff.company;
-    if($stateParams.staffId){
+    if ($stateParams.staffId) {
         staff = await Models.staff.get($stateParams.staffId);
-    }else{
+    } else {
         staff = Staff.create();
         staff.company = company;
     }
     $scope.staff = staff;
-    var role={id:false};
-    if(staff.roleId == EStaffRole.OWNER || staff.roleId == EStaffRole.ADMIN){
+    var role = {id: false};
+    if (staff.roleId == EStaffRole.OWNER || staff.roleId == EStaffRole.ADMIN) {
         role.id = true;
     }
     $scope.role = role;
+    console.info(role);
+    console.info(staff.roleId);
 
     $scope.travelpolicylist = await company.getTravelPolicies();
-    console.info($scope.travelpolicylist);
-    console.info($scope.staff.travelPolicyId);
-    // $scope.departmentlist = await company.department.get(companyId);
-    $scope.savestaff = function(){
+    $scope.departmentlist = await company.getDepartments();
+    console.info($scope.departmentlist);
+    $scope.savestaff = function () {
         let _staff = $scope.staff;
         if (_staff.travelPolicyId && _staff.travelPolicyId.id) {
             _staff.travelPolicyId = _staff.travelPolicyId.id;
         }
-
-        if(_staff.role  && _staff.role.id ==true){
+        console.info(role, $scope.role);
+        if ($scope.role && $scope.role.id == true) {
             _staff.roleId = EStaffRole.ADMIN;
-        }else{
+        } else {
             _staff.roleId = EStaffRole.COMMON;
         }
         _staff.save();
         $ionicHistory.goBack(-1);
     }
-    $scope.showrole = function(){
-        if($scope.role.id ==true){
+    $scope.showrole = function () {
+        if ($scope.role.id == true) {
             $scope.staff.roleId = EStaffRole.ADMIN;
-        }else{
+        } else {
             $scope.staff.roleId = EStaffRole.COMMON;
         }
-        console.info($scope.role);
-        console.info($scope.staff);
-        console.info($scope.staff.travelPolicyId);
-        // $ionicHistory.goBack(-1);
+        // console.info($scope.role);
+        // console.info($scope.staff);
+        // console.info($scope.staff.travelPolicyId);
+        $ionicHistory.goBack(-1);
     }
 }
 
-export async function TravelpolicyController($scope , Models, $location){
+export async function TravelpolicyController($scope, Models, $location) {
     var staff = await Staff.getCurrent();
     var company = await staff.company;
     var travelPolicies = await company.getTravelPolicies();
     $scope.travelPolicies = travelPolicies.map(function (policy) {
-        var obj = {policy:policy,usernum:''};
+        var obj = {policy: policy, usernum: ''};
         return obj;
     })
-    await Promise.all($scope.travelPolicies.map(async function(obj){
+    await Promise.all($scope.travelPolicies.map(async function (obj) {
         var result = await obj.policy.getStaffs();
         obj.usernum = result.length;
         return obj;
     }))
     console.info($scope.travelPolicies);
-    $scope.editpolicy = async function(id){
+    $scope.editpolicy = async function (id) {
         var travelpolicy = await Models.travelPolicy.get(id);
-        $location.path('/company/editpolicy').search({'policyId':id}).replace();
+        $location.path('/company/editpolicy').search({'policyId': id}).replace();
         console.info(travelpolicy);
     }
 }
 
-export async function EditpolicyController($scope, Models, $stateParams, $ionicHistory){
+export async function EditpolicyController($scope, Models, $stateParams, $ionicHistory) {
     //使下一个面变为根目录可呼出menu
     $ionicHistory.nextViewOptions({
         historyRoot: true,
@@ -178,18 +176,18 @@ export async function EditpolicyController($scope, Models, $stateParams, $ionicH
         expire: 300
     });
     var discounts = $scope.discounts = [
-        { value:0, text:'不限' },
-        { value:9, text:'9折及以下' },
-        { value:8, text:'8折及以下' },
-        { value:7, text:'7折及以下' },
-        { value:6, text:'6折及以下' }
+        {value: 0, text: '不限'},
+        {value: 9, text: '9折及以下'},
+        {value: 8, text: '8折及以下'},
+        {value: 7, text: '7折及以下'},
+        {value: 6, text: '6折及以下'}
     ]
     var staff = await Staff.getCurrent();
     var travelPolicy;
-    if($stateParams.policyId){
+    if ($stateParams.policyId) {
         console.info($stateParams);
         travelPolicy = await Models.travelPolicy.get($stateParams.policyId)
-    }else{
+    } else {
         travelPolicy = TravelPolicy.create();
         travelPolicy.companyId = staff.company.id;
         travelPolicy.planeLevel = '不限';
@@ -199,12 +197,12 @@ export async function EditpolicyController($scope, Models, $stateParams, $ionicH
     }
     console.info(travelPolicy);
     $scope.travelPolicy = travelPolicy;
-    $scope.savePolicy = async function(){
+    $scope.savePolicy = async function () {
         console.info($scope.travelPolicy);
         await $scope.travelPolicy.save();
         $ionicHistory.goBack(-1);
     }
-    $scope.consoles = function(obj){
+    $scope.consoles = function (obj) {
         console.info("aaaaaaa");
         console.info(obj);
     }
