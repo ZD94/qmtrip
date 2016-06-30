5e9df1cf2f524688ad15624e28b1aef326bc345e
Ke Peng <ke.peng@tulingdao.com>
2016-06-30 19:34:41 +0800

    nglibs的Models不需要$resolve()

--- nglibs/models/index.ts
+++ nglibs/models/index.ts
@@ -144,12 +144,6 @@ class ClientModels implements ModelsInterface {
         this.seed = createService<Seed>(Services.seed, $cacheFactory);
         initModels(this);
     }
-
-    async $resolve() : Promise<this> {
-        API.require('staff');
-        await API.onload();
-        return this;
-    }
 }
 
 import './menu';
