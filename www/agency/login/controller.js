3a15c60fdac47d586237beb279c0655ab7636d4c
Ke Peng <ke.peng@tulingdao.com>
2016-07-04 15:41:08 +0800

    代码格式化

--- www/agency/login/controller.js
+++ www/agency/login/controller.js
@@ -1,6 +1,5 @@
-
 'use strict';
-var login=(function(){
+var login = (function () {
     API.require('auth');
     API.require("checkcode");
     var login = {};
@@ -9,37 +8,37 @@ var login=(function(){
     login.LoginController = function ($scope, $stateParams) {
 
         var backUrl = $stateParams.backurl || "#";
-        $scope.checkLogin = function() {
+        $scope.checkLogin = function () {
             var name = $('#agencyName').val();
-            var pwd  = $('#agencyPwd').val();
+            var pwd = $('#agencyPwd').val();
             var commit = true;
-            if(commit){
-                if(!name){
+            if (commit) {
+                if (!name) {
                     alert("用户名不能为空！");
                     return false;
-                }else if(!pwd){
+                } else if (!pwd) {
                     alert("登录密码不能为空!");
                     return false;
                 }
 
-                API.onload(function(){
+                API.onload(function () {
                     console.info(API)
-                    API.auth.login({email:name,pwd:pwd, type: 2})
-                        .then(function(data){
-                            Cookie.set("agent_id", data.user_id, { expires:30 });
-                            Cookie.set("agent_token_id", data.token_id, { expires:30 });
-                            Cookie.set("agent_token_sign", data.token_sign, { expires:30 });
-                            Cookie.set("agent_token_timestamp", data.timestamp, { expires:30 });
+                    API.auth.login({email: name, pwd: pwd, type: 2})
+                        .then(function (data) {
+                            Cookie.set("agent_id", data.user_id, {expires: 30});
+                            Cookie.set("agent_token_id", data.token_id, {expires: 30});
+                            Cookie.set("agent_token_sign", data.token_sign, {expires: 30});
+                            Cookie.set("agent_token_timestamp", data.timestamp, {expires: 30});
                             alert("登录成功");
                             API.reload_all_modules();
-                            window.location.href= backUrl;
-                        }).catch(function(err){
-                            if (err.msg) {
-                                TLDAlert(err.msg);
-                            } else {
-                                TLDAlert(err);
-                            }
-                        }).done();
+                            window.location.href = backUrl;
+                        }).catch(function (err) {
+                        if (err.msg) {
+                            TLDAlert(err.msg);
+                        } else {
+                            TLDAlert(err);
+                        }
+                    }).done();
                 })
             }
         }
