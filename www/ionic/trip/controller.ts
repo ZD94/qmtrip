a7b7553ebd7b03a8c6e0d06a539b7b2d549cb6e7
wanglihui <lihui.wang@jingli.tech>
2016-07-08 12:52:44 +0800

    预算申请后跳转详情页修改

--- www/ionic/trip/controller.ts
+++ www/ionic/trip/controller.ts
@@ -343,7 +343,8 @@ export async function CommittedController($scope, $stateParams, Models){
     $scope.tripPlan = tripPlan.target;
 
     $scope.goToDetail = function() {
-        window.location.href = '#/trip/detail?id='+id;
+        window.location.href = '#/trip/list-detail?tripid='+id;
+        // window.location.href = '#/trip/detail?id='+id;
     }
 }
 
