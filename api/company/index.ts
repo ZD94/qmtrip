a1c0ce380fdb7f1d128b469fc2daf2050b418781
miaomiao <1014418876@qq.com>
2016-07-12 19:03:50 +0800

    企业注册API，做邮箱、域名和手机号限制

--- api/company/index.ts
+++ api/company/index.ts
@@ -69,6 +69,12 @@ class CompanyModule {
             throw {code: -6, msg: "邮箱格式不符合要求"};
         }
 
+        let companies = await Models.company.find({where: {$or: [{email: params.email}, {mobile: params.mobile}, {domain_name: domain}]}});
+
+        if(companies && companies.length > 0) {
+            throw {code: -7, msg: '邮箱或手机号已经注册'};
+        }
+
         if(session && session.accountId) {
             let agencyUser = await Models.agencyUser.get(session.accountId);
 
