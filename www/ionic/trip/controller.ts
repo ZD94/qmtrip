5a536f13a671ca42bcdf7175f53e7ce5bc6ecf91
wanglihui <lihui.wang@jingli.tech>
2016-08-19 15:42:16 +0800

    修改住宿,补助天数计算问题

--- www/ionic/trip/controller.ts
+++ www/ionic/trip/controller.ts
@@ -325,7 +325,7 @@ export async function BudgetController($scope, $storage, Models, $stateParams, $
     }
 
     $scope.totalPrice = totalPrice;
-    let duringDays = moment(trip.endDate).diff(moment(trip.beginDate), 'days') + 1;
+    let duringDays = moment(trip.endDate).diff(moment(trip.beginDate), 'days');
     $scope.duringDays = duringDays;
     $scope.budgets = budgets;
     $scope.EInvoiceType = EInvoiceType;
