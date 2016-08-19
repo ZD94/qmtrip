2049cc9d134719f7a351e278d24055a358a90800
Ke Peng <ke.peng@jingli.tech>
2016-08-19 18:18:19 +0800

    优化angular app启动代码

--- www/ionic/index.ts
+++ www/ionic/index.ts
@@ -2,35 +2,6 @@
 import * as L from 'common/language';
 import { getSession } from 'common/model';
 require('ionic');
-var API = require('common/api');
-API.require('auth');
-
-var browserspec = require('browserspec');
-// browserspec.enum_wechat();
-
-var Cookie = require('tiny-cookie');
-var API = require('common/api');
-API.onlogin(async function(){
-    var backUrl = window.location.href;
-
-    if(/^http\:\/\/.*\/(index.html)?\#\/login\/(index)?\?backurl\=.*/.test(backUrl)) {
-        console.info("login page...");
-        window.location.reload();
-        return;
-    }
-
-    if(browserspec.is_wechat && /.*jingli365\.com/.test(window.location.host)){
-        if(!/.*wxauthcode\=\w*\&wxauthstate\=.*/.test(backUrl)) {
-            await API.onload();
-            let url = await API.auth.getWeChatLoginUrl({redirectUrl: backUrl});
-            window.location.href = url;
-            return;
-        }
-    }else {
-        backUrl = encodeURIComponent(backUrl);
-        window.location.href = "#/login/?backurl="+backUrl;
-    }
-});
 
 function getAuthData() {
     return localStorage.getItem('auth_data');
@@ -38,42 +9,127 @@ function getAuthData() {
 
 window['getAuthDataStr'] = function() {
     let authData: any = getAuthData();
-    if (typeof authData == 'string') {
+    if(typeof authData == 'string') {
         authData = JSON.parse(authData);
     }
     let strs: any = [];
     for(var k in authData) {
-        strs.push(k+'='+authData[k]);
+        strs.push(k + '=' + authData[k]);
     }
     strs = strs.join("&");
     return strs;
 }
 
-API.authenticate = function(remote, callback){
+function apiAuth(remote, callback) {
     var datastr = getAuthData();
-    if(!datastr){
+    if(!datastr) {
         return callback(L.ERR.NEED_LOGIN, remote);
     }
     var data;
-    try{
+    try {
         data = JSON.parse(datastr);
-    }catch(e){
+    } catch(e) {
         return callback(e, remote);
     }
-    remote.authenticate({ accountid: data.user_id, tokenid: data.token_id, timestamp: data.timestamp, tokensign: data.token_sign },
-            function(err, handle){
-                if(!err){
-                    var session = getSession();
-                    session.accountId = data.user_id;
-                    session.token = {
-                        id: data.token_id,
-                        sign: data.token_sign,
-                        timestamp: data.timestamp
-                    };
-                }
-                callback(err, handle);
+    remote.authenticate({
+            accountid: data.user_id,
+            tokenid: data.token_id,
+            timestamp: data.timestamp,
+            tokensign: data.token_sign
+        },
+        function(err, handle) {
+            if(!err) {
+                var session = getSession();
+                session.accountId = data.user_id;
+                session.token = {
+                    id: data.token_id,
+                    sign: data.token_sign,
+                    timestamp: data.timestamp
+                };
+            }
+            callback(err, handle);
+        });
+}
+
+function initAPI($window, $location){
+    var API = require('common/api');
+    API.require('auth');
+    API.authenticate = apiAuth;
+    API.onlogin(gotoLogin);
+
+    if(/^\/login\//.test($location.path()))
+        return;
+    var datastr = getAuthData();
+    if(!datastr) {
+        gotoLogin();
+    }
+
+    async function gotoLogin() {
+        var backUrl = $location.absUrl();
+
+        if(/^\/login\//.test($location.path())) {
+            console.info("login page...");
+            window.location.reload();
+            return;
+        }
+
+        var API = require('common/api');
+        var browserspec = require('browserspec');
+        if(browserspec.is_wechat && /^[tj]\.jingli365\.com$/.test($location.host())) {
+            let args = $location.search();
+            if(!args.wxauthcode || !args.wxauthstate) {
+                await API.onload();
+                let url = await API.auth.getWeChatLoginUrl({redirectUrl: backUrl});
+                $window.location.href = url;
+                return;
+            }
+        }
+        $location.path('/login/').search('backurl', backUrl);
+    }
+}
+
+function initKeyboard($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins && window.cordova.plugins['Keyboard']) {
+            let Keyboard = cordova.plugins['Keyboard'];
+            Keyboard.hideKeyboardAccessoryBar(true);
+            Keyboard.disableScroll(true);
+        }
+    });
+}
+function initStatusBar($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        if(window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+}
+function initUpdater($ionicPlatform, $ionicPopup) {
+    $ionicPlatform.ready(function() {
+        window['bundle_url'] = cordova.file.applicationDirectory + 'www/';
+        var el = $('<script manifest="' + window['bundle_url'] + 'manifest.json" timeout="10000"></script>');
+        $('head').append(el);
+        var dyload = require('dyload');
+        dyload('script/libs/bundle.update.js')
+            .then(function() {
+                var updater = require('common/client/updater');
+                updater.initUpdater(function(appLoader) {
+                    $ionicPopup.confirm({
+                        title: '有更新',
+                        template: '是否更新?'
+                    }).then(function(value) {
+                        if(!value)
+                            return;
+                        location.href = window['bundle_url'] + 'update.html';
+                    })
+                })
             });
-};
+    })
+}
+
 require("angular-chart.js")
 require('nglibs');
 var ngapp = require('ngapp');
@@ -89,48 +145,9 @@ var app = ngapp.create('qm.ionic');
 app.config(function($ionicConfigProvider){
     $ionicConfigProvider.views.maxCache(0);
 });
-app.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins['Keyboard']) {
-            let Keyboard = cordova.plugins['Keyboard'];
-            Keyboard.hideKeyboardAccessoryBar(true);
-            Keyboard.disableScroll(true);
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
-    });
-});
-
+app.run(initAPI);
+app.run(initKeyboard);
+app.run(initStatusBar);
 if(window.cordova) {
-    app.run(function($ionicPlatform, $ionicPopup) {
-        $ionicPlatform.ready(function(){
-            window['bundle_url'] = cordova.file.applicationDirectory + 'www/';
-            var el = $('<script manifest="' + window['bundle_url'] + 'manifest.json" timeout="10000"></script>');
-            $('head').append(el);
-            dyload('script/libs/bundle.update.js')
-                .then(function() {
-                    var updater = require('common/client/updater');
-                    updater.initUpdater(function(appLoader) {
-                        $ionicPopup.confirm({
-                            title: '有更新',
-                            template: '是否更新?'
-                        }).then(function(value) {
-                            if(!value)
-                                return;
-                            location.href = window['bundle_url'] + 'update.html';
-                        })
-                    })
-                });
-        })
-    });
-}
-
-var dyload = require('dyload');
-
-if(browserspec.is_wechat) {
-    dyload("http://res.wx.qq.com/open/js/jweixin-1.0.0.js")
+    app.run(initUpdater);
 }
