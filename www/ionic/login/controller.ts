267a2074e3be50d72496b606a89589c36ce6d5b1
wanglihui <lihui.wang@jingli.tech>
2016-08-26 16:58:42 +0800

    删除无效代码

--- www/ionic/login/controller.ts
+++ www/ionic/login/controller.ts
@@ -472,24 +472,6 @@ export async function FirstSetPwdController ($scope, $stateParams) {
     }
 }
 
-//企业注册页面  sc
-export async function CompanyGuideController ($scope){
-    require("./company-guide.scss");
-}
-
-export async function CompanyFirstController ($scope){
-    require("./company-guide.scss");
-}
-
-export async function CompanySecondController ($scope){
-    require("./company-guide.scss");
-}
-
-export async function CompanyGuideSuccessController ($scope){
-    require("./company-guide.scss");
-}
-
-
 function trim(s) {
     if (!s) return s;
     s = s.replace(/\s+/g, "");
