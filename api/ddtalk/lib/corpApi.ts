0ca0fe07eeede8ec31bdfc02acb5a1aba2cc0b25
wanglihui <lihui.wang@jingli.tech>
2016-09-14 09:51:05 +0800

    实现钉钉企业相关API

--- api/ddtalk/lib/corpApi.ts
+++ api/ddtalk/lib/corpApi.ts
@@ -5,10 +5,19 @@
 'use strict';
 import {CorpAccessToken} from "./interface";
 import {reqProxy} from "./reqProxy";
+import RedisCache = require('./redisCache');
+
+interface CorpTicket {
+    ticket: string;
+    expire_at: number;
+}
 
 class CorpApi {
 
-    constructor(public accessToken: CorpAccessToken) {
+    private cache: RedisCache;
+
+    constructor(public corpid: string, public accessToken: CorpAccessToken) {
+        this.cache = new RedisCache();
     }
 
     async departmentList() {
@@ -36,6 +45,37 @@ class CorpApi {
             },
             method: 'GET'
         });
+    }
+
+    async getTicket() : Promise<CorpTicket> {
+        let key = `corp_ticket_token:${this.corpid}`;
+        let self = this;
+        let ticketObj: any = await self.cache.get(key)
+        if (ticketObj && ticketObj.expire_at > Date.now()) {
+            return ticketObj;
+        }
+        let url = `https://oapi.dingtalk.com/get_jsapi_ticket?access_token=${this.accessToken.access_token}`
+        return reqProxy(url, {
+            name: '获取ticket',
+            method: 'GET'
+        }).then( async (ret: any) => {
+            if (ret.errcode) throw new Error(ret);
+            let expire_at = Date.now() + (ret.expires_in - 30) * 1000;
+            ticketObj = {ticket: ret.ticket, expire_at: expire_at}
+            await self.cache.set(key, ticketObj);
+            return ticketObj;
+        })
+    }
+
+    async getUserInfoByOAuth(code) : any {
+        let url = `https://oapi.dingtalk.com/user/getuserinfo?access_token=${this.accessToken.access_token}&code=${code}`;
+        return reqProxy(url, {
+            name: '通过code换取用户信息',
+            method: 'GET'
+        }).then( async (ret: any) => {
+            if (ret.errrcode) throw new Error(ret);
+            return {userId: ret.userid, deviceId: ret.deviceId, isSys: ret.is_sys, sysLevel: ret.sys_level}
+        })
     }
 }
 
