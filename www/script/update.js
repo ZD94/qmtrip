ca438bdc97ca911526e7b72bb298108ac3cbc72b
Ke Peng <ke.peng@jingli.tech>
2016-09-22 01:03:30 +0800

    ionic app: 如果安装前卸载有残留导致可能导致更新页卡死

--- www/script/update.js
+++ www/script/update.js
@@ -3,26 +3,34 @@
 var $ = require('jquery');
 var updater = require('common/client/updater');
 
-function setupAutoUpdate(){
-    updater.initUpdater(function (appLoader) {
-        var msg = $('.initial-loading .loading .message');
-        msg.text('有更新, 下载中...');
-        var progress = $('.initial-loading .loading .progress .progress-bar');
-        function onProgress(ev) {
-            progress.css('width', (ev.percentage * 100) + '%');
-        }
-        return appLoader.download(onProgress)
-            .then(function () {
-                return appLoader.update();
-            })
-            .catch(function (err) {
-                msg.text('下载出错: ', err);
-                alert(err);
-                location.reload();
-            });
-    }, function(){
-        //if(confirm('跳转到index.html?'))
-        location.href = window.Manifest.root+'index.html';
-    });
-}
 document.addEventListener('deviceready', setupAutoUpdate, false);
+
+function setupAutoUpdate(){
+    navigator.splashscreen.hide();
+    updater.initUpdater(onUpdate, onUpdated, true);
+}
+
+function onUpdate(appLoader) {
+    var msg = $('.initial-loading .loading .message');
+    msg.text('有更新, 下载中...');
+    var progress = $('.initial-loading .loading .progress .progress-bar');
+
+    function onProgress(ev) {
+        progress.css('width', (ev.percentage * 100) + '%');
+    }
+
+    return appLoader.download(onProgress)
+        .then(function () {
+            return appLoader.update();
+        })
+        .catch(function (err) {
+            msg.text('下载出错: ', err);
+            alert(err);
+            location.reload();
+        });
+}
+
+function onUpdated(){
+    //if(confirm('跳转到index.html?'))
+    location.href = window.Manifest.root+'index.html';
+}
