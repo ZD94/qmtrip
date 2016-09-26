95c76db26b72a82ae0ca60ef347a14f48b39d572
wangyali <yali.wang@jingli.com>
2016-09-26 11:43:55 +0800

    修改飞机价格平方打分

--- www/ionic/login/storage-set.ts
+++ www/ionic/login/storage-set.ts
@@ -1,4 +1,4 @@
-export function StorageSetController($scope, $stateParams, $storage) {
+export function StorageSetController($scope, $stateParams, $storage, $cookies) {
     let token_id = $stateParams.token_id;
     let user_id = $stateParams.user_id;
     let token_sign = $stateParams.token_sign;
@@ -10,10 +10,15 @@ export function StorageSetController($scope, $stateParams, $storage) {
         timestamp: timestamp };
     $storage.local.set('auth_data', data);
     //服务器端无法读取storage
-    Cookie.set("user_id", data.user_id, {expires: 30});
+    /*Cookie.set("user_id", data.user_id, {expires: 30});
     Cookie.set("token_sign", data.token_sign, {expires: 30});
     Cookie.set("timestamp", data.timestamp, {expires: 30});
-    Cookie.set("token_id", data.token_id, {expires: 30});
+    Cookie.set("token_id", data.token_id, {expires: 30});*/
+    var expires = new Date(Date.now() + 30*24*3600*1000);
+    $cookies.put("user_id", data.user_id, {expires});
+    $cookies.put("token_sign", data.token_sign, {expires});
+    $cookies.put("timestamp", data.timestamp, {expires});
+    $cookies.put("token_id", data.token_id, {expires});
     API.reload_all_modules();
     window.location.href = back_url;
 }
