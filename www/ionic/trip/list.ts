19a087f94e104d788b81420666be208d837355ad
chenhao <hao.chen@jingli.tech>
2016-09-28 12:30:49 +0800

    我的行程添加筛选功能，审批页面添加已通过状态

--- www/ionic/trip/list.ts
+++ www/ionic/trip/list.ts
@@ -1,5 +1,6 @@
 import { EPlanStatus } from 'api/_types/tripPlan';
 import { Staff } from 'api/_types/staff/staff';
+import indexOf = require("lodash/indexOf");
 export async function ListController($scope , $stateParams, Models){
     var staff = await Staff.getCurrent();
     let statusTxt = {};
@@ -20,8 +21,7 @@ export async function ListController($scope , $stateParams, Models){
             EPlanStatus.WAIT_UPLOAD,
             EPlanStatus.WAIT_COMMIT,
             EPlanStatus.AUDIT_NOT_PASS,
-            EPlanStatus.COMPLETE,
-            EPlanStatus.NO_BUDGET,
+            // EPlanStatus.COMPLETE,
             EPlanStatus.AUDITING
         ];
     }
@@ -35,14 +35,19 @@ export async function ListController($scope , $stateParams, Models){
         limit: 5,
         where: where
     });
-    loadTripPlan(pager);
+    // loadTripPlan(pager);
     var vm = {
         hasNextPage: function() {
             return pager.totalPages-1 > pager.curPage;
         },
         nextPage : async function() {
             try {
+                let newArr =[];
+                let join = {};
                 pager = await pager.nextPage();
+                pager.map(function(item){
+                    $scope.tripPlans.push(item);
+                })
             } catch(err) {
                 alert("获取数据时,发生异常");
                 return;
@@ -53,7 +58,34 @@ export async function ListController($scope , $stateParams, Models){
     }
 
     $scope.vm = vm;
+    $scope.filter = 'ALL';
+    $scope.EPlanStatus = EPlanStatus;
 
+    $scope.changeTo = async function(filter){
+        $scope.tripPlans = [];
+        if (['WAIT_UPLOAD', 'ALL', 'AUDITING', 'TRIP_FAIL', 'TRIP_SUCCESS'].indexOf(filter) >= 0) {
+            $scope.filter = filter;
+        }
+
+        let PlanStatus: any = {$notIn: [EPlanStatus.COMPLETE, EPlanStatus.NO_BUDGET, EPlanStatus.CANCEL]};
+        switch(filter) {
+            case 'ALL': PlanStatus = {$notIn: [EPlanStatus.COMPLETE, EPlanStatus.NO_BUDGET, EPlanStatus.CANCEL]};break;
+            case 'WAIT_UPLOAD': PlanStatus = {$in: [EPlanStatus.WAIT_UPLOAD, EPlanStatus.WAIT_COMMIT]}; break;
+            case 'AUDITING': PlanStatus = EPlanStatus.AUDITING; break;
+            case 'TRIP_FAIL': PlanStatus = EPlanStatus.AUDIT_NOT_PASS; break;
+            case 'TRIP_SUCCESS': PlanStatus = EPlanStatus.COMPLETE; break;
+        }
+        let where:any = {
+            status: PlanStatus
+        }
+        pager = await staff.getTripPlans({
+            limit: 5,
+            where: where
+        });
+        loadTripPlan(pager);
+    };
+
+    await $scope.changeTo($scope.filter);
     $scope.enterdetail = function(trip){
         if (!trip) return;
         window.location.href = "#/trip/list-detail?tripid="+trip.id;
