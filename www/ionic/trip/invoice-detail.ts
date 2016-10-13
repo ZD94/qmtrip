cb7803971ea75eeb3004b3b7cf1878db21397b3b
chenhao <hao.chen@jingli.tech>
2016-10-13 11:03:23 +0800

    Revert "Revert "多票据上传""
    This reverts commit bee8ee8ecc420c34da19dd946733849f2ca7237a.

--- www/ionic/trip/invoice-detail.ts
+++ www/ionic/trip/invoice-detail.ts
@@ -1,8 +1,9 @@
 import { ETripType, EPlanStatus, EInvoiceType } from 'api/_types/tripPlan';
 import * as path from 'path';
 
-export async function InvoiceDetailController($scope , Models, $stateParams, $ionicPopup){
+export async function InvoiceDetailController($scope , Models, $stateParams, $ionicPopup, $ionicSlideBoxDelegate){
     //////绑定上传url
+    require("./invoice-detail.scss");
     let authDataStr = window['getAuthDataStr']();
     $scope.uploadUrl = '/upload/ajax-upload-file?type=image&auth='+authDataStr;
     ///// END
@@ -35,6 +36,7 @@ export async function InvoiceDetailController($scope , Models, $stateParams, $io
             invoiceImgs.push(img);
         }
         $scope.invoiceImgs = invoiceImgs;
+        $ionicSlideBoxDelegate.update();
     })
 
     let statusTxt = {};
