c2f75087b1429968dd38f19dd773654f9d3dc8a6
chenhao <hao.chen@jingli.tech>
2016-11-07 19:37:59 +0800

    关联订单时，未绑定供应商进行判断，不予跳转

--- www/ionic/trip/select-supplier.ts
+++ www/ionic/trip/select-supplier.ts
@@ -1,11 +1,20 @@
+import {Staff} from "api/_types/staff/staff";
+var msgbox = require('msgbox');
 
 export async function SelectSupplierController($scope, Models, $stateParams){
     require('./reserve.scss');
     //公共的供应商
     var suppliers = await Models.supplier.find({where:{companyId: null}});
+    let currentStaff = await Staff.getCurrent();
     $scope.suppliers = suppliers;
 
-    $scope.queryOrders = function(id){
-        window.location.href = "#/trip/orders?detailId="+$stateParams.detailId+"&supplierId="+id;
+    $scope.queryOrders = async function(id){
+        let binded = await currentStaff.getOneStaffSupplierInfo({supplierId:id});
+        console.info('binded',binded);
+        if(binded == null){
+            msgbox.log('请先绑定账号')
+        }else{
+            window.location.href = "#/trip/orders?detailId="+$stateParams.detailId+"&supplierId="+id;
+        }
     }
 }
