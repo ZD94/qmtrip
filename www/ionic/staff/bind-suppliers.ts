0c7c5d398af3723a16fbc50f4aaf9dc033fce7a4
chenhao <hao.chen@jingli.tech>
2016-11-08 17:23:47 +0800

    绑定第三方账号

--- www/ionic/staff/bind-suppliers.ts
+++ www/ionic/staff/bind-suppliers.ts
@@ -1,7 +1,16 @@
 import {StaffSupplierInfo} from "api/_types/staff/staff-supplier-info";
 import { Staff } from 'api/_types/staff/staff';
+var msgbox = require('msgbox');
+export function selectSuppliers($scope, id, ngModalDlg){
+    return ngModalDlg.createDialog({
+        parent: $scope,
+        scope: {id},
+        template: require('./bind-account.html'),
+        controller: bindSupplierController
+    })
+}
 
-export async function BindSuppliersController($scope, Models){
+export async function BindSuppliersController($scope, Models, ngModalDlg){
     var staff = await Staff.getCurrent();
     var alreadyBinds = await Models.staffSupplierInfo.find({where: {staffId: staff.id}});
     var suppliers = await Models.supplier.find({where: {companyId: null}});
@@ -22,7 +31,68 @@ export async function BindSuppliersController($scope, Models){
         })
     }
     $scope.suppliers = suppliers;
-    $scope.bindSupplier = function (id) {
-        window.location.href = `#/staff/bind-others?supplierId=${id}`;
+    $scope.bindSupplier = async function (id) {
+        let bind = await selectSuppliers($scope,{id:id}, ngModalDlg)
+        console.info(bind);
+        // window.location.href = `#/staff/bind-others?supplierId=${id}`;
+    }
+}
+
+
+export async function bindSupplierController($scope, Models, $ionicPopup){
+    require('./bind-account.scss');
+    $scope.form = {userName:'', pwd: ''};
+
+    var staffSupplierInfo;
+    var supplierId = $scope.supplierId;
+    var staff = await Staff.getCurrent();
+    var supplier = await Models.supplier.get(supplierId);
+    var alreadyBind = await Models.staffSupplierInfo.find({where: {supplierId: supplierId, staffId: staff.id}});
+    $scope.alreadyBind = alreadyBind;
+    if(alreadyBind && alreadyBind.length>0){
+
+        staffSupplierInfo = alreadyBind[0];
+    }else{
+
+        staffSupplierInfo = StaffSupplierInfo.create();
+    }
+
+    $scope.staffSupplierInfo = staffSupplierInfo;
+
+    if(staffSupplierInfo.loginInfo){
+        $scope.form = JSON.parse(staffSupplierInfo.loginInfo);
+    }
+
+    $scope.save = async function(){
+        var checkoutResult = await staff.checkStaffSupplierInfo({supplierId: supplierId, userName: $scope.form.userName, pwd: $scope.form.pwd});
+        if(checkoutResult){
+            staffSupplierInfo.loginInfo = JSON.stringify($scope.form);
+            staffSupplierInfo.supplier = supplier;
+            staffSupplierInfo.staff = staff;
+            await staffSupplierInfo.save();
+            msgbox.log("绑定成功");
+            $scope.modal.hide();
+        }else{
+            msgbox.log("验证失败");
+        }
+    }
+    $scope.delete = async function(){
+        $ionicPopup.show({
+            title: '确定要解除绑定吗?',
+            buttons: [
+                {
+                    text: '取消'
+                },
+                {
+                    text: '确定',
+                    type: 'button-positive',
+                    onTap: async function(){
+                        await staffSupplierInfo.destroy();
+                        msgbox.log('解绑成功');
+                        $scope.modal.hide();
+                    }
+                }
+            ]
+        })
     }
 }
