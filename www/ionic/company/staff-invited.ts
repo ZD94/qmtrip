9b128aab71ba8cfef1f6ae3e846a9d0c67f0b6e3
chenhao <hao.chen@jingli.tech>
2016-11-18 16:08:50 +0800

    修改分享微信图片链接

--- www/ionic/company/staff-invited.ts
+++ www/ionic/company/staff-invited.ts
@@ -3,6 +3,7 @@ import moment = require('moment');
 import { InvitedLink } from 'api/_types/staff/invited-link';
 var browserspec = require('browserspec');
 var printf = require('printf');
+import * as path from 'path';
 
 declare var wx:any;
 declare var ionic:any;
@@ -29,7 +30,7 @@ export async function StaffInvitedController($scope, Models, $ionicHistory, $ion
         var min = Math.floor(secend/60)%60;
         var hour = Math.floor(secend/3600);
         return printf('%d:%02d:%02d', hour, min, sec);
-    }
+    };
     if(invitedLinks && invitedLinks.length > 0){
         $scope.invitedLink = invitedLinks[0];
         $scope.encodeLink = encodeURIComponent($scope.invitedLink.goInvitedLink);
@@ -53,33 +54,35 @@ export async function StaffInvitedController($scope, Models, $ionicHistory, $ion
         $scope.invitedLink = invitedLink;
         $scope.encodeLink = encodeURIComponent(invitedLink.goInvitedLink);
         transformSeconds();
-    }
+    };
     $scope.stopLink = function(invitedLink){
         invitedLink.status = 0;
         invitedLink.save();
         $scope.invitedLink = null;
         clearInterval(timer);
-    }
+    };
     $scope.copyLink = async function(){
         var link:any = document.getElementById('link');
         link.select();
         document.execCommand('Copy');
-    }
+    };
     $scope.isAndroid = ionic.Platform.isAndroid();
     $scope.isIos =  ionic.Platform.isIOS();
     $scope.is_wechat = browserspec.is_wechat;
+    var config = require('config');
+    let shareImgUrl = path.join(config.update,'ionic/images/logo-whiteback.png');
     $scope.sendWx = async function(){
         if(browserspec.is_wechat){
             var show = $ionicPopup.show({
                 template: '<p>请点击微信右上角菜单<br>将链接分享给好友</p>',
                 cssClass: 'share_alert'
-            })
+            });
             ClosePopupService.register(show);
             var WxConfig={
                 title: staff.name +'邀请您注册鲸力商旅',
                 desc:'加入'+staff.company.name+',共同开启智能商旅!',
                 link: $scope.invitedLink.goInvitedLink,
-                imgUrl: 'https://j.jingli365.com/ionic/images/logo-whiteback.png',
+                imgUrl: shareImgUrl,
                 type: '', // 分享类型,music、video或link，不填默认为link
                 dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             };
@@ -91,11 +94,11 @@ export async function StaffInvitedController($scope, Models, $ionicHistory, $ion
                 if(type == 'App'){
                     
                 }
-            })
+            });
             $scope.$on('WechatShareCanceled', function(event, type){
                 // 用户取消分享后执行的回调函数
                 show.close();
-            })
+            });
             let openConfig = _.clone(WxConfig);
             openConfig.link = 'https://jingli365.com';
             wxApi.setupSharePublic(openConfig);
@@ -105,12 +108,12 @@ export async function StaffInvitedController($scope, Models, $ionicHistory, $ion
                 var wxConfig = {
                     title: staff.name+'邀请您注册鲸力商旅',
                     desc: '加入'+staff.company.name+',共同开启智能商旅!',
-                    imgUrl: 'https://j.jingli365.com/ionic/images/logo-whiteback.png',
+                    imgUrl: shareImgUrl,
                     mediaTagName: "TEST-TAG-001",
                     messageExt: "这是第三方带的测试字段",
                     messageAction: "<action>dotalist</action>",
                     link: $scope.invitedLink.goInvitedLink
-                }
+                };
                 await wxApi.setupSharePrivate(wxConfig)
             }
         }
