86f4173d5577d2d79684ce8afbda0a79cbf2e728
shicong <shicongbuct@126.com>
2016-11-30 09:46:16 +0800

    携程商旅的跳转(待完善)

---
+++ test/test.sc.b_ctrip.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+var requestPromise = require('request-promise');
+var request = require('request');
+
+
+var res = request.post({
+    json: true,
+    uri: "http://ct.ctrip.com/m/City/SearchCities",
+    form: {
+        key: "厦门",
+        site:3,
+    },
+    headers: {
+        'Referer': 'http://ct.ctrip.com',
+},
+    }, function(err, httpResponse, body){
+        if(err) return;
+        console.log(body.Response.city[0]);
+        var cityObj = body.Response.city[0];
+        var cityStr = cityObj.city;
+        var trainCityCode = cityStr.split(",")[1].split("|")[0];
+        console.log(trainCityCode);
+    })
