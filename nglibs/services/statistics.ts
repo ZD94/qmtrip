f6f811ab57b2e847eb046005f22bc3d780238899
shicong <shicongbuct@126.com>
2016-12-02 16:23:18 +0800

    对于基本统计的修改

--- nglibs/services/statistics.ts
+++ nglibs/services/statistics.ts
@@ -6,12 +6,12 @@ import angular = require('angular');
 angular
     .module('nglibs')
     .factory('statistics', function(){
-        var script = $('<script src="https://s95.cnzz.com/z_stat.php?id=1260899849&web_id=1260899849" language="JavaScript"></script>');
         var style = $('<style>a[title="站长统计"]{display: none}</style>');
-        $('head').append(script).append(style);
+        //var script = $('<script>var _czcn = _czcn || [];</script>')
+        $('head').append(style);
         return {
             trigger: function(){
-                
+                //console.log("trigger");
             }
         }
     })
