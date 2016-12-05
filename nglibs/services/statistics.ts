94ed9c000274f7b585588b9431c15a3e36423b6a
shicong <shicongbuct@126.com>
2016-12-05 10:49:52 +0800

    去掉U-web的跳转

--- nglibs/services/statistics.ts
+++ nglibs/services/statistics.ts
@@ -1,17 +1,21 @@
-"use strict";
-
-import angular = require('angular');
-
-
-angular
-    .module('nglibs')
-    .factory('statistics', function(){
-        var style = $('<style>a[title="站长统计"]{display: none}</style>');
-        //var script = $('<script>var _czcn = _czcn || [];</script>')
-        $('head').append(style);
-        return {
-            trigger: function(){
-                //console.log("trigger");
-            }
-        }
-    })
+// "use strict";
+//
+// import angular = require('angular');
+//
+//
+// angular
+//     .module('nglibs')
+//     .factory('statistics', function(){
+//         return new StatisticsCZCN();
+//     })
+//
+//
+// class StatisticsCZCN{
+//     $resolve(){
+//         return dyload('https://s95.cnzz.com/z_stat.php?id=1260899849&web_id=1260899849');
+//     }
+//
+//     trigger(){
+//         var el = $('<style>a[title="站长统计"]{display:none}</style>');
+//     }
+// }
