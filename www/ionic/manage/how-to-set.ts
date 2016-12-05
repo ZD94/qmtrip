72992b464d4ac08b3af6b88d5a82625baefa2f3f
chenhao <hao.chen@jingli.tech>
2016-12-05 18:03:10 +0800

    通知类型最后一个icon位置微调,并且修复oa提交bug

--- www/ionic/manage/how-to-set.ts
+++ www/ionic/manage/how-to-set.ts
@@ -19,19 +19,30 @@ export async function HowToSetController($scope, ngModalDlg, $ionicPopup){
 async function supportUsController($scope,$ionicPopup){
     API.require("approve");
     await API.onload();
-    var OA : {
-        name: string
-        url?: string
+    var OA = {
+        name: null,
+        url: null
     };
     $scope.OA = OA;
     $scope.saveOAOptions = async function(){
+        if(!$scope.OA.name){
+            msgbox.log('请填写OA名称');
+            return;
+        }
         try{
             await API.approve.reportHimOA({oaName: OA.name, oaUrl: OA.url});
-            $ionicPopup.alert({
+            $ionicPopup.show({
                 title: '提交成功',
                 template: 'OA信息已经提交成功，我们会记录您的信息并在系统支持该系统时第一时间通知您，谢谢！',
-                okText: '确定',
-                okType: 'button-clam'
+                buttons:[
+                    {
+                        text: '确定',
+                        type: 'button-calm',
+                        onTap: function(){
+                            $scope.modal.hide();
+                        }
+                    }
+                ]
             })
         }catch(err){
             msgbox.log(err.msg||err);
