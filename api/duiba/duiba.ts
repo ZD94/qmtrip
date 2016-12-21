83a5885b3e13f54793f7c0fe365bffa03cce628b
wangyali <yali.wang@jingli.com>
2016-12-21 15:05:44 +0800

    test 标记

--- api/duiba/duiba.ts
+++ api/duiba/duiba.ts
@@ -22,6 +22,7 @@ module.exports = function(app) {
  * @returns {any}
  */
 async function costCredit(req, res, next) {
+    console.info("扣积分接口================");
     var params = req.query;
     var { uid, credits,appKey, timestamp, description, orderNum, actualPrice,sign } = params;
     var staff = await Models.staff.get(uid);
@@ -83,6 +84,7 @@ async function costCredit(req, res, next) {
  * @returns {any}
  */
 async function resultNotice(req, res, next) {
+    console.info("接收通知接口================");
     var params = req.query;
     var { appKey, timestamp, success, errorMessage, orderNum, bizId,sign } = params;
 
