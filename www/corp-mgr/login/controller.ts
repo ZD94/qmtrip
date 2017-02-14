6c76215621c55d114fecc04aa4330bac3aea5445
wanglihui <lihui.wang@tulingdao.com>
2017-02-14 17:47:17 +0800

    add notify template

---
+++ www/corp-mgr/login/controller.ts
@@ -0,0 +1,28 @@
+/**
+ * Created by wlh on 2017/2/14.
+ */
+
+'use strict';
+const API = require("common/api");
+
+export async function LoginController($scope, $stateParams, $location) {
+    $scope.showAutoLoginMsg = false;
+    $scope.errorMsg = 'sorry, 登录失败!';
+    let backurl = $stateParams.backurl;
+    let authstr = $stateParams.authstr;
+    if (authstr) {
+        $scope.showAutoLoginMsg = true;
+    }
+
+    if (authstr) {
+        if (!backurl) {
+            backurl = '#/index/index';
+        }
+        API.require("auth");
+        await API.onload();
+        let data = new Buffer(authstr, 'base64').toString('utf-8');
+        await localStorage.setItem('auth_data', data);
+        API.reload_all_modules();
+        window.location.href = backurl;
+    }
+}
