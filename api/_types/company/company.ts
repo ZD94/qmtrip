1b65148549e9f3cb861469ed18815b7f837377e1
wangyali <yali.wang@jingli.com>
2017-02-24 12:30:22 +0800

    add company tripPlanNumBalance

--- api/_types/company/company.ts
+++ api/_types/company/company.ts
@@ -214,6 +214,15 @@ export class Company extends ModelObject{
     get extraExpiryDate(): Date { return null; }
     set extraExpiryDate(val: Date) {}
 
+    //可用剩余行程点数
+    get tripPlanNumBalance(): number {
+        let num = this.tripPlanNumLimit - this.extraTripPlanNum - this.tripPlanFrozenNum;
+        if(this.extraTripPlanNum && this.extraExpiryDate && (this.extraExpiryDate.getTime() - new Date().getTime()) > 0){
+            num = num + this.extraTripPlanNum;
+        }
+        return num;
+    }
+
     @Field({type: Types.INTEGER})
     get type() :ECompanyType{return ECompanyType.TRYING}
     set type(type: ECompanyType) {}
