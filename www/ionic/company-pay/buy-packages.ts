7752b4f46205f182fe0923b6727d0c8769168757
admin <1757898559@qq.com>
2017-02-27 19:19:22 +0800

    drop console

--- www/ionic/company-pay/buy-packages.ts
+++ www/ionic/company-pay/buy-packages.ts
@@ -8,6 +8,5 @@ export async function BuyPackagesController($scope){
     var staff = await Staff.getCurrent();
     $scope.staff = staff;
     $scope.company = staff.company;
-    $scope.company.type =1;
     $scope.ECompanyType = ECompanyType;
 }
