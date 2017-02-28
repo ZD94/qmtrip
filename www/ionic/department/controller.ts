8af4d715b8ffb541cbe58117d9c1e598be956649
chenhao <hao.chen@jingli.tech>
2017-02-28 15:58:34 +0800

    修复部门主管选择时的bug

--- www/ionic/department/controller.ts
+++ www/ionic/department/controller.ts
@@ -149,6 +149,7 @@ export async function IndexController($scope, $stateParams, Models, $ionicPopup,
     $scope.staffSelector = {
         query: async function(keyword) {
             let staffs = await rootDepartment.getStaffs({where: {'name': {$ilike: '%'+keyword+'%'}}});
+            Object.setPrototypeOf(staffs, Pager.prototype);
             return staffs;
         },
         display: (staff)=>staff.name
