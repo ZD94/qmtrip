abdbcd1a5062029cd07e9e262a428bf380b4f86f
[wangjixiang] <[739086063@qq.com]>
2017-03-07 12:23:44 +0800

    JLTRIP-127 预算调试页打分算法描述翻译,翻译完成，算法更新

--- www/budget-debug/index.js
+++ www/budget-debug/index.js
@@ -37,32 +37,95 @@ var app = angular.module('debugModule',[]);
 ];*/
 var available_prefer = [
     {
+        title:"[交通]直接到达",
+        value: '{"name": "directArrive", "options": {"deductScorePerStop": 10000}}'
+    },
+    {
+        title:"[交通]出境等待时长",
+        value:'{"name": "transitWaitDuration", "options": {"baseScore": 500, "subScore": 5, "maxDuration": 360, "minDuration": 120}}'
+    },
+    {
+        title:"[交通]出境城市",
+        value:'{"name": "transitCityInChina", "options": {"baseScore": 5000}}'
+    },
+    {
+        title:"[交通]运营时长",
+        value:'{"name": "runningTimePrefer", "options": {"planeScore": 500, "trainScore": 250, "planeScoreInterval": 5, "trainScoreInterval":5}}'
+    },
+    {
+        title:"[交通]运营时长",
+        value:'{"name": "runningTimePrefer", "options": {"planeScore": 500, "trainScore": 250, "planeScoreInterval": 5, "trainScoreInterval":5}}'
+    },
+    {
+        title:"[交通]标准出发时间",
+        value:'{"name": "departStandardTimePrefer", "options": {"end": "最晚时间（格式YYYY-MM-DD HH:mm +0800）", "begin": "开始时间（格式YYYY-MM-DD HH:mm +0800）", "score": 500, "scoreInterval":-3}}'
+    },
+    {
+        title: "[交通]标准到达时间",
+        value: '{"name": "arriveStandardTimePrefer", "options": {"end": "最晚时间（格式YYYY-MM-DD HH:mm +0800）", "begin": "开始时间（格式YYYY-MM-DD HH:mm +0800）", "score": 500,  "scoreInterval":3}}'
+    },
+    {
+        title: "[交通]火车运行时间",
+        value: '{"name": "trainDurationPrefer", "options": {"score": 100000, "trainDuration":360}}'
+    },
+    {
+        title: "[交通]最早返回时间",
+        value: '{"name": "earliestGoBackTimePrefer", "options": {"score": 100000, "earliestGoBackTime":"最早返回时间（格式YYYY-MM-DD HH:mm +0800）"}}'
+    },
+    {
+        title: "[交通]最晚到达时间",
+        value: '{"name": "latestArrivalTimePrefer", "options": {"score": 100000, "latestArrivalTime":"最晚到达时间（格式YYYY-MM-DD HH:mm +0800）"}}'
+    },
+    {
+        title: "[交通]火车价格",
+        value: '{"name": "trainPricePrefer", "options": {"score": 100000}}'
+    },
+    {
+        title: "[交通]飞机票价期待值",
+        value: '{"name": "planePricePrefer", "options": {"type": "square", "score": 10000, "cabins":2, "percent":0.2}}'
+    },
+    {
+        title: "[交通]代理商",
+        value: '{"name": "preferAgent", "options": {"score": 100, "expectedAgents":"ctrip,携程,同城,南航,中航,中国航空公司"}}'
+    },
+    {
         title: "[交通]到达时间",
-        value: '{"name": "arrivalTime", "options": {"end": "最晚时间", "begin": "开始时间,格式YYYY-MM-DD HH:mm +0800", "score": "符合到达时间得分"}}'
+        value: '{"name": "arrivalTime", "options": {"end": "最晚时间（格式YYYY-MM-DD HH:mm +0800）", "begin": "开始时间（格式YYYY-MM-DD HH:mm +0800）", "score": "20000"}}'
     },
     {
         title: "[交通]舱位",
-        value: '{"name": "cabin", "options": {"score": "符合舱位得分", "expectTrainCabins": "期待的火车舱位", "expectFlightCabins": "期待的飞机舱位"}}'
+        value: '{"name": "cabin", "options": {"score": "10000", "expectTrainCabins": "2", "expectFlightCabins": "3,4,5"}}'
     },
     {
         title: "[交通]廉价航空",
-        value: '{"name": "cheapSupplier", "options": {"score": "如果在廉价航空中得分", "cheapSuppliers": "廉价航班"}}'
+        value: '{"name": "cheapSupplier", "options": {"score": "-100", "cheapSuppliers": "9C,KN,HO,PN,EU,AQ,JR"}}'
     },{
         title: "[交通]出发时间",
-        value: '{"name": "departTime", "options": {"end": "最晚时间", "begin": "开始时间", "score": "符合时间得分"}}'
+        value: '{"name": "departTime", "options": {"end": "最晚时间（格式YYYY-MM-DD HH:mm +0800）", "begin": "开始时间（格式YYYY-MM-DD HH:mm +0800）", "score": "-20000"}}'
     },
     {
         title: "[交通]交通方式",
         value: '{"name": "selectTraffic", "options": {"score": 500, "commonTrainScore":-100000, "selectTrainDuration": 360, "selectFlightDuration": 210}}'
-    },{
+    },
+    {
         title: "[酒店]星级",
         value: '{"name": "starMatch", "options": {"score": 500, "expectStar": 3}}'
-    },{
+    },
+    {
         title: "[酒店]代表性酒店",
         value: '{"name": "represent", "options": {"score": 100}}'
-    },{
+    },
+    {
         title: "[酒店]黑名单",
         value: '{"name": "blackList", "options": {"score": -100}}'
+    },
+    {
+        title: "[酒店]价格区间",
+        value: '{"name": "priceRange", "options": {"range":{"2":[120,380],"3":[180,600],"4":[280,1000],"5":[450,2300]}, "score": -1000000}}'
+    },
+    {
+        title: "[酒店]价格",
+        value: '{"name": "price", "options": { "score": 2000,"percent":0.2}}'
     }
 ];
 //描述翻译
