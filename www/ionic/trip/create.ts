118fbfd950f7313053a9fe5a01a909c48a87e388
chenhao <hao.chen@jingli.tech>
2017-03-10 17:37:48 +0800

    JLTRIP-128 我要出差的页面重新布局及流程，便于接下来的几个功能引入

--- www/ionic/trip/create.ts
+++ www/ionic/trip/create.ts
@@ -1,6 +1,12 @@
+/**
+ * Created by seven on 2017/3/8.
+ */
+"use strict";
+
 import moment = require('moment');
 import { Staff } from 'api/_types/staff/staff';
 import {EApproveChannel} from "api/_types/approve/types";
+import {destinationController} from "./destination-template";
 
 var msgbox = require('msgbox');
 
@@ -8,15 +14,15 @@ var msgbox = require('msgbox');
 var defaultTrip = {
     beginDate: moment().add(3, 'days').startOf('day').hour(18).toDate(),
     endDate: moment().add(4, 'days').startOf('day').hour(9).toDate(),
-    place: undefined,
+    destination: undefined,
     placeName: '',
     reason: '',
 
     traffic: true,
-    fromPlace: undefined,
+    origin: undefined,
     round: true,
 
-    hotel: false,
+    hotel: true,
     hotelPlace: undefined
 };
 type TripDefine = typeof defaultTrip;
@@ -31,65 +37,10 @@ function TripDefineFromJson(obj: any): TripDefine{
 
 export async function CreateController($scope, $storage, $loading, ngModalDlg, $ionicPopup, Models, City){
     require('./create.scss');
-    API.require('tripPlan');
-    await API.onload();
-    /*******************出差补助选择begin************************/
-    $scope.select_subsidy = {
-        name: '请选择'
-    };
+    $scope.showOrigin = false;
+    $scope.showDestination = false;
     $scope.currentStaff = await Staff.getCurrent();
     let currentCompany = $scope.currentStaff.company;
-    $scope.currentTp = await $scope.currentStaff.getTravelPolicy();
-    if($scope.currentTp){
-        $scope.currentTpSts = await $scope.currentTp.getSubsidyTemplates();
-    }
-    $scope.subsidy = {hasFirstDaySubsidy: true, hasLastDaySubsidy: true, template: null};
-
-    var ret = await $scope.currentStaff.testServerFunc();
-    console.log('$scope.currentStaff.testServerFunc() return', ret);
-
-    $scope.selectSubsidyTemplate = async function(){
-        $ionicPopup.show({
-            title:'出差补助选择',
-            cssClass:'selectSubsidy',
-            template:require('./selectSubsidy.html'),
-            scope: $scope,
-            buttons:[
-                {
-                    text: '确定',
-                    type: 'button-positive',
-                    onTap: async function (e) {
-                        try{
-                            if(!$scope.subsidy.template){
-                                e.preventDefault();
-                                msgbox.log("请选择补助模板");
-                                return false;
-                            }else{
-                                $scope.select_subsidy = $scope.subsidy.template;
-                            }
-                        }catch(err){
-                            msgbox.log(err.msg || err);
-                        }
-                    }
-                }
-            ]
-        });
-    }
-
-    /*******************出差补助选择end************************/
-    $scope.showSpecialApprove = (!$scope.currentStaff.company.oa || $scope.currentStaff.company.oa == EApproveChannel.QM);
-
-    /******住宿打开******/
-    $scope.$watch('trip.place',function(n,o){
-        if(n != undefined){
-            $scope.trip.hotel = true;
-        }
-    })
-    /*****************/
-    let minStDate = moment().format('YYYY-MM-DD');
-    $scope.minStDate = minStDate;
-    $scope.minEndDate = moment().add(3, 'days').format('YYYY-MM-DD');
-
     let trip;
     try {
         trip= TripDefineFromJson($storage.local.get('trip'));
@@ -121,43 +72,46 @@ export async function CreateController($scope, $storage, $loading, ngModalDlg, $
             trip.endDate = moment(trip.beginDate).add(3, 'days').toDate();
         }
     })
-
     $scope.calcTripDuration = function(){
         return moment(trip.endDate).startOf('day').diff(moment(trip.beginDate).startOf('day'), 'days') || 1;
     };
-    $scope.incTripDuration = function(){
-        trip.endDate = moment(trip.endDate).add(1, 'days').toDate();
-        $scope.$applyAsync();
-    };
-    $scope.decTripDuration = function(){
-        var newDate = moment(trip.endDate).subtract(1, 'days').toDate();
-        if(newDate > trip.beginDate){
-            trip.endDate = newDate;
-            $scope.$applyAsync();
+    $scope.addStartCity = async function(){
+        let option = $scope.placeSelector;
+        option.title = '出发地选择';
+        let city = await ngModalDlg.selectCity($scope,option,$scope.trip.origin);
+        console.info(city);
+        if(city){
+            $scope.showOrigin = true;
+            $scope.trip.origin = city;
         }
-    };
-
-    $scope.$watch('trip.place.name', function($newVal, $oldVal) {
-        if ($newVal != $oldVal) {
-            $scope.trip.hotelPlaceObj = undefined;
-            $scope.trip.hotelPlace = '';
-            $scope.trip.hotelName = '';
+    }
+    $scope.onlyHotel = async function(){
+        let option = $scope.fromPlaceSelector;
+        option.title = '目的地选择';
+        let city = await ngModalDlg.selectCity($scope,option,$scope.trip.destination);
+        if(city){
+            $scope.trip.destination = city;
+            $scope.changeDestination($scope.trip);
         }
-    });
-
-    // async function queryPlaces(keyword){
-    //     if (!keyword) {
-    //         let hotCities = $storage.local.get("hot_cities")
-    //         if (hotCities && hotCities[0] && hotCities[0].id) {
-    //             return hotCities;
-    //         }
-    //     }
-    //     var places = await API.place.queryPlace({keyword: keyword});
-    //     if (!keyword) {
-    //         $storage.local.set('hot_cities', places);
-    //     }
-    //     return places;
-    // }
+    }
+    $scope.changeDestination = async function(trip){
+        let ret = await ngModalDlg.createDialog({
+            parent: $scope,
+            scope:{
+                trip: $scope.trip,
+                beginDate: $scope.trip.beginDate,
+                endDate: $scope.trip.endDate
+            },
+            template: require('./destination-template.html'),
+            controller: destinationController
+        })
+        console.info(ret);
+        if(ret){
+            $scope.showDestination = true;
+            $scope.trip = ret;
+            console.info(ret);
+        }
+    }
     async function queryAllPlaces(keyword: string, isAbroad: boolean){
         let key= 'hot_cities_170228_domestic';
         if (isAbroad) {
@@ -216,68 +170,8 @@ export async function CreateController($scope, $storage, $loading, ngModalDlg, $
             return item.name
         }
     };
-    $scope.projectSelector = {
-        query: async function(keyword){
-            var staff = await Staff.getCurrent();
-            var options = {where:{companyId: staff.company.id}};
-            if(keyword){
-                options.where["name"] = {$like: '%'+keyword+'%'};
-            }
-            var projects = await Models.project.find(options);
-            return projects;
-        },
-        display: (item)=>item.name,
-        create: async function(name){
-            return {
-                name: name,
-                id: undefined
-            }
-        },
-        done: function(val) {
-            $scope.trip.reason = val.name ? val.name: val;
-        }
-    };
-    $scope.hotelPlaceSelector = {
-        done: function(val) {
-            if (!val.point || !val.point.lat || !val.point.lng) {
-                $scope.showErrorMsg("获取住宿位置失败");
-                return;
-            }
-            $scope.trip.hotelPlace = val.point.lat + "," + val.point.lng
-            $scope.trip.hotelName = val.title;
-        }
-    };
-
-    $scope.selectDatespan = async function(){
-        let value = {
-            begin: $scope.trip.beginDate,
-            end: $scope.trip.endDate
-        }
-        value = await ngModalDlg.selectDateSpan($scope, {
-            beginDate: new Date(),
-            endDate: moment().add(1, 'year').toDate(),
-            timepicker: true,
-            title: '选择开始时间',
-            titleEnd: '选择结束时间',
-        }, value);
-        if(value){
-            $scope.trip.beginDate = value.begin;
-            $scope.trip.endDate = value.end;
-        }
-
-    };
-
-    $scope.endDateSelector = {
-        beginDate: $scope.trip.beginDate,
-        endDate: moment().add(1, 'year').toDate(),
-        timepicker: true,
-    };
-    $scope.$watch('trip.beginDate', function(n, o){
-        $scope.endDateSelector.beginDate = $scope.trip.beginDate;
-    })
-    $scope.nextStep = async function() {
+    $scope.nextStep = async function(){
         let trip = $scope.trip;
-
         let number = 0;
         if(trip.traffic){
             number = number + 1;
@@ -297,42 +191,15 @@ export async function CreateController($scope, $storage, $loading, ngModalDlg, $
             })
             return false;
         }
-        if ($scope.currentTpSts && $scope.currentTpSts.length && (!$scope.subsidy || !$scope.subsidy.template)) {
-            $scope.showErrorMsg('请选择补助信息');
-            return false;
-        }
         let beginMSecond = Date.now();
         API.require("travelBudget");
         await API.onload();
-
-        if(!trip.place || !trip.place.id) {
-            $scope.showErrorMsg('请填写出差目的地！');
-            return false;
+        if(!trip.origin){
+            trip.traffic = false;
         }
-
-        if(!trip.reasonName) {
-            $scope.showErrorMsg('请填写出差事由！');
-            return false;
-        }
-
-        if(!trip.traffic && ! trip.hotel) {
-            $scope.showErrorMsg('请选择交通或者住宿！');
-            return false;
-        }
-
-        if(trip.traffic && (!trip.fromPlace || !trip.fromPlace.id)) {
-            $scope.showErrorMsg('请选择出发地！');
-            return false;
-        }
-
-        if(moment(trip.endDate).toDate().getTime() - moment(trip.beginDate).toDate().getTime() <= 0) {
-            $scope.showErrorMsg('到达时间不可晚于离开时间！');
-            return false;
-        }
-
         let params = {
-            originPlace: trip.fromPlace? trip.fromPlace.id : '',
-            destinationPlace: trip.place ? trip.place.id : '',
+            originPlace: trip.origin? trip.origin.id : '',
+            destinationPlace: trip.destination ? trip.destination.id : '',
             leaveDate: moment(trip.beginDate).toDate(),
             goBackDate: moment(trip.endDate).toDate(),
             latestArrivalDateTime: moment(trip.beginDate).toDate(),
@@ -344,12 +211,10 @@ export async function CreateController($scope, $storage, $loading, ngModalDlg, $
             hotelName: trip.hotelName,
             subsidy: $scope.subsidy
         };
-
-        if(params.originPlace == params.destinationPlace){
+        if(trip.origin && params.originPlace == params.destinationPlace){
             msgbox.log("出差地点和出发地不能相同");
             return false;
         }
-
         let front = ['正在验证出行参数', '正在匹配差旅政策', '正在搜索全网数据', '动态预算即将完成'];
         $loading.reset();
         $loading.start({
@@ -402,74 +267,4 @@ export async function CreateController($scope, $storage, $loading, ngModalDlg, $
         }
     }
 
-    $scope.specialApprove = async function() {
-        API.require("travelBudget");
-        await API.onload();
-
-        let trip = $scope.trip;
-
-        if(!trip.place || !trip.place.id) {
-            $scope.showErrorMsg('请填写出差目的地！');
-            return false;
-        }
-
-        if(!trip.reasonName) {
-            $scope.showErrorMsg('请填写出差事由！');
-            return false;
-        }
-
-        // if(!trip.traffic && ! trip.hotel) {
-        //     $scope.showErrorMsg('请选择交通或者住宿！');
-        //     return false;
-        // }
-
-        if(trip.traffic && (!trip.fromPlace || !trip.fromPlace.id)) {
-            $scope.showErrorMsg('请选择出发地！');
-            return false;
-        }
-
-        let params = {
-            originPlace: trip.fromPlace? trip.fromPlace.id : '',
-            destinationPlace: trip.place ? trip.place.id : '',
-            leaveDate: moment(trip.beginDate).toDate(),
-            goBackDate: moment(trip.endDate).toDate(),
-            latestArrivalDateTime: moment(trip.beginDate).toDate(),
-            earliestGoBackDateTime: moment(trip.endDate).toDate(),
-            isNeedTraffic: trip.traffic,
-            isRoundTrip: trip.round,
-            isNeedHotel: trip.hotel,
-            businessDistrict: trip.hotelPlace,
-            hotelName: trip.hotelName,
-        };
-
-        if(params.originPlace == params.destinationPlace){
-            msgbox.log("出差地点和出发地不能相同");
-            return false;
-        }
-
-        try {
-            $loading.end();
-            window.location.href = "#/trip/special-approve?params="+JSON.stringify(params);
-        } catch(err) {
-            $loading.end();
-            alert(err.msg || err);
-        }
-    }
-
-    $scope.checkDate = function(isStartTime?: boolean) {
-        let beginDate = trip.beginDate;
-        let endDate = trip.endDate;
-        if(moment(endDate).diff(moment(beginDate)) < 0) {
-            if(isStartTime) {
-                $scope.minEndDate = moment(beginDate).format('YYYY-MM-DD');
-            }else {
-                $scope.showErrorMsg('结束日期不能早于结束日期！');
-            }
-            $scope.trip.endDate = beginDate;
-            return;
-        }
-
-        $scope.oldBeginDate = beginDate;
-        $scope.minEndDate = moment(beginDate).format('YYYY-MM-DD');
-    }
 }
---
+++ www/ionic/trip/destination-template.ts
@@ -0,0 +1,139 @@
+/**
+ * Created by seven on 2017/3/8.
+ */
+"use strict";
+import {Staff} from "api/_types/staff/staff";
+import moment = require('moment');
+var msgbox = require('msgbox');
+export async function destinationController($scope, ngModalDlg, Models, $ionicPopup){
+    require('./destination-template.scss');
+    $scope.projectSelector = {
+        query: async function(keyword){
+            var staff = await Staff.getCurrent();
+            var options = {where:{companyId: staff.company.id}};
+            if(keyword){
+                options.where["name"] = {$like: '%'+keyword+'%'};
+            }
+            var projects = await Models.project.find(options);
+            return projects;
+        },
+        display: (item)=>item.name,
+        create: async function(name){
+            return {
+                name: name,
+                id: undefined
+            }
+        },
+        done: function(val) {
+            $scope.trip.reason = val.name ? val.name: val;
+        }
+    };
+    $scope.hotelPlaceSelector = {
+        done: function(val) {
+            if (!val.point || !val.point.lat || !val.point.lng) {
+                $scope.showErrorMsg("获取住宿位置失败");
+                return;
+            }
+            $scope.trip.hotelPlace = val.point.lat + "," + val.point.lng
+            $scope.trip.hotelName = val.title;
+        }
+    };
+
+    $scope.selectDatespan = async function(){
+        let value = {
+            begin: $scope.trip.beginDate,
+            end: $scope.trip.endDate
+        }
+        value = await ngModalDlg.selectDateSpan($scope, {
+            beginDate: new Date(),
+            endDate: moment().add(1, 'year').toDate(),
+            timepicker: true,
+            title: '选择开始时间',
+            titleEnd: '选择结束时间',
+        }, value);
+        if(value){
+            $scope.trip.beginDate = value.begin;
+            $scope.trip.endDate = value.end;
+        }
+
+    };
+
+    $scope.endDateSelector = {
+        beginDate: $scope.trip.beginDate,
+        endDate: moment().add(1, 'year').toDate(),
+        timepicker: true,
+    };
+    $scope.$watch('trip.beginDate', function(n, o){
+        $scope.endDateSelector.beginDate = $scope.trip.beginDate;
+    })
+    $scope.calcTripDuration = function(){
+        return moment($scope.trip.endDate).startOf('day').diff(moment($scope.trip.beginDate).startOf('day'), 'days') || 1;
+    };
+    /*******************出差补助选择begin************************/
+    $scope.select_subsidy = {
+        name: '请选择'
+    };
+    $scope.currentStaff = await Staff.getCurrent();
+    let currentCompany = $scope.currentStaff.company;
+    $scope.currentTp = await $scope.currentStaff.getTravelPolicy();
+    if($scope.currentTp){
+        $scope.currentTpSts = await $scope.currentTp.getSubsidyTemplates();
+    }
+    $scope.subsidy = {hasFirstDaySubsidy: true, hasLastDaySubsidy: true, template: null};
+
+    var ret = await $scope.currentStaff.testServerFunc();
+    console.log('$scope.currentStaff.testServerFunc() return', ret);
+
+    $scope.selectSubsidyTemplate = async function(){
+        $ionicPopup.show({
+            title:'出差补助选择',
+            cssClass:'selectSubsidy',
+            template:require('./selectSubsidy.html'),
+            scope: $scope,
+            buttons:[
+                {
+                    text: '确定',
+                    type: 'button-positive',
+                    onTap: async function (e) {
+                        try{
+                            if(!$scope.subsidy.template){
+                                e.preventDefault();
+                                msgbox.log("请选择补助模板");
+                                return false;
+                            }else{
+                                $scope.select_subsidy = $scope.subsidy.template;
+                            }
+                        }catch(err){
+                            msgbox.log(err.msg || err);
+                        }
+                    }
+                }
+            ]
+        });
+    }
+
+    /*******************出差补助选择end************************/
+
+    $scope.complete = function(){
+        let trip = $scope.trip;
+        if(!trip.destination || !trip.destination.id) {
+            $scope.showErrorMsg('请填写出差目的地！');
+            return false;
+        }
+
+        if(!trip.reasonName) {
+            $scope.showErrorMsg('请填写出差事由！');
+            return false;
+        }
+
+        if(moment(trip.endDate).toDate().getTime() - moment(trip.beginDate).toDate().getTime() <= 0) {
+            $scope.showErrorMsg('到达时间不可晚于离开时间！');
+            return false;
+        }
+        if ($scope.currentTpSts && $scope.currentTpSts.length && (!$scope.subsidy || !$scope.subsidy.template)) {
+            $scope.showErrorMsg('请选择补助信息');
+            return false;
+        }
+        $scope.confirmModal(trip)
+    }
+}
