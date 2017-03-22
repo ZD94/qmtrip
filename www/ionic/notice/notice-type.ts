28d29490bedbadad8e542f07b7cfcf1a8a7cf2d8
wangyali <yali.wang@jingli365.com>
2017-03-22 13:57:22 +0800

    JLTRIP-264 排序问题 250已读标记问题

--- www/ionic/notice/notice-type.ts
+++ www/ionic/notice/notice-type.ts
@@ -8,13 +8,17 @@ export async function NoticeTypeController($scope, Models, $stateParams) {
 
     var noticeTypes = [
         {name: "系统通知", value: ENoticeType.SYSTEM_NOTICE
-            , unReadNum: statisticInfo[ENoticeType.SYSTEM_NOTICE].unReadNum, latestInfo: statisticInfo[ENoticeType.SYSTEM_NOTICE].latestInfo},
+            , unReadNum: statisticInfo[ENoticeType.SYSTEM_NOTICE] ? statisticInfo[ENoticeType.SYSTEM_NOTICE].unReadNum : 0,
+            latestInfo: statisticInfo[ENoticeType.SYSTEM_NOTICE] ? statisticInfo[ENoticeType.SYSTEM_NOTICE].latestInfo: null},
         {name: "出差审批通知", value: ENoticeType.TRIP_APPROVE_NOTICE
-            , unReadNum: statisticInfo[ENoticeType.TRIP_APPROVE_NOTICE].unReadNum, latestInfo: statisticInfo[ENoticeType.TRIP_APPROVE_NOTICE].latestInfo},
+            , unReadNum: statisticInfo[ENoticeType.TRIP_APPROVE_NOTICE] ? statisticInfo[ENoticeType.TRIP_APPROVE_NOTICE].unReadNum : 0,
+            latestInfo: statisticInfo[ENoticeType.TRIP_APPROVE_NOTICE] ? statisticInfo[ENoticeType.TRIP_APPROVE_NOTICE].latestInfo: null},
         {name: "出差请示", value: ENoticeType.TRIP_APPLY_NOTICE
-            , unReadNum: statisticInfo[ENoticeType.TRIP_APPLY_NOTICE].unReadNum, latestInfo: statisticInfo[ENoticeType.TRIP_APPLY_NOTICE].latestInfo},
+            , unReadNum: statisticInfo[ENoticeType.TRIP_APPLY_NOTICE] ? statisticInfo[ENoticeType.TRIP_APPLY_NOTICE].unReadNum : 0,
+            latestInfo: statisticInfo[ENoticeType.TRIP_APPLY_NOTICE] ? statisticInfo[ENoticeType.TRIP_APPLY_NOTICE].latestInfo: null},
         {name: "精彩活动", value: ENoticeType.ACTIVITY_NOTICE
-            , unReadNum: statisticInfo[ENoticeType.ACTIVITY_NOTICE].unReadNum, latestInfo: statisticInfo[ENoticeType.ACTIVITY_NOTICE].latestInfo}
+            , unReadNum: statisticInfo[ENoticeType.ACTIVITY_NOTICE] ? statisticInfo[ENoticeType.ACTIVITY_NOTICE].unReadNum : 0,
+            latestInfo: statisticInfo[ENoticeType.ACTIVITY_NOTICE] ? statisticInfo[ENoticeType.ACTIVITY_NOTICE].latestInfo: null}
     ];
     $scope.noticeTypes = noticeTypes;
     $scope.goList = async function(type){
