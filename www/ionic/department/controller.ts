126855c7fb21676dc5e789ab81552cc0077d73c3
jixiang.wang <jixiang.wang@jingli365.com>
2017-03-22 11:43:35 +0800

    JLTRIP-287 【test】选择部门管理-返回-js报错

--- www/ionic/department/controller.ts
+++ www/ionic/department/controller.ts
@@ -172,9 +172,9 @@ export async function IndexController($scope, $stateParams, Models, $ionicPopup,
         if(result){
             if(result.destroy){
                 initDepartment();
-            }else{
-                initDepartment(departmentId);
             }
+        }else{
+            initDepartment(departmentId);
         }
     }
     $scope.multipleMove = async function(){
