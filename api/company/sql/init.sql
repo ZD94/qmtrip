3a4b1f743e6799798e5bd23a6bcd3315194f5df1
hexisen <616082046@qq.com>
2017-03-28 12:01:42 +0800

    JIRA 219 暂存

--- api/company/sql/init.sql
+++ api/company/sql/init.sql
@@ -9,3 +9,7 @@ INSERT INTO company.suppliers(
     VALUES ('e3a4adc0-977f-11e6-87ee-1bf6381bbd7b', '携程商旅', 'http://ct.ctrip.com/m/Book/Flight', 'http://ct.ctrip.com/m/Book/Hotel',
     'c3ebd500-385f-11e6-8e0a-d53d0d2d4e19', null, now(), now(), 'ct_ctrip_com_m', 2, true);
 
+INSERT INTO company.suppliers(
+            id, name, traffic_book_link, hotel_book_link, logo, company_id, created_at, updated_at, supplier_key, type, is_in_use)
+    VALUES ('2244b1a1-74c7-11e6-b1b9-f14ff84d5a8e', '去哪儿', 'https://touch.qunar.com/h5/flight', 'https://touch.qunar.com/hotel',
+    'c3ebd500-385f-11e6-8e0a-d53d0d2d4e19', null, now(), now(), 'qunar_com_m', 2, true);
---
+++ libs/suppliers/qunar_com/index.ts
@@ -0,0 +1,142 @@
+
+import _ = require('lodash');
+import moment = require("moment");
+import { SupplierWebRobot, SupplierOrder, ReserveLink } from '../index';
+import L from 'common/language';
+
+var iconv = require('iconv-lite');
+
+export default class SupplierCtripCT extends SupplierWebRobot{
+    constructor(){
+        super('https://touch.qunar.com/h5');
+    }
+
+    async login(authDate: any): Promise<any>{
+        throw L.ERR.NOT_IMPLEMENTED();
+    }
+
+    async getOrderList(): Promise<SupplierOrder[]>{
+        throw L.ERR.NOT_IMPLEMENTED();
+    }
+
+    async getBookLink(options): Promise<ReserveLink>{
+        var reserveType = options.reserveType;
+        var bookLink: any = {};
+
+        if(reserveType == "travel_plane"){
+            bookLink = await this.getAirTicketReserveLink({fromCity: options.fromCity, toCity: options.toCity, leaveDate: options.leaveDate});
+        }
+
+        if(reserveType == "travel_train"){
+            bookLink = await this.getTrainTicketReserveLink({fromCity: options.fromCity, toCity: options.toCity, leaveDate: options.leaveDate});
+        }
+
+        if(reserveType == "hotel"){
+            bookLink = await this.getHotelReserveLink(options);
+        }
+
+        return bookLink;
+    }
+
+    async getAirTicketReserveLink(options):Promise<ReserveLink> {
+        if(options.leaveDate){
+            options.leaveDate = moment(options.leaveDate).format('YYYY-MM-DD')
+        }
+        // var fromCityCode = await this.queryFlightCityCode(options.fromCity);
+        var toCityCode = await this.queryFlightCityCode(options.toCity);
+        var values = {fromCityCode: fromCityCode, toCityCode: toCityCode, departDate: options.leaveDate};
+        var template = "http://m.ctrip.com/html5/flight/flight-list.html?triptype=1&dcode=<%=fromCityCode%>&acode=<%=toCityCode%>&ddate=<%=departDate%>";
+        var temp = _.template(template);
+        var link = temp(values);
+        return {url:link, jsCode: ""};
+    }
+
+    async getHotelReserveLink(options):Promise<ReserveLink> {
+        let days = +moment(options.backDate) - (+moment(options.leaveDate));
+        days = Math.floor( days / (1000 * 60 * 60 * 24) );
+        let after = +moment(options.leaveDate) - (+new Date());
+        after = Math.floor( after / (1000 * 60 * 60 * 24) );
+
+        var cityInfo = await this.queryHotelCityCode(options.city);
+        var values = {cityInfo: cityInfo , days : days , after : after};
+        var template = "http://m.ctrip.com/webapp/hotel/<%=cityInfo%>/checkin-<%=days%>-<%=after%>?fr=index";
+        var temp = _.template(template);
+        var link = temp(values);
+        return {url:link, jsCode: ""};
+    }
+
+    async getTrainTicketReserveLink(options):Promise<ReserveLink> {
+        let trafficBookLink = "http://m.ctrip.com/webapp/train/v2/index#!/list";
+        let indexBookLink = 'http://m.ctrip.com/webapp/train/v2/index';
+        let param = {
+            "value":
+                {
+                    "from": {
+                        "name": "",
+                        "cityName": ""
+                    },
+                    "to": {
+                        "name": "",
+                        "cityName": ""
+                    },
+                    "date": "",
+                    "isGaotie": false
+                },
+            "timeout":"",
+            "savedate":""
+        };
+        param.value.to.name = param.value.to.cityName = options.toCity;
+        param.value.from.name = param.value.from.cityName = options.fromCity;
+        param.value.date = options.leaveDate.getTime();
+        param.savedate = moment().format("YYYY/MM/DD HH:mm:ss");
+        param.timeout = moment().add(1, 'month').format("YYYY/MM/DD HH:mm:ss");
+
+        var param_str = JSON.stringify(param);
+        var linkJS = "localStorage.setItem('TRAIN_SEARCH_STORE_LIGHT', \'"+param_str+"\');console.log('train_search_param');";
+        return {url:trafficBookLink, indexUrl:indexBookLink, jsCode: linkJS};
+    }
+
+    // async queryFlightCityCode(city: string): Promise<string>{
+    //     var res = await this.client.post({
+    //         json: true,
+    //         uri: 'https://sec-m.ctrip.com/restapi/soa2/11783/Flight/Common/FlightSimilarNearAirportSearch/Query?_fxpcqlniredt=09031117210396050637',
+    //         form: {
+    //             head: {},
+    //             key: city,
+    //         },
+    //         headers: {
+    //             'Referer': 'http://m.ctrip.com/html5/flight/matrix.html',
+    //         },
+    //     })
+    //     if(res.body && res.body.fpairinfo && res.body.fpairinfo.length){
+    //         var arr = res.body.fpairinfo;
+    //         var code = arr[0].code;
+    //         return code;
+    //     }
+    //     return "";
+    // }
+    //
+    // async queryHotelCityCode(city: string): Promise<string>{
+    //     var requestPromise = require('request-promise');
+    //     var res = await this.client.post({
+    //         uri: 'http://m.ctrip.com/restapi/soa2/10932/hotel/static/destinationget?_fxpcqlniredt=09031117210396050637',
+    //         json: true,
+    //         form:{
+    //             head:{},
+    //             word: city,
+    //         },
+    //         headers: {
+    //             'Referer': 'http://m.ctrip.com/webapp/hotel/citylist',
+    //         },
+    //     })
+    //
+    //     if(res.body && res.body.keywords && res.body.keywords.length){
+    //         var arr = res.body.keywords;
+    //         var cityCode = arr[0].region['cid'];
+    //         var cityPy = arr[0].region['cengname'];
+    //         return cityPy + cityCode;
+    //     }
+    //     return "";
+    // }
+
+}
