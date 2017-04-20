bc2e075e485ab525630bb0b76f824c14dca3a7a2
Ke Peng <ke.peng@jingli365.com>
2017-04-20 23:24:34 +0800

    use @jingli/zone-setup

--- test/kepeng/sqltype.test.ts
+++ test/kepeng/sqltype.test.ts
@@ -3,7 +3,7 @@ import { Company } from '_types/company';
 import { Staff, EStaffRole } from '_types/staff';
 import { Models } from '_types/index';
 
-require('common/zone');
+require('@jingli/zone-setup');
 
 global.Promise = require('bluebird');
 Promise.promisifyAll(require("fs"));
