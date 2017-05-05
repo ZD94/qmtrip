c7d633e1fea1690c4ec821184a86e1e6ce242113
hexisen <616082046@qq.com>
2017-05-05 16:32:01 +0800

    change

--- api/company/sql/update.1705051606.sql
+++ api/company/sql/update.1705051606.sql
@@ -1,12 +1,12 @@
 INSERT INTO company.suppliers(
             id, name, traffic_book_link, hotel_book_link, logo, company_id, created_at, updated_at, supplier_key, type, is_in_use)
     VALUES ('2244b1a1-74c7-11e6-b1b9-f14ff84d5a8e', '去哪儿', 'https://touch.qunar.com/h5/flight', 'https://touch.qunar.com/hotel',
-    '5abf9f3e-fdd4-454e-a8f7-3d19b95f944e', null, now(), now(), 'qunar_com_m', 2, true);
+    '5abf9f3e-fdd4-454e-a8f7-3d19b95f944e', null, now(), now(), 'qunar_com_m', 3, true);
 
 INSERT INTO company.suppliers(
             id, name, traffic_book_link, hotel_book_link, logo, company_id, created_at, updated_at, supplier_key, type, is_in_use)
     VALUES ('c45ea130-13af-11e7-a4e1-75d4ee29fe4d', '飞猪', 'https://h5.m.taobao.com/trip/flight/search/index.html', 'https://h5.m.taobao.com/trip/hotel/search/index.html',
-    'c45ea130-13af-11e7-a4e1-75d4ee29fe4d', null, now(), now(), 'taobao_com', 2, true);
+    'c45ea130-13af-11e7-a4e1-75d4ee29fe4d', null, now(), now(), 'taobao_com', 3, true);
 
 
  INSERT INTO company.suppliers(
