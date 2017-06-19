8dbf73e03ef3f200e2f6448b253057733dd09f70
yuchanglong <changlong.yu@jingli365.com>
2017-06-19 09:49:40 +0800

    fix function duplicated definition

--- api/travelPolicy/index.ts
+++ api/travelPolicy/index.ts
@@ -507,20 +507,7 @@ class TravelPolicyModule{
         return travelp;
     }
 
-    /**
-     * 根据regionId 获取某个地区的差旅标准
-     * @param {String} params.id
-     * @param {Boolean} params.isReturnDefault 如果不存在返回默认 default true,
-     * @returns {*}
-     */
-    @clientExport
-    @requireParams(["regionId"], ["travelPolicyId"])
-    async getTravelPolicyByPlaceId (params:{id?: string, regionId:string}) :Promise<TravelPolicyRegion[]> {
-        let { id, regionId} =params;
-        let query = { where : { policyId: id,regionId: regionId }};
-        let travelp = await Models.travelPolicyRegion.all(query);
-        return travelp;
-    }
+
 
     @clientExport
     @requireParams(["policyId"])
