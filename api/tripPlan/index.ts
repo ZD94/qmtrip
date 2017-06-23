44c4cb2618f47172b566819027369cf0f0029e7d
hexisen <616082046@qq.com>
2017-06-23 16:26:29 +0800

    修改票据审核状态

--- api/tripPlan/index.ts
+++ api/tripPlan/index.ts
@@ -472,6 +472,14 @@ class TripPlanModule {
 
         //保存更改记录
         await Promise.all([tripPlan.save(), tripDetail.save()]);
+        //修改票据状态
+        let invoices = await tripDetail.getInvoices();
+        invoices.map((invoice)=>{
+            invoice.status = audit == EAuditStatus.INVOICE_PASS ? EInvoiceStatus.AUDIT_PASS : EInvoiceStatus.AUDIT_FAIL;
+            invoice.auditRemark = params.reason || '';
+            invoice.save();
+        });
+
 
         /*******************************************发送通知消息**********************************************/
         let staff = await Models.staff.get(tripPlan['accountId']);
