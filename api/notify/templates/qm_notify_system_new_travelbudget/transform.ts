1088c098cbd1c5c8c9e026ab9e742d0299da4799
wanglihui <lihui.wang@tulingdao.com>
2017-07-18 19:29:34 +0800

    修改重新生成预算错误

--- api/notify/templates/qm_notify_system_new_travelbudget/transform.ts
+++ api/notify/templates/qm_notify_system_new_travelbudget/transform.ts
@@ -15,7 +15,7 @@ export = async function transform(values: any): Promise<any>{
     values.MPlaneLevel =  MPlaneLevel;
     values.MTrainLevel =  MTrainLevel;
     values.MHotelLevel =  MHotelLevel;
-    let budgetInfo = await API.travelBudget.getBudgetInfo({id: values.cacheId});
+    let budgetInfo = await API.travelBudget.getBudgetInfo({ id: values.cacheId, accountId: staff.id});
     let {budgets, query} = budgetInfo;
     let totalBudget = 0;
     budgets.forEach((b) => {totalBudget += Number(b.price);});
