803f53e3b36146ecd30f702924ceaa25e0a111a8
hexisen <616082046@qq.com>
2017-08-28 18:47:32 +0800

    change

--- api/tripPlan/index.ts
+++ api/tripPlan/index.ts
@@ -1456,7 +1456,12 @@ class TripPlanModule {
         if (!staff.email) {
             throw L.ERR.EMAIL_EMPTY();
         }
-        let cities  = JSON.parse(tripPlan.arrivalCityCodes);
+        let cities;
+        try{
+            cities  = JSON.parse(tripPlan.arrivalCityCodes);
+        }catch(e){
+            cities = tripPlan.arrivalCityCodes || [];
+        }
         let firstDept = cities[0];
         let lastDept = cities[cities.length - 1];
         firstDept = await API.place.getCityInfo({cityCode: firstDept});
