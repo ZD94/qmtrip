2cdcf3c9aba0e8ebe5a087cd72cad21fed6a7b9f
李泽霖 <zelin.li@jingli365.com>
2017-09-13 20:03:13 +0800

    供应商迁移

--- api/company/index.ts
+++ api/company/index.ts
@@ -24,7 +24,7 @@ import {requirePermit, conditionDecorator, condition, modelNotNull} from "api/_d
 import {md5} from "common/utils";
 import { FindResult, PaginateInterface } from "common/model/interface";
 import {CoinAccount} from "_types/coin";
-import RestfulAPIUtil from "../RestfulAPIUtil"
+import RestfulAPIUtil from "../restfulAPIUtil"
 
 const supplierCols = Supplier['$fieldnames'];
 const companyCols = Staff['$getAllFieldNames']();
