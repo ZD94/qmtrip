7c2ef59f18652643dd9654561182cf22eadc0f55
changlong.yu <changlong.yu@jingli365.com>
2017-09-19 16:49:44 +0800

    beautify

--- api/staff/sql/update.1709191150.ts
+++ api/staff/sql/update.1709191150.ts
@@ -14,6 +14,8 @@ export = async function(db, transaction) {
         budgetUrl = 'postgres://times:time0418@l.jingli365.com:15432/t_jlbudget';
     } else if(/.+j.jingli365.com.+\/qmtrip/.test(config.postgres.url)) {
         budgetUrl = 'postgres://jingli:J1n9L1.t3ch@j.jingli365.com:5432/jlbudget';
+    } else if(/.+localhost.+\/times/.test(config.postgres.url)) {
+        budgetUrl = 'postgres://postgres:root@localhost:5432/jlbudget';
     }
     console.log(budgetUrl)
     if(!budgetUrl) return null;
@@ -25,18 +27,23 @@ export = async function(db, transaction) {
     let count = 0;
     for(let i =0; i < companies.length; i++ ){
         if(!companies[i].id || typeof(companies[i]) == undefined) continue;
-        let isEmpty = await db.query(`select count(*) as total from staff.staffs where company_id = '${companies[i].id}' and travel_policy_id is null and deleted_at is null;`, {type: Sequelize.QueryTypes.SELECT});
+        let isEmpty = await db.query(`select count(*) as total from staff.staffs where 
+                                      company_id = '${companies[i].id}' and travel_policy_id is null and 
+                                      deleted_at is null;`, {type: Sequelize.QueryTypes.SELECT});
          if(!isEmpty || isEmpty.length == 0 || !isEmpty[0].total || isEmpty[0].total == 0) continue;
 
-        let defaultTravelPolicySql = `select * from travel_policy.travel_policies where company_id = '${companies[i].id}' and  deleted_at is null;`;
+        let defaultTravelPolicySql = `select * from travel_policy.travel_policies where 
+                                      company_id = '${companies[i].id}' and  deleted_at is null;`;
         let defaultTravelPolicy = await db2.query(defaultTravelPolicySql, {type: Sequelize.QueryTypes.SELECT});
         if(defaultTravelPolicy && defaultTravelPolicy.length) {
-            let updateStaff = `update staff.staffs set travel_policy_id = '${defaultTravelPolicy[0].id}', updated_at = now() where company_id = '${companies[i].id}' and travel_policy_id is null;`;
+            let updateStaff = `update staff.staffs set travel_policy_id = '${defaultTravelPolicy[0].id}', updated_at = now() 
+                               where company_id = '${companies[i].id}' and travel_policy_id is null;`;
             try{
                 await db.query(updateStaff, {transaction: transaction});
-                transaction.commit();
             } catch(err) {
-                transaction.rollback();
+                if(err) {
+                   await transaction.rollback();
+                }
             }
         }
     }
