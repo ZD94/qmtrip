51e21e00d46d95b46ef5cd099b6ed47f0d69328e
changlong.yu <changlong.yu@jingli365.com>
2017-09-20 13:16:00 +0800

    采用resetful接口

--- api/staff/sql/update.1709191150.ts
+++ api/staff/sql/update.1709191150.ts
@@ -4,20 +4,10 @@
 'use strict';
 import Sequelize = require('sequelize');
 import config = require('@jingli/config');
+var request = require("request");
+var path = require("path");
 
 export = async function(db, transaction) {
-
-    let budgetUrl = null;
-    if(/[lt]\.jingli365\.com.+\/times/.test(config.postgres.url)) {
-        budgetUrl = 'postgres://times:time0418@l.jingli365.com:15432/jlbudget';
-    } else if(/[lt]\.jingli365\.com.+\/time_test/.test(config.postgres.url)){
-        budgetUrl = 'postgres://times:time0418@l.jingli365.com:15432/t_jlbudget';
-    } else if(/.+j.jingli365.com.+\/qmtrip/.test(config.postgres.url)) {
-        budgetUrl = 'postgres://jingli:J1n9L1.t3ch@j.jingli365.com:5432/jlbudget';
-    } 
-    console.log(budgetUrl)
-    if(!budgetUrl) return null;
-    let db2 = new Sequelize(budgetUrl);
 
     let companySql  = 'select * from company.companies where deleted_at is null';
     let companies = await db.query(companySql, {type: Sequelize.QueryTypes.SELECT});
@@ -25,26 +15,60 @@ export = async function(db, transaction) {
     let count = 0;
     for(let i =0; i < companies.length; i++ ){
         if(!companies[i].id || typeof(companies[i]) == undefined) continue;
-        let isEmpty = await db.query(`select count(*) as total from staff.staffs where 
-                                      company_id = '${companies[i].id}' and travel_policy_id is null and 
-                                      deleted_at is null;`, {type: Sequelize.QueryTypes.SELECT});
-         if(!isEmpty || isEmpty.length == 0 || !isEmpty[0].total || isEmpty[0].total == 0) continue;
 
-        let defaultTravelPolicySql = `select * from travel_policy.travel_policies where 
-                                      company_id = '${companies[i].id}' and  deleted_at is null;`;
-
-        let defaultTravelPolicy = await db2.query(defaultTravelPolicySql, {type: Sequelize.QueryTypes.SELECT});
-
-        if(defaultTravelPolicy && defaultTravelPolicy.length) {
-            let updateStaff = `update staff.staffs set travel_policy_id = '${defaultTravelPolicy[0].id}', updated_at = now() 
+        let travelPolicy = await requestTravelPolicy(companies[0].id, true);
+        if(typeof(travelPolicy) == 'string')  travelPolicy = JSON.parse(travelPolicy);
+        if(!travelPolicy || travelPolicy.length == 0) {
+            travelPolicy = await requestTravelPolicy(companies[0].id, false)
+            if(typeof(travelPolicy) == 'string')  travelPolicy = JSON.parse(travelPolicy);
+        }
+        if(travelPolicy && travelPolicy.length) {
+            let updateStaff = `update staff.staffs set travel_policy_id = '${travelPolicy[0].id}', updated_at = now()
                                where company_id = '${companies[i].id}' and travel_policy_id is null;`;
             try{
                 await db.query(updateStaff, {transaction: transaction});
             } catch(err) {
                 if(err) {
-                   await transaction.rollback();
+                    await transaction.rollback();
                 }
             }
         }
     }
+}
+
+
+async function requestTravelPolicy(companyId, isDefault){
+
+    let result = await new Promise<any>((resolve, reject) => {
+        let qs: { [index: string]: string} = {
+            companyId: companyId,
+            isDefault: isDefault
+        };
+        let url = config.cloudAPI + `/company/${companyId}/travelPolicy`;
+        console.log("url: ", url);
+        request({
+            uri: url,
+            qs: qs,
+            method: 'get',
+            headers: {
+                key: config.cloudKey
+            }
+        }, function(err,res){
+            if(err) {
+                console.log(err)
+                return resolve(null)
+            }
+            console.log(res.body);
+            let body = res.body;
+            if(typeof(body) == 'string') {
+                body = JSON.parse(body);
+            }
+            if(body && body.data && body.data.length) {
+                return resolve(body.data)
+            }
+            return resolve(null);
+        });
+    })
+    return result;
+
 }
