54511d6c94b88251d1819417bd51dda6121eb036
wangyali <yali.wang@jingli365.com>
2017-10-11 17:47:32 +0800

    delete console

--- api/notify/templates/qm_notify_system_new_travelbudget/transform.ts
+++ api/notify/templates/qm_notify_system_new_travelbudget/transform.ts
@@ -85,10 +85,6 @@ export = async function transform(values: any): Promise<any>{
     if(destinationPlacesInfo && destinationPlacesInfo.length > 0){
         await Promise.all(destinationPlacesInfo.map(async function(item, index){
             let arrivalInfo = await API.place.getCityInfo({cityCode: item.destinationPlace});
-            let a = moment(item.latestArrivalDateTime).tz(arrivalInfo.timezone).toDate();
-            let b = moment(item.earliestGoBackDateTime).tz(arrivalInfo.timezone).toDate();
-            console.info("a=====================", a);
-            console.info("b=====================", b);
             item.latestArrivalDateTime = moment(item.latestArrivalDateTime).tz(arrivalInfo.timezone).format("MM-DD HH:mm");
             item.earliestGoBackDateTime = moment(item.earliestGoBackDateTime).tz(arrivalInfo.timezone).format("MM-DD HH:mm");
             cityMap[item.destinationPlace] = arrivalInfo;
