0f9e8ee680aef35ac585e760f06f66a62a320c54
yuchanglong <changlong.yu@jingli365.com>
2017-11-08 14:35:57 +0800

    修改接口

--- api/shorturl/md5.ts
+++ api/shorturl/md5.ts
@@ -2,19 +2,26 @@
  * Created by wlh on 16/1/21.
  */
 var md5 = require("common/utils").md5;
-var sequelize = require("common/model").importModel("./models");
-var Models = sequelize.models;
+// var sequelize = require("common/model").importModel("./models");
+// var Models = sequelize.models;
+var Models = require("_types");
+import  {Url } from "_types/shorturl";
 
 var shorturl: any = {};
 export class Md5ShortUrl{
     async long2short(long) {
         var key = md5(long);
-        await Models.Url.findOrCreate({ where: {id: key, url: long}})
+        let urls = await Models.url.find({ where: {id: key, url: long}})
+        let urlObj;
+        if(urls || urls.length == 0) {
+             urlObj = Url.create({ where: {id: key, url: long}});
+            await urlObj.save();
+        }
         return key;
     }
 
     async short2long(short) {
-        let item = await Models.Url.findById(short);
+        let item = await Models.url.get(short);
         if (!item) {
             throw {code: -1, msg: "链接不存在或者已经失效"};
         }
--- api/shorturl/sql/init.sql
+++
@@ -1,7 +0,0 @@
---schema=shorturl
-
-create table IF NOT EXISTS shorturl.urls (
-    id varchar(50) primary key,
-    url text,
-    create_at timestamp default now()
-);
