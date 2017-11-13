3639ff7780150ef5772016a262e2c0347576a4df
yuchanglong <changlong.yu@jingli365.com>
2017-11-13 18:07:36 +0800

    fix conflict

--- api/place/index.ts
+++ api/place/index.ts
@@ -6,76 +6,121 @@ export default class PlaceModule {
     @clientExport
     @requireParams(['id'])
     static async getCityById(id) {
-        const city = await restfulAPIUtil.operateOnModel({
+        let city = await restfulAPIUtil.operateOnModel({
             model: `/place/${id}`,
             params: {
-                method: 'GET'
+                method: 'GET',
+                fields: {}
             }
         });
-        return city;
+        return city.data;
     }
 
     @clientExport
     static async findByKeyword(keyword: string) {
-        return await restfulAPIUtil.operateOnModel({
+        let city = await restfulAPIUtil.operateOnModel({
             model: `/place/search/${encodeURIComponent(keyword)}`,
             params: {
-                method: 'GET'
+                method: 'GET',
+                fields: {}
             }
         });
+        return city.data;
     }
 
     @clientExport
     static async findSubCities(parentId: string) {
-        return await restfulAPIUtil.operateOnModel({
+        let subcities = await restfulAPIUtil.operateOnModel({
             model: `/place/${parentId}/children`,
             params: {
-                method: 'GET'
+                method: 'GET',
+                fields: {}
             }
         });
+        return subcities.data;
     }
 
     @clientExport
     static async findNearCitiesByGC(longitude: number, latitude: number) {
-        return await restfulAPIUtil.operateOnModel({
+        let neighbors = await restfulAPIUtil.operateOnModel({
             model: `/place/nearby/${longitude}/${latitude}`,
             params: {
-                method: 'GET'
+                method: 'GET',
+                fields: {}
             }
         });
+
+        return neighbors.data;
     }
 
+    @clientExport
     static async getCitiesByLetter(params: {isAbroad?: boolean, letter?: string, limit?: number, page?: number, type: number}){
-        return restfulAPIUtil.operateOnModel({
+        let cities = await restfulAPIUtil.operateOnModel({
             model: `/place/getCitiesByLetter`,
             params: {
                 method: 'GET',
                 fields: params
             }
         });
+        return cities.data;
     }
 
-
+    @clientExport
     static async getCityInfoByName(params){
 
-        return restfulAPIUtil.operateOnModel({
+        let cities = await restfulAPIUtil.operateOnModel({
             model: `/place/getCityInfoByName`,
             params: {
                 method: 'GET',
                 fields: params
             }
         });
+        return cities.data;
     }
 
+    @clientExport
     static async getAirPortsByCity(params){
-        
-        return restfulAPIUtil.operateOnModel({
+        let airports = await restfulAPIUtil.operateOnModel({
             model: `/place/getAirPortsByCity`,
             params: {
                 method: 'GET',
                 fields: params
             }
         });
+        return airports.data;
+    }
+
+    @clientExport
+    static async getCityInfo(params){
+        let {cityCode } = params;
+        let city = await  PlaceModule.getCityById(cityCode);
+        console.log("city: ", city);
+        return city;
+
+    }
+
+    @clientExport
+    static async queryHotCity(params: {limit?: number, isAbroad: boolean }){
+        let cities = await  restfulAPIUtil.operateOnModel({
+            model: `/place`,
+            params: {
+                method: 'GET',
+                fields: params
+            }
+        });
+        return cities.data;
+    }
+
+    @clientExport
+    static async queryCity(params: {keyword?: number, isAbroad: boolean, max?: number }){
+        let cities = await  restfulAPIUtil.operateOnModel({
+            model: `/place`,
+            params: {
+                method: 'GET',
+                fields: params
+            }
+        });
+        return cities.data;
     }
 
 }
