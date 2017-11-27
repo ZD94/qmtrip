77adacef342d338b2da3e1b438ff53b94302fc06
wangyali <yali.wang@jingli365.com>
2017-11-27 19:14:44 +0800

    update sql

--- api/approve/sql/init.sql
+++ api/approve/sql/init.sql
@@ -1,3 +1,3 @@
 
 --schema=approve
-update approve.approves a set (budget , old_budget) = (select budget, old_budget from trip_plan.trip_approves b where a.id = b.id);
+--update approve.approves a set (budget , old_budget) = (select budget, old_budget from trip_plan.trip_approves b where a.id = b.id);
