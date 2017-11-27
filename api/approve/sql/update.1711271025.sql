08cb7ef661c2affbfef1f0a18e6aad755ddd23a7
wangyali <yali.wang@jingli365.com>
2017-11-27 19:03:29 +0800

    修改sql

--- api/approve/sql/update.1711271025.sql
+++ api/approve/sql/update.1711271025.sql
@@ -1,1 +1,1 @@
-update approve.approves a set (budget , old_budget) = (select budget, old_budget from trip_plan.trip_approves b where a.id = b.id);
+--update approve.approves a set (budget , old_budget) = (select budget, old_budget from trip_plan.trip_approves b where a.id = b.id);
