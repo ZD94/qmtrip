bf07762187c42d6203c28c19014be207dadf1771
yuchanglong <changlong.yu@jingli365.com>
2017-12-21 20:06:19 +0800

    set ordertype

--- api/proxy/index.ts
+++ api/proxy/index.ts
@@ -3,7 +3,7 @@ import {Request, Response} from "express-serve-static-core";
 import {parseAuthString} from "_types/auth/auth-cert";
 import { Staff } from "_types/staff";
 import { Models } from "_types";
-import { EOrderStatus } from "_types/tripPlan";
+import { EOrderStatus, EOrderType } from "_types/tripPlan";
 var request = require("request");
 var path = require("path");
 var _ = require("lodash");
@@ -113,15 +113,19 @@ class Proxy {
                 if(result.data.orderNos && typeof(result.data.orderNos) != 'undefined'){
                     tripDetail.reserveStatus = EOrderStatus.AUDITING;  //飞机的orderNos为数组
                     tripDetail.orderNo = result.data.orderNos[0];
+                    tripDetail.orderType = EOrderType.PLANE;
                 }
                 if(result.data.OrderNo && typeof(result.data.OrderNo) != 'undefined') {  
-                    tripDetail.reserveStatus = EOrderStatus.AUDITING;  //飞机的orderNos为数组
+                    tripDetail.reserveStatus = EOrderStatus.AUDITING;  //火车的OrderNo为数组
                     tripDetail.orderNo = result.data.OrderNo;
+                    tripDetail.orderType = EOrderType.TRAIN;
                 }  
                 if(result.data.orderNo && typeof(result.data.orderNo) != 'undefined') {  
                     tripDetail.reserveStatus = EOrderStatus.AUDITING;   //酒店的orderNo为string
                     tripDetail.orderNo = result.data.orderNo;
+                    tripDetail.orderType = EOrderType.HOTEL;
                 }
+                // tripDetail.orderType = body.orderType? body.orderType: null;  //后期返回的orderNo统一后，使用此确定订单类型
                 await tripDetail.save();
             }
             return res.json(result);
