c2385e56b0ea7a11d06c1764ebfe37f75faaaf44
yuchanglong <changlong.yu@jingli365.com>
2017-12-25 10:06:37 +0800

    调整java的链接

--- api/proxy/index.ts
+++ api/proxy/index.ts
@@ -30,181 +30,184 @@ class Proxy {
      */
     static __initHttpApp(app: Express){
 
-        app.options(/order*/, cors(corsOptions), (req: Request, res: Response, next: Function) => {  
+        // app.options(/order*/, cors(corsOptions), (req: Request, res: Response, next: Function) => {  
         
-        });
-
-
-        // app.options(/order*/, cors(corsOptions), (req: Request, res: Response, next: Function) => {         
-        //     return res.sendStatus(200);
-        // })
-
-        // verifyToken
-
-        // app.all(/travel.*/, cors(corsOptions), resetTimeout, timeout('120s'), async (req: any, res: Response, next: Function) => {
-        //     if (req.method == 'OPTIONS') {
-        //         return next();
-        //     }
-        //     console.log('---------query--------->', req.query);
-        //     console.log('---------body---------->', req.body);
-            
-        //     //qmtrip验证
-        //     let {authstr, staffid}  = req.headers;
-        //     let token: AuthRequest = parseAuthString(authstr);
-        //     let verification = await API.auth.authentication(token);
-        //     if (!verification) {
-        //         console.log('authentic failed!', JSON.stringify(req.cookies));
-        //         res.sendStatus(401);
-        //         return;
-        //     }
-
-        //     //公有云验证
-        //     // let staff: Staff = await Staff.getCurrent();
-        //     let staff: Staff = await Models.staff.get(staffid);
-        //     let companyId: string = staff.company.id;
-        //     let companyToken: string = await getCompanyTokenByAgent(companyId);
-        //     if (!companyToken) {
-        //         throw new Error('换取 token 失败！');
-        //     }
-            
-        //     //request to JLCloud(jlbudget) 
-        //     let result: any;
-        //     let pathstr: string = req.path;
-        //     pathstr = pathstr.replace('/travel', '');
-        //     let JLOpenApi: string = config.cloud;
-        //     JLOpenApi.replace('/cloud', '');
-        //     let url: string = `${JLOpenApi}${pathstr}`;
-
-        //     try {
-        //         result = await new Promise((resolve, reject) => {
-        //             return request({
-        //                 uri: url,
-        //                 body: req.body,
-        //                 json: true,
-        //                 method: req.method,
-        //                 qs: req.query,
-        //                 headers: {
-        //                     token: companyToken
-        //                 }
-        //             }, (err, resp, result) => {
-        //                 if (err) {
-        //                     reject(err);
-        //                 }
-        //                 resolve(result);
-        //             });
-        //         });
-        //         return res.json(result);
-        //     } catch(err) {
-        //         if (err) {
-        //             console.error('ERROR TRAVEL In api/proxy/index:   ', err);
-        //             return null;
-        //         }
-        //     }
-        // })
-
-        // // verifyToken
-        // app.all(/order.*/, cors(corsOptions),resetTimeout, timeout('120s'), async (req: Request, res: Response, next: Function) => {
-        //     if(req.method == 'OPTIONS') {
-        //         return next();
-        //     }
-
-        //     let {tripDetailId} = req.query;
-        //     if(!tripDetailId || typeof tripDetailId == undefined)
-        //         tripDetailId = req.body.tripDetailId;
-        //     let tripDetail = await Models.tripDetail.get(tripDetailId)
-        //     if(!tripDetail) {
-        //         console.log("tripDetail not found");
-        //         return res.sendStatus(403);
-        //     }
-
-        //     let staff: Staff = await Staff.getCurrent();
-        //     if(!staff) {
-        //         staff = await Models.staff.get(tripDetail.accountId);
-        //     }
-      
-        //     let addon:{[index: string]: any} = {
-        //         staffID: staff.id,
-        //         companyID: staff.companyId,
-        //         listeningon: `${config.orderSysConfig.tripDetailMonitorUrl}/${tripDetail.id}`
-        //     };
-        //     let headers: {[index: string]: any} = {
-        //        auth: req.headers['auth'],
-        //        supplier: req.headers['supplier'],
-        //        listeningon: `${config.orderSysConfig.tripDetailMonitorUrl}/${tripDetail.id}`
-        //     }
-
-        //     let body: {[index: string]: any} = req.body;
-
-        //     if(req.method == 'GET') {
-        //         _.assign(headers, addon)
-        //     }
-        //     if(req.method == 'POST') {
-        //         _.assign(headers, addon)
-        //         _.assign(body, addon);
-        //     }
-        //     let pathstring = req.path;
-        //     pathstring = pathstring.replace("/order", '');
-        //     let url = `${config.orderSysConfig.orderLink}${pathstring}`;
-        //     let result:any;
-        //     console.log("===========url: ", url, '===tripDetailId: ', tripDetailId, '====>method:', req.method, '=======> body: ', req.body);
-        //     try{
-        //         result = await new Promise((resolve,reject) => {  
-        //             request({ url, headers, body, 
-        //                 json: true,
-        //                 method: req.method,
-        //                 timeout: 120*1000
-        //             }, (err: Error, res: any, body: any) => {
-        //                 if(err) {
-        //                     console.log("-=========>err: ", err);
-        //                     reject(err)
-        //                 }
-        //                 resolve(body);
-        //             });
-        //         });
-        //     }catch(err) {
-        //         if(err) {
-        //             console.log("请求预定错误: ", err)
-        //             return null;
-        //         }  
-        //     }
-        //     console.log("========================> result.", result)
-        //     if(!result) 
-        //         return res.json(null);
-        //     //以下提交订单成功后，更新订单状态和订单号
-        //     if(typeof result == 'string') {
-        //         result = JSON.parse(result);
-        //     }
-        //     if(result.code == 0 && result.data && tripDetail.orderNo == null){  //&& result.data.orderN
-        //         if(result.data.orderNos && typeof(result.data.orderNos) != 'undefined'){
-        //             tripDetail.reserveStatus = EOrderStatus.AUDITING;  //飞机的orderNos为数组
-        //             tripDetail.orderNo = result.data.orderNos[0];
-        //         }
-        //         if(result.data.OrderNo && typeof(result.data.OrderNo) != 'undefined') {  
-        //             tripDetail.reserveStatus = EOrderStatus.AUDITING;  //火车的OrderNo为string
-        //             tripDetail.orderNo = result.data.OrderNo;
-        //         }  
-        //         if(result.data.orderNo && typeof(result.data.orderNo) != 'undefined') {  
-        //             tripDetail.reserveStatus = EOrderStatus.AUDITING;   //酒店的orderNo为string
-        //             tripDetail.orderNo = result.data.orderNo;
-        //         }
-        //         tripDetail.orderType = body.orderType != null? body.orderType: null;  //后期返回的orderNo统一后，使用此确定订单类型
-        //         await tripDetail.save();
-        //     }
-        //     return res.json(result);
-
         // });
 
+
+        app.options(/^\/order*/, cors(corsOptions), (req: Request, res: Response, next: Function) => {         
+            return res.sendStatus(200);
+        })
+
         // verifyToken
-        app.all(/mall.*/ , async (req: Request, res: Response, next: Function)=> {
+
+        app.all(/^\/travel.*$/, cors(corsOptions), resetTimeout, timeout('120s'), async (req: any, res: Response, next: Function) => {
+            if (req.method == 'OPTIONS') {
+                return next();
+            }
+            console.log('---------query--------->', req.query);
+            console.log('---------body---------->', req.body);
+            
+            //qmtrip验证
+            let {authstr, staffid}  = req.headers;
+            let token: AuthRequest = parseAuthString(authstr);
+            let verification = await API.auth.authentication(token);
+            if (!verification) {
+                console.log('authentic failed!', JSON.stringify(req.cookies));
+                res.sendStatus(401);
+                return;
+            }
+
+            //公有云验证
+            // let staff: Staff = await Staff.getCurrent();
+            let staff: Staff = await Models.staff.get(staffid);
+            let companyId: string = staff.company.id;
+            let companyToken: string = await getCompanyTokenByAgent(companyId);
+            if (!companyToken) {
+                throw new Error('换取 token 失败！');
+            }
+            
+            //request to JLCloud(jlbudget) 
+            let result: any;
+            let pathstr: string = req.path;
+            pathstr = pathstr.replace('/travel', '');
+            let JLOpenApi: string = config.cloud;
+            JLOpenApi.replace('/cloud', '');
+            let url: string = `${JLOpenApi}${pathstr}`;
+
+            try {
+                result = await new Promise((resolve, reject) => {
+                    return request({
+                        uri: url,
+                        body: req.body,
+                        json: true,
+                        method: req.method,
+                        qs: req.query,
+                        headers: {
+                            token: companyToken
+                        }
+                    }, (err, resp, result) => {
+                        if (err) {
+                            reject(err);
+                        }
+                        resolve(result);
+                    });
+                });
+                return res.json(result);
+            } catch(err) {
+                if (err) {
+                    console.error('ERROR TRAVEL In api/proxy/index:   ', err);
+                    return null;
+                }
+            }
+        })
+
+        // verifyToken
+        app.all(/^\/order.*$/, cors(corsOptions),resetTimeout, timeout('120s'), async (req: Request, res: Response, next: Function) => {
+            if(req.method == 'OPTIONS') {
+                return next();
+            }
+            console.log("=====this is order")
+
+            let {tripDetailId} = req.query;
+            if(!tripDetailId || typeof tripDetailId == undefined)
+                tripDetailId = req.body.tripDetailId;
+            let tripDetail = await Models.tripDetail.get(tripDetailId)
+            if(!tripDetail) {
+                console.log("tripDetail not found");
+                return res.sendStatus(403);
+            }
+
+            let staff: Staff = await Staff.getCurrent();
+            if(!staff) {
+                staff = await Models.staff.get(tripDetail.accountId);
+            }
+      
+            let addon:{[index: string]: any} = {
+                staffID: staff.id,
+                companyID: staff.companyId,
+                listeningon: `${config.orderSysConfig.tripDetailMonitorUrl}/${tripDetail.id}`
+            };
+            let headers: {[index: string]: any} = {
+               auth: req.headers['auth'],
+               supplier: req.headers['supplier'],
+               listeningon: `${config.orderSysConfig.tripDetailMonitorUrl}/${tripDetail.id}`
+            }
+
+            let body: {[index: string]: any} = req.body;
+
+            if(req.method == 'GET') {
+                _.assign(headers, addon)
+            }
+            if(req.method == 'POST') {
+                _.assign(headers, addon)
+                _.assign(body, addon);
+            }
+            let pathstring = req.path;
+            pathstring = pathstring.replace("/order", '');
+            let url = `${config.orderSysConfig.orderLink}${pathstring}`;
+            let result:any;
+            console.log("===========url: ", url, '===tripDetailId: ', tripDetailId, '====>method:', req.method, '=======> body: ', req.body);
+            try{
+                result = await new Promise((resolve,reject) => {  
+                    request({ url, headers, body, 
+                        json: true,
+                        method: req.method,
+                        timeout: 120*1000
+                    }, (err: Error, res: any, body: any) => {
+                        if(err) {
+                            console.log("-=========>err: ", err);
+                            reject(err)
+                        }
+                        resolve(body);
+                    });
+                });
+            }catch(err) {
+                if(err) {
+                    console.log("请求预定错误: ", err)
+                    return null;
+                }  
+            }
+            console.log("========================> result.", result)
+            if(!result) 
+                return res.json(null);
+            //以下提交订单成功后，更新订单状态和订单号
+            if(typeof result == 'string') {
+                result = JSON.parse(result);
+            }
+            if(result.code == 0 && result.data && tripDetail.orderNo == null){  //&& result.data.orderN
+                if(result.data.orderNos && typeof(result.data.orderNos) != 'undefined'){
+                    tripDetail.reserveStatus = EOrderStatus.AUDITING;  //飞机的orderNos为数组
+                    tripDetail.orderNo = result.data.orderNos[0];
+                }
+                if(result.data.OrderNo && typeof(result.data.OrderNo) != 'undefined') {  
+                    tripDetail.reserveStatus = EOrderStatus.AUDITING;  //火车的OrderNo为string
+                    tripDetail.orderNo = result.data.OrderNo;
+                }  
+                if(result.data.orderNo && typeof(result.data.orderNo) != 'undefined') {  
+                    tripDetail.reserveStatus = EOrderStatus.AUDITING;   //酒店的orderNo为string
+                    tripDetail.orderNo = result.data.orderNo;
+                }
+                tripDetail.orderType = body.orderType != null? body.orderType: null;  //后期返回的orderNo统一后，使用此确定订单类型
+                await tripDetail.save();
+            }
+            return res.json(result);
+
+        });
+
+        
+        app.all(/^\/mall.*$/ ,cors(corsOptions),resetTimeout, timeout('120s'), verifyToken, async (req: Request, res: Response, next: Function)=> {
             console.log("======hello world, ", req.body)
             let params =  req.body;
+            if(req.method == 'GET') 
+                params = req.query;
             let appSecret = config.mall.appSecret;
             console.log('secret:', appSecret)
             let pathstring = req.path;
             pathstring = pathstring.replace("/mall", '');
             let sign = genSign(params, Math.floor(Date.now()/1000), appSecret)
             let url = `${config.mall.orderLink}${pathstring}`;
-            console.log("======hello world, sign", sign) 
+            console.log("===>sign", sign, '====>url', url, 'appid: ', config.mall.appId) 
             let result = await new Promise((resolve, reject) => {
                 return request({
                     uri: url,
@@ -214,7 +217,7 @@ class Proxy {
                     qs: req.query,
                     headers: {
                         sign: sign,
-                        appid: config.order.appid
+                        appid: config.mall.appId
                     }
                 }, (err, resp, result) => {
                     if (err) {
@@ -223,8 +226,8 @@ class Proxy {
                     resolve(result);
                 });
             });
-
-            return result;
+            console.log("======.result: ", result)
+            return res.json(result);
         });
         
         
@@ -235,7 +238,7 @@ export default Proxy;
 
 async function verify(req: Request, res: Response, next: Function) {
     let {authstr, staffid} = req.headers;
-    console.log("======> authstr ", authstr)
+    console.log("======> authstr ", authstr, staffid)
     let token = parseAuthString(authstr);
     let verification: AuthResponse = await API.auth.authentication(token);
     if(!verification) {
@@ -255,12 +258,12 @@ async function verify(req: Request, res: Response, next: Function) {
 }
 var verifyToken = CLSNS.bind(verify, CLSNS.createContext())
 
- function getJLSign(params: any, appSerect: string) {
-    const timestamp = Math.ceil(Date.now()/1000);
-    console.log("======hello world, timestamp", timestamp) 
-    let sign = genSign(params, timestamp, appSerect);
-    return sign;
-}
+//  function getJLSign(params: any, appSerect: string) {
+//     const timestamp = Math.ceil(Date.now()/1000);
+//     console.log("======hello world, timestamp", timestamp) 
+//     let sign = genSign(params, timestamp, appSerect);
+//     return sign;
+// }
 
 
 
