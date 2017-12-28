857c787a3949c607f7611e7b564e8f5af553de16
wanglihui <lihui.wang@tulingdao.com>
2017-12-29 01:19:29 +0800

    try to scrub Error

--- libs/patch-scrub-error.ts
+++ libs/patch-scrub-error.ts
@@ -2,8 +2,20 @@ import { registerClass } from "@jingli/patch-dnode-scrub";
 import { ERROR_CODE_C } from '@jingli/language';
 
 registerClass(
-    ERROR_CODE_C, "ERROR_CODE_C", (obj) => {
-        return obj.toJSON();
+    ERROR_CODE_C, null, (obj) => {
+        return { __class: "ERROR_CODE_C", value: obj.toJSON()}
     }, (obj: any) => { 
         return new ERROR_CODE_C(obj.code, obj.msg);
     })
+
+registerClass(
+    Error,
+    null,
+    (err) => { 
+        if (err['error@context']) { 
+            delete err['error@context'];
+        }
+        return err;
+    },
+    null,
+)
