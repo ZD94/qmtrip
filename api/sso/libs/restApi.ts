4d3ee11b213e88eb57cde5c6d5ed45b0166dd773
lisongwei <1429594204@qq.com>
2017-12-29 20:59:22 +0800

    Add getAccessToeknByPermanentCode

--- api/sso/libs/restApi.ts
+++ api/sso/libs/restApi.ts
@@ -1,6 +1,6 @@
 import { WStaff, IWStaff } from "api/sso/libs/wechat-staff";
 import { IWDepartment } from "api/sso/libs/wechat-department";
-import {Express} from "express-serve-static-core";
+import { Express } from "express-serve-static-core";
 var request = require("request-promise");
 
 
@@ -13,8 +13,26 @@ const enum EIteratorSwitch {
 export class RestApi {
 
     access_token: string;
-    constructor(access_token: string){
+    constructor(access_token: string) {
         this.access_token = access_token;
+    }
+
+    static async getAccessTokenByPermanentCode(corpId: string, permanentCode: string, suiteAccessToken: string) {
+        let url = 'https://qyapi.weixin.qq.com/cgi-bin/service/get_corp_token?suite_access_token=' + suiteAccessToken
+        const res = await reqProxy({
+            url,
+            method: 'POST',
+            body: {
+                auth_corpid: corpId,
+                permanent_code: permanentCode
+            }
+        })
+        if (res.errcode == 0)
+            return {
+                accessToken: res.access_token,
+                expiresIn: res.expires_in
+            }
+        return null
     }
 
     /**
@@ -26,7 +44,7 @@ export class RestApi {
             url,
             method: 'POST',
             body: {
-                auth_code: authCode 
+                auth_code: authCode
             }
         });
         return {
@@ -45,20 +63,20 @@ export class RestApi {
     }
 
     /**根据corpid和secret获取该公司的access_token */
-    static async getAccessToken(corpid: string, secret: string): Promise<IAccessToken>{
+    static async getAccessToken(corpid: string, secret: string): Promise<IAccessToken> {
         let url = `https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${corpid}&corpsecret==${secret}`;
         // let url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wwb398745b82d67068&corpsecret=x51OLfe5UWqI5VEW2nXg6tAph5P8kPqmJ_RxtgnbPBE'
         let result: IAccessToken = await reqProxy({
             url,
             method: 'GET'
         });
-        if(result && result.errcode == 0) 
+        if (result && result.errcode == 0)
             return result;
         return null;
     }
 
     /**根据该公司的access_token获取某个staff的信息 */
-    async getStaff(userid?: string){
+    async getStaff(userid?: string) {
         let url = `https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=${this.access_token}&userid=${userid}`;
         let result: IWStaffResult = await reqProxy({
             url,
@@ -94,78 +112,78 @@ export class RestApi {
     // }
 
 
-        /**
-         * @method 根据该公司的access_token获取某个staff的信息 
-         * @param.id {string} 可选， 获取指定部门及其子部门。若不填，默认获取全量组织架构
-         * @return {IDepartmentResult}
-         */
-        async getDepartments(id?: string): Promise<Array<IWDepartment>>{
-            let url = `https://qyapi.weixin.qq.com/cgi-bin/department/list`;
-            let qs: {access_token: string, id?: string} = {
-                access_token: this.access_token
-            }
-            if(id) qs['id'] = id;
-            console.log("=====this department: ", qs, url)
-            let result: IDepartmentResult = await reqProxy({
-                url,
-                method: 'GET',
-                qs: qs
-            });
-            if(!result) return null;
-            return result.department;
+    /**
+     * @method 根据该公司的access_token获取某个staff的信息 
+     * @param.id {string} 可选， 获取指定部门及其子部门。若不填，默认获取全量组织架构
+     * @return {IDepartmentResult}
+     */
+    async getDepartments(id?: string): Promise<Array<IWDepartment>> {
+        let url = `https://qyapi.weixin.qq.com/cgi-bin/department/list`;
+        let qs: { access_token: string, id?: string } = {
+            access_token: this.access_token
         }
+        if (id) qs['id'] = id;
+        console.log("=====this department: ", qs, url)
+        let result: IDepartmentResult = await reqProxy({
+            url,
+            method: 'GET',
+            qs: qs
+        });
+        if (!result) return null;
+        return result.department;
+    }
 
-        /**
-         * @method 根据该公司的access_token获取部门成员信息
-         * @param.departmentId {string} 部门id
-         * @param.fetchChild {number} 0: 关闭递归获取子部门下面的成员 1: 递归获取所有部门成员
-         * @return {IMemberListResult}
-         */
-        async getDetailedStaffsByDepartment(departmentId?: string, fetchChild?: number): Promise<Array<IWStaff>>{
-            let url = 'https://qyapi.weixin.qq.com/cgi-bin/user/list';
-            let qs: {
-                access_token: string,
-                department_id: string,
-                fetch_child?: number
-            } = {
+    /**
+     * @method 根据该公司的access_token获取部门成员信息
+     * @param.departmentId {string} 部门id
+     * @param.fetchChild {number} 0: 关闭递归获取子部门下面的成员 1: 递归获取所有部门成员
+     * @return {IMemberListResult}
+     */
+    async getDetailedStaffsByDepartment(departmentId?: string, fetchChild?: number): Promise<Array<IWStaff>> {
+        let url = 'https://qyapi.weixin.qq.com/cgi-bin/user/list';
+        let qs: {
+            access_token: string,
+            department_id: string,
+            fetch_child?: number
+        } = {
                 access_token: this.access_token,
                 department_id: departmentId
             }
-            if(fetchChild) qs.fetch_child = fetchChild;
-            let result: IMemberListResult = await reqProxy({
-                url,
-                method: 'GET',
-                qs
-            });
-            if(!result || !result.userlist)
-                return null;
-            return result.userlist;
-        }
+        if (fetchChild) qs.fetch_child = fetchChild;
+        let result: IMemberListResult = await reqProxy({
+            url,
+            method: 'GET',
+            qs
+        });
+        if (!result || !result.userlist)
+            return null;
+        return result.userlist;
+    }
 
-        /**
-         * @method 根据该公司的access_token获取部门成员简单信息
-         * @param.departmentId {string} 部门id
-         * @param.fetchChild {number} 是否递归获取子部门下面的成员
-         * @return {IConciseMemberListResult}
-         */
-        async getConciseStaffsByDepartment(departmentId?: string, fetchChild?: number): Promise<IConciseMemberListResult>{
-            let url = 'https://qyapi.weixin.qq.com/cgi-bin/user/simplelist';
-            let qs: {
-                access_token: string,
-                department_id: string,
-                fetch_child?: number
-            } = {
+    /**
+     * @method 根据该公司的access_token获取部门成员简单信息
+     * @param.departmentId {string} 部门id
+     * @param.fetchChild {number} 是否递归获取子部门下面的成员
+     * @return {IConciseMemberListResult}
+     */
+    async getConciseStaffsByDepartment(departmentId?: string, fetchChild?: number): Promise<IConciseMemberListResult> {
+        let url = 'https://qyapi.weixin.qq.com/cgi-bin/user/simplelist';
+        let qs: {
+            access_token: string,
+            department_id: string,
+            fetch_child?: number
+        } = {
                 access_token: this.access_token,
                 department_id: departmentId,
             }
-            if(fetchChild) qs.fetch_child = fetchChild;
-            let result: IConciseMemberListResult = await reqProxy({
-                url,
-                method: 'GET',
-                qs
-            });
-            return result;
-        }
+        if (fetchChild) qs.fetch_child = fetchChild;
+        let result: IConciseMemberListResult = await reqProxy({
+            url,
+            method: 'GET',
+            qs
+        });
+        return result;
+    }
 }
 
 
@@ -174,19 +192,19 @@ async function reqProxy(params: {
     method: string,
     body?: any,
     qs?: any
-}): Promise<any>{
-    let {url, method, body = {}, qs = {}} = params;
+}): Promise<any> {
+    let { url, method, body = {}, qs = {} } = params;
     let result;
     let options
-    try{
-        result = await request({ url, method, body, qs, json: true});
-    }catch(err) {
-        if(err)
+    try {
+        result = await request({ url, method, body, qs, json: true });
+    } catch (err) {
+        if (err)
             throw new Error(err);
     }
-    if(typeof result == 'string')
+    if (typeof result == 'string')
         result = JSON.parse(result);
-    if(!result) 
+    if (!result)
         return null;
     return result;
 }
@@ -234,7 +252,7 @@ export interface IWPermanentCodeResult {
     access_token: string;
     expires_in: number;
     permanent_code: string;
-    auth_corp_info : {
+    auth_corp_info: {
         corpid: string,
         corp_name: string,
         corp_type: string,
@@ -260,7 +278,7 @@ export interface IWPermanentCodeResult {
 export interface IWPermanentCode {
     accessToken: string,
     permanentCode: string,
-    corpId: string, 
+    corpId: string,
     corpName: string,
     authUserInfo: {
         email: string,
