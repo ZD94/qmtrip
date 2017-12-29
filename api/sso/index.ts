d2af844f3d322ce22953e448afa6115a44a41d61
lisongwei <1429594204@qq.com>
2017-12-29 19:45:08 +0800

    Delete http/controller/wechat and modify api/sso/index

--- api/sso/index.ts
+++ api/sso/index.ts
@@ -1,26 +1,23 @@
 import { L } from '@jingli/language';
 import { clientExport, requireParams } from '@jingli/dnode-api/dist/src/helper';
 import cache from 'common/cache'
+import { Request, NextFunction, Response, Application } from 'express-serve-static-core';
 const API = require('@jingli/dnode-api')
 const config = require('@jingli/config')
+const { Parser } = require('xml2js')
+const wxCrypto = require('wechat-crypto')
+
+const crypto = new wxCrypto(config.workWechat.token, config.workWechat.encodingAESKey, config.workWechat.corpId)
 
 const axios = require('axios')
-const PROVIDER_TOKEN_URL = `https://qyapi.weixin.qq.com/cgi-bin/service/get_provider_token`
 const SUITE_TOKEN_URL = 'https://qyapi.weixin.qq.com/cgi-bin/service/get_suite_token'
 const USER_INFO_URL = 'https://qyapi.weixin.qq.com/cgi-bin/service/getuserinfo3rd'
 
 export default class SSOModule {
 
-    @clientExport
-    static async getProviderToken() {
-        const res = await axios.post(PROVIDER_TOKEN_URL, {
-            corpid: config.workWechat.corpId,
-            provider_secret: config.workWechat.providerSecret
-        })
-        if (res.status == 200 && res.data.errcode == 0) {
-            return res.data.provider_access_token
-        }
-        throw new L.ERROR_CODE_C(500, '获取服务商 token 失败')
+    static __initHttpApp(app: Application) {
+        app.all('/wechat/receive', receive)
+        app.all('/wechat/data/callback', dataCallback)
     }
 
     @clientExport
@@ -29,7 +26,7 @@ export default class SSOModule {
         if (suite_token) return suite_token
 
         const suite_ticket = await cache.read('suite_ticket')
-        if(!suite_ticket) throw new L.ERROR_CODE_C(500, '数据回调处理异常')
+        if (!suite_ticket) throw new L.ERROR_CODE_C(500, '数据回调处理异常')
         const res = await axios.post(SUITE_TOKEN_URL, {
             suite_id: config.workWechat.suiteId,
             suite_secret: config.workWechat.suiteSecret,
@@ -46,11 +43,68 @@ export default class SSOModule {
     @requireParams(['code'])
     static async getUserInfo({ code }: { code: string }): Promise<string> {
         const suite_token = await API.sso.getSuiteToken()
-        console.log('suite_token:', suite_token)
         const res = await axios.get(`${USER_INFO_URL}?access_token=${suite_token}&code=${code}`)
         if (res.status == 200 && res.data.errcode == 0) {
             return res.data.UserId
         }
         throw new L.ERROR_CODE_C(500, "获取用户信息失败")
     }
+}
+
+async function receive(req: Request, res: Response) {
+    const { timestamp, nonce, msg_signature, echostr } = req.query
+    if (echostr) {
+        if (msg_signature != crypto.getSignature(timestamp, nonce, echostr)) {
+            return res.sendStatus(403)
+        }
+        return res.send(crypto.decrypt(echostr).message)
+    }
+
+    let rawBody = ''
+    req.setEncoding('utf8')
+
+    req.on('data', chunk => {
+        rawBody += chunk
+    })
+    req.on('end', () => {
+        if (rawBody == '') {
+            return res.sendStatus(403)
+        }
+        new Parser().parseString(rawBody, (err, data) => {
+            const resp = crypto.decrypt(data.xml['Encrypt'][0])
+            return res.send(resp.message)
+        })
+    })
+}
+
+async function dataCallback(req: Request, res: Response, next: NextFunction) {
+    const { timestamp, nonce, msg_signature, echostr } = req.query
+    if (req.method.toUpperCase() == 'GET') {
+        if (msg_signature != crypto.getSignature(timestamp, nonce, echostr)) {
+            return res.sendStatus(403)
+        }
+        return res.send(crypto.decrypt(echostr).message)
+    }
+
+    let rawBody = ''
+    req.setEncoding('utf8')
+
+    req.on('data', chunk => {
+        rawBody += chunk
+    })
+    req.on('end', () => {
+        if (rawBody == '') {
+            return res.sendStatus(403)
+        }
+        new Parser().parseString(rawBody, (err, data) => {
+            const resp = crypto.decrypt(data.xml['Encrypt'][0])
+            new Parser().parseString(resp.message, async (err, data) => {
+                if (data.xml['InfoType'] == 'suite_ticket')
+                    await cache.write('suite_ticket', data.xml['SuiteTicket'][0])
+                // if (data.xml['InfoType'] == 'create_auth')
+                //     await cache.write('create_auth', data.xml['AuthCode'])
+                res.send('success')
+            })
+        })
+    })
 }
