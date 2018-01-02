983dd6d663baded92dfa677e169f35fac7e48012
wangyali <yali.wang@jingli365.com>
2018-01-02 16:46:40 +0800

    修改网信共享秘钥

--- server.js
+++ server.js
@@ -92,14 +92,14 @@ server.on('init.api', function(API){
         initData.initDataForTest({id: "60294e10-1448-11e7-aa89-6b4a98eecf40", name: '笑傲江湖', userName: '风清扬', mobile: '13700000001', pwd: '123456', email: 'fq.yang@jingli.tech'});
     }
 
-    /*if(cluster.isMaster && config.wxSysCode){
+    if(cluster.isMaster && config.wxSysCode){
         WX.syncOrganization();
-        /!*let key = "ZjBmMjU0NzAtMWI3Yi0xMWU3LTk4NzUtYTdkOWQ4MmY=";
+        /*let key = "ZjBmMjU0NzAtMWI3Yi0xMWU3LTk4NzUtYTdkOWQ4MmY=";
         // let result = WangxUtils.createLtpaToken("*shangguanzirui*", 12, key);
         let token = "AQIDBDVBNDVGNzBCNUE0NjlGQ0Iqc2hhbmdndWFuemlydWkq9G2GcWudAn8KpUHh8g9cRJEmtGc=";
         let result = WangxUtils.parseLtpaToken(token, key);
-        console.info("result---====....>>", result);*!/
-    }*/
+        console.info("result---====....>>", result);*/
+    }
 
     if(cluster.isMaster){
         let replServer = new ReplServer(1337, {context: {API: API}});
