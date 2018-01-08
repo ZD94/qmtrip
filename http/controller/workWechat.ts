87156d4534651780539e65818cea107a7b479eee
lisongwei <1429594204@qq.com>
2018-01-08 10:39:36 +0800

    Add interface getPermanentCode

--- http/controller/workWechat.ts
+++ http/controller/workWechat.ts
@@ -1,7 +1,9 @@
 import { AbstractController, Restful, Router } from "@jingli/restful";
 const API = require('@jingli/dnode-api');
 import { Request, Response } from 'express-serve-static-core';
-import { JLError, CustomerError } from '@jingli/error';
+import { CustomerError } from '@jingli/error';
+import { Models } from '_types';
+import { SPropertyType } from '_types/staff';
 
 @Restful()
 export class WorkWechatController extends AbstractController {
@@ -17,6 +19,16 @@ export class WorkWechatController extends AbstractController {
         } catch (e) {
             throw new CustomerError(400, '登录失败')
         }
+    }
+
+    @Router('/permanentCode/:corpId', 'GET')
+    async getCorpPermanentCode(req: Request, res: Response) {
+        const companies = await Models.companyProperty.find({
+            where: { type: SPropertyType.WECHAT_CORPID, value: req.params.corpId }
+        })
+        const permanentCode = await Models.companyProperty.find({
+            where: { type: SPropertyType, company_id: companies[0].companyId }
+        })
     }
 
 }
