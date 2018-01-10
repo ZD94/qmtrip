16cfd3aa2ed39f3e2bb49950f1d7e3bdfd430b72
yuchanglong <changlong.yu@jingli365.com>
2018-01-10 19:19:44 +0800

    修改获取staff的逻辑

--- api/sso/libs/wechat-staff.ts
+++ api/sso/libs/wechat-staff.ts
@@ -3,7 +3,7 @@ import { OaStaff } from 'libs/asyncOrganization/oaStaff';
 import { Company, CPropertyType } from "_types/company";
 import { EGender, Models } from "_types";
 import { RestApi } from "api/sso/libs/restApi";
-import { StaffProperty, SPropertyType } from "_types/staff";
+import { StaffProperty, SPropertyType, Staff } from "_types/staff";
 import { WDepartment, IWDepartment } from "api/sso/libs/wechat-department";
 var corpId = 'wwb398745b82d67068'
 var suiteId ='wwcd27af224b6e42e8';
@@ -101,6 +101,31 @@ export class WStaff extends OaStaff {
         super(target);
         this.restApi = target.restApi;
         this.departmentIds = target.departmentIds;
+    }
+
+    /**
+     * @method 企业微信同一人在不同的公司，userid可能相同，存在姓名为userid的情况
+     * @return {Staff}
+     */
+    async getStaff(): Promise<Staff>{
+        let self = this;
+        let staff: Staff = null;
+        
+        let staffPro = await Models.staffProperty.find({where : {value: self.id}});
+        if(staffPro && staffPro.length > 0){
+            staff = await Models.staff.get(staffPro[0].staffId);
+        }
+        if(!staff) return null;
+        let comPro = await Models.staffProperty.find({
+            where: {
+                staffId: staff.id,
+                type: SPropertyType.WECHAT_CORPID,
+                value: self.corpId
+            }
+        });
+        if(!comPro || comPro.length == 0) 
+            return null;
+        return staff;
     }
     
     /**
