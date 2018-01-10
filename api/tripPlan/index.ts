8d0d6d1466deda45ea04f759c5f3247e39ee5dc2
lizeilin <zelin.li@jingli365.com>
2018-01-10 10:14:13 +0800

    fixxxx

--- api/tripPlan/index.ts
+++ api/tripPlan/index.ts
@@ -606,7 +606,7 @@ class TripPlanModule {
                 let staffId = tripPlan.accountId;
                 let staff = await Models.staff.get(staffId);
                 let staffName = staff.name;
-                tripPlan.remark = `员工${staffName}节省${savedMoney}后企业奖励${tripPlan.score}元`
+                tripPlan.remark = `员工${staffName}节省${savedMoney}后企业奖励${tripPlan.score}元`;
                 if(tripPlan.isSpecialApprove){
                     tripPlan.saved = 0;
                 }else{
