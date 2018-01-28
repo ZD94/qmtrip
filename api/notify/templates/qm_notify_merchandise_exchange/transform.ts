2fa87e40e6c791f81a4d43e230bfc62911883e93
yuchanglong <changlong.yu@jingli365.com>
2018-01-28 17:22:17 +0800

    update

--- api/notify/templates/qm_notify_merchandise_exchange/transform.ts
+++ api/notify/templates/qm_notify_merchandise_exchange/transform.ts
@@ -1,6 +1,9 @@
 /**
  * Created by yuchanglong on 2018/01/15
  */
+import * as moment from "moment";
 export = async function transform(values: any): Promise<any>{
+    if(values.updateTime)
+        values.updateTime = moment(values.updateTime).format('YYYY-MM-DD');
     return values;
 }
