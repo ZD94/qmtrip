519aba9925132a8c888ec6473e317809dce6c3a3
lizeilin <zelin.li@jingli365.com>
2018-02-05 16:00:07 +0800

    fix sms link problem

--- api/tripApprove/index.ts
+++ api/tripApprove/index.ts
@@ -274,7 +274,7 @@ export default class TripApproveModule {
         let version = params.version || config.link_version || 2  //@#template 外链生成的版本选择优先级：参数传递的版本 > 配置文件中配置的版本 > 默认版本为2
         if (version == 2) {
             appMessageUrl = `#/trip-approval/approve-detail/${tripApprove.id}/1`
-            approve_url = `${config.v2_host}/${appMessageUrl}` //参数为tripApproveId和titleId；
+            approve_url = `${config.v2_host}${appMessageUrl}` //参数为tripApproveId和titleId；
         } else {
             approve_url = `${config.host}/index.html#/trip-approval/detail?approveId=${tripApprove.id}`;
             let finalUrl = `#/trip-approval/detail?approveId=${tripApprove.id}`;
