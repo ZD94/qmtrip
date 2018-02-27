460827b43c952dfa95b793d9def95105c328b38a
yuchanglong <changlong.yu@jingli365.com>
2018-02-27 10:43:16 +0800

    remove log

--- api/travelBudget/index.ts
+++ api/travelBudget/index.ts
@@ -260,10 +260,9 @@ export default class ApiTravelBudget {
                 commonData = combineData(commonData, 'No', 'agents')
             }    
             if (meiyaTrain){      
-                commonData2 = await handleTrainData(meiyaTrain, params)
+                commonData2 = handleTrainData(meiyaTrain, params)
                 commonData2 = combineData(commonData2, 'No', 'agents')
             }       
-            console.log("commonData: ", commonData)
             return [...commonData, ...commonData2];
         }
     }
