aff14e6c3300865d2b32377f3c9d88b015cac745
wanglihui <lihui.wang@jingli365.com>
2018-03-09 15:31:57 +0800

    modify citycache version

--- api/place/index.ts
+++ api/place/index.ts
@@ -2,7 +2,7 @@ import { requireParams, clientExport } from '@jingli/dnode-api/dist/src/helper';
 import { restfulAPIUtil } from '../restful'
 var _ = require("lodash");
 const cache = require("common/cache")
-const cityPrefix = 'city:info:id:v2'
+const cityPrefix = 'city:info:id:v3'
 export default class PlaceModule {
 
     @clientExport
