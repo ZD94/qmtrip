ab81074982e577ee41306ae4c43c184f480c7fce
lisongwei <1429594204@qq.com>
2018-03-29 11:56:22 +0800

    modify event resend

--- api/eventListener/baseEvent.ts
+++ api/eventListener/baseEvent.ts
@@ -2,13 +2,14 @@
 
 import { Models } from '_types';
 import { EventListener } from '_types/eventListener'
+import L from '@jingli/language';
 const request = require("request-promise");
 
 export class BaseEvent {
     private maxTryTimes = 5;
     private getDelay(count: number): number {
         let seconds: number = 0;
-        switch(count) {
+        switch (count) {
             case 1:
                 seconds = 0;
                 break;
@@ -28,36 +29,41 @@ export class BaseEvent {
         return seconds;
     }
 
-    async sendEventNotice(params: { url: string, body: object }, count = 1): Promise<any> {
-        let self = this;
+    async sendEventNotice(params: { url: string, body: object }, count = 1, maxTimes = 1, isRet = true): Promise<any> {
         const { url, body } = params
         console.log('send event to ', url)
-        try {
-            let result = await request({
-                body,
-                uri: url,
-                method: 'POST',
-                json: true
-            });
-            if (typeof (result) == 'string') {
-                result = JSON.parse(result);
-            }
-            console.log('response is', result)
-            if (result && result.code == 0 || result.code == 200) {
-                return result.data;
-            }
-        } catch (err) {
-            console.error('send event notify err:', err);
-        }
         count++;
-        if (count > self.maxTryTimes) {
-            console.error('send event notify try ', self.maxTryTimes, 'but fail!');
-            return;
+        let result = await request({
+            body,
+            uri: url,
+            method: 'POST',
+            json: true,
+        });
+        if (typeof (result) == 'string') {
+            result = JSON.parse(result);
+        }
+        console.log('response is', result)
+        if (result && result.code == 0 || result.code == 200) {
+            return result.data;
         }
 
-        setTimeout( () => {
-            self.sendEventNotice(params, count);
-        }, self.getDelay(count) * 1000);
+        let err = new L.ERROR_CODE_C(result.code, result.msg);
+        let self = this;
+        let ret = new Promise((resolve, reject) => {
+            if (count > maxTimes) {
+                console.error('send event notify had try ', maxTimes, 'but still failed!');
+                return reject(err);
+            }
+
+            let delay = self.getDelay(count) * 1000
+            setTimeout(() => {
+                return resolve(self.sendEventNotice(params, count));
+            }, delay);
+        });
+        if (isRet) return ret
+        ret.catch((err) => {
+            console.error("sendEventNotice==>", err);
+        })
     }
 
     async findEventListener(event: string, companyId: string): Promise<EventListener | null> {
