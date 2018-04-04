bf58d14e6e91c78398e3023541f9ced1f7d09a0f
wangyali <yali.wang@jingli365.com>
2018-04-04 21:24:48 +0800

    delete static

--- api/tripPackage/index.ts
+++ api/tripPackage/index.ts
@@ -9,13 +9,13 @@ import { TripBasicPackage, TripFuelAddPackage } from '_types/tripPackage';
 export class TripPackageModule{
     @clientExport
     @requireParams(["id"])
-    static getBasicTripPackage(params: {id: string}): Promise<TripBasicPackage | null> {
+    getBasicTripPackage(params: {id: string}): Promise<TripBasicPackage | null> {
         return Models.tripBasicPackage.get(params.id);
     }
 
     @clientExport
     @requireParams(["id"])
-    static getFuelAddPackage(params: {id: string}): Promise<TripFuelAddPackage | null> {
+    getFuelAddPackage(params: {id: string}): Promise<TripFuelAddPackage | null> {
         return Models.tripFuelAddPackage.get(params.id);
     }
 
