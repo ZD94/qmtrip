d2335c924ff92dc6aa858eadbd418da9af881c32
王亚丽 <yali.wang@jingli365.com>
2018-04-04 18:19:15 +0800

    Update index.ts

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -52,7 +52,7 @@ function makeLinkSign(linkToken: string, invitedLinkId: string, timestamp: numbe
  */
 export class ApiAuth {
 
-    public __public: boolean = true;
+    static __public: boolean = true;
 
     /**
      * 验证验证码(通过手机重置密码第一步)
